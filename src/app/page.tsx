6e1892673711d091d9d82d71f4e955e16cb9f0aa
Dan Ko <danko56666@gmail.com>
2023-08-04 00:42:16 -0400

    rename redux provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 // counter page is just a test, remove later. Don't remove the provider
 
-import { ReduxProvider } from "@/components";
+import { StoreProvider } from "@/components";
 import { CounterPage } from "@/app/counter";
 
 export default function Home() {
   return (
     <main>
-      <ReduxProvider>
+      <StoreProvider>
         <CounterPage />
-      </ReduxProvider>
+      </StoreProvider>
     </main>
   );
 }
