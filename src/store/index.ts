c8438813478384e7ca7dc38946757a2a67c05430
Dan Ko <danko56666@gmail.com>
2023-08-03 22:44:26 -0400

    setup redux toolkit

---
+++ src/store/index.ts
@@ -0,0 +1,7 @@
+export { store, type RootState, type AppDispatch } from "./store";
+export { useAppDispatch, useAppSelector } from "./hooks";
+export {
+  increment,
+  decrement,
+  selectCount,
+} from "./features/counter/counterSlice";
