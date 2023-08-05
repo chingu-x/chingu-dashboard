d9c930601fcb85a1fa33dab2582e752ec8729d9a
Dan Ko <danko56666@gmail.com>
2023-08-04 22:39:36 -0400

    setup cypress

--- src/app/counter/counter.test.tsx
+++ src/app/counter/counter.test.tsx
@@ -1,10 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import CounterPage from "./page";
 
 it("should render the component", () => {
   render(<CounterPage />);
-
-  const button = screen.getByRole("button", { name: "+" });
-
-  expect(button).toBeInTheDocument();
 });
