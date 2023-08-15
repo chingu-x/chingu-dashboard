735ca095437f6c7c80fa309b284a528a8576fec4
Dan Ko <danko56666@gmail.com>
2023-08-14 22:52:36 -0400

    update folder structure to follow barrel export convention

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,3 @@
 export { store, type RootState, type AppDispatch } from "./store";
 export { useAppDispatch, useAppSelector } from "./hooks";
-export {
-  increment,
-  decrement,
-  selectCount,
-} from "./features/counter/counterSlice";
+export { increment, decrement, selectCount } from "./features/counter";
