d2b266c358c44d6b8074638c353348f09c9ffd40
Dan Ko <danko56666@gmail.com>
2023-08-15 22:41:40 -0400

    move redux provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 // counter page is just a test, remove later. Don't remove the provider
 
-import { StoreProvider } from "@/components";
 import { CounterPage } from "@/app/counter";
 
 export default function Home() {
   return (
     <main>
-      <StoreProvider>
-        <CounterPage />
-      </StoreProvider>
+      <CounterPage />
     </main>
   );
 }
