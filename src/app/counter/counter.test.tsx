d753f2219eaef03a599fa53dd127108f6e786cf5
Mark Linn <marktlinn@gmail.com>
2023-08-25 11:00:40 +0200

    fix: counter test -> with redux provider

--- src/app/counter/counter.test.tsx
+++ src/app/counter/counter.test.tsx
@@ -1,6 +1,10 @@
 import { render } from "@testing-library/react";
 import CounterPage from "./page";
-
+import { StoreProvider } from "@/components";
 it("should render the component", () => {
-  render(<CounterPage />);
+  render(
+    <StoreProvider>
+      <CounterPage />
+    </StoreProvider>,
+  );
 });
