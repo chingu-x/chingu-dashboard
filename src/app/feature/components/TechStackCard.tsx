0f842971df7a29e7d6f2212c5676076338d11c34
Timothy Russo <timothy.russo.ext@adevinta.com>
2023-08-27 14:28:01 +0200

    feat: improve accessibility

--- src/app/feature/components/TechStackCard.tsx
+++ src/app/feature/components/TechStackCard.tsx
@@ -1,21 +1,26 @@
 import { frontend } from "./fixtures/TechStackCard";
 import { Avatar } from "@/components";
 
-export default function TechStackCard() {
+interface TechStackCardProps {
+  title: string
+}
+
+export default function TechStackCard({ title }: TechStackCardProps) {
   return (
     <div className="card w-96 text-primary-content bg-neutral-100 rounded-lg">
       <div className="flex flex-row justify-between">
-        <h2 className="text-xl font-semibold text-neutral-900 mt-5 ml-5 self-center">
-          Frontend
-        </h2>
+        <h3 className="text-xl font-semibold text-neutral-900 mt-5 ml-5 self-center">
+          {title}
+        </h3>
         <button
           type="button"
           className="btn mt-5 mr-5 capitalize w-16 h-8 min-h-full text-sm font-semibold text-neutral-500 bg-white border-transparent"
+          aria-label={`Edit ${title}`}
         >
           Edit
         </button>
       </div>
-      <div className="h-40 overflow-y-auto ml-5 mr-5 mt-6 mb-5">
+      <div className="h-40 overflow-y-auto mx-5 mt-6 mb-5">
         <ul className="text-neutral-900 last:mb-0">
           {frontend.map((element) => (
             <li
