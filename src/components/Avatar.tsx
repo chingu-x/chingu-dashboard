7f383438dfc9225e6fc9ceac43e5e32bbdd8da8b
Timothy Russo <russotimothy@live.it>
2023-08-29 09:07:25 +0200

    feat: pr review corrections

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,6 +1,6 @@
 export default function Avatar() {
   return (
-    <div className="avatar bg-neutral-300 rounded-full border-2 border-neutral-100">
+    <div className="avatar bg-neutral-300 rounded-full border-2 border-neutral-50">
       <div className="w-6"></div>
     </div>
   );
