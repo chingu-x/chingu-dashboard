fdc1beab5ccc13407a5cb2fb3e4aa45cbaadddaa
Dan Ko <danko56666@gmail.com>
2023-09-04 21:54:36 -0400

    vote description

--- src/app/ideation/components/VoteDescriptionCard.tsx
+++ src/app/ideation/components/VoteDescriptionCard.tsx
@@ -1,9 +1,11 @@
 function VoteDescriptionCard() {
   return (
-    <div className="card w-1/4 h-32 bg-base-content text-neutral-focus rounded-lg">
+    <div className="card w-1/4 h-32 bg-base-content rounded-lg ml-10">
       <div className="flex flex-col items-start p-4 gap-y-4">
-        <h2 className="text-xl font-semibold">Votes</h2>
-        <div className="text-base font-medium text-neutral text-left">Vote for the projects you are interested in.</div>
+        <h2 className="text-xl font-semibold text-neutral-focus">Votes</h2>
+        <div className="text-base font-medium text-neutral text-left">
+          Vote for the projects you are interested in.
+        </div>
       </div>
     </div>
   );
