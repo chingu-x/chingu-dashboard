9e6954ac23bf475ce4bab1d77cf075e678625525
Dan Ko <danko56666@gmail.com>
2023-09-05 19:32:15 -0400

    finish implementing vote card with mock data

--- src/app/ideation/components/index.ts
+++ src/app/ideation/components/index.ts
@@ -3,3 +3,4 @@ export { default as IdeationContainer } from "./IdeationContainer";
 export { default as VoteDescriptionCard } from "./VoteDescriptionCard";
 export { default as VoteCard } from "./VoteCard";
 export { default as ContributionCard } from "./ContributionCard";
+export { ideation, type Ideation } from "./fixtures/ideation";
