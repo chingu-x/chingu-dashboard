4302f4a9d8329d7b2210893618d070710b265113
Dan Ko <danko56666@gmail.com>
2023-09-04 21:54:36 -0400

    vote description

--- src/app/ideation/components/CreateIdeationContainer.tsx
+++ src/app/ideation/components/CreateIdeationContainer.tsx
@@ -3,8 +3,10 @@ import { VoteDescriptionCard } from ".";
 function CreateIdeationContainer() {
   return (
     <div className="card w-[1280px] h-[190px] bg-base-100 text-neutral-content flex flex-row items-center">
-      <div className="card-body items-center text-center">
         <VoteDescriptionCard />
+      <div className="card-body gap-y-7">
+        <h2 className="text-xl font-semibold text-neutral-focus">What is your Voyage project idea & vision?</h2>
+        <p className="text-base font-medium text-neutral">We value your ideas! Share your ideas on what our project should be. Describe your vision to capture what it does and the benefit it will bring to users.</p>
       </div>
     </div>
   );
