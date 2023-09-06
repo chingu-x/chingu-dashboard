81ee7a7fc6193c0640342a18bee35828729e44db
Jane Moroz <Moroz.jane@yahoo.com>
2023-09-06 13:48:26 +0300

    feat: add "Edit" button

--- src/app/my-team/components/TeamRow.tsx
+++ src/app/my-team/components/TeamRow.tsx
@@ -1,4 +1,9 @@
+import { PencilSquareIcon } from "@heroicons/react/24/solid";
 import { TeamMember } from "./fixtures/MyTeam";
+import { Button } from "@/components";
+
+// Temp:
+const currentUserId = "1";
 
 interface TeamRowProps {
   teamMemeber: TeamMember;
@@ -9,7 +14,19 @@ function TeamRow({ teamMemeber }: TeamRowProps) {
     <tr>
       <td className="text-black">{teamMemeber.name}</td>
       <td>{teamMemeber.discordId}</td>
-      <td>{teamMemeber.averageHour}</td>
+      <td>
+        <div className="flex items-center justify-between">
+          {teamMemeber.averageHour}
+          {teamMemeber.id === currentUserId && (
+            <Button
+              title={"edit"}
+              customClassName="gap-x-0 px-2 h-8 p-0 min-h-full text-sm font-semibold text-black bg-white border-transparent"
+            >
+              <PencilSquareIcon className="w-4 h-4 text-black" />
+            </Button>
+          )}
+        </div>
+      </td>
       <td>{teamMemeber.location}</td>
       <td>{teamMemeber.timeZone}</td>
       <td>{teamMemeber.email}</td>
