e13d21da352bf8f1c2f92229441ab9e168b4cfdf
Mark Linn <marktlinn@gmail.com>
2023-09-07 19:08:49 +0200

    refactor: rm use of useHooks for native DaisyUI btn

--- src/components/navbar/DropDownLink.tsx
+++ src/components/navbar/DropDownLink.tsx
@@ -3,21 +3,20 @@ import Link from "next/link";
 interface DropDownLinkProps {
   title: string;
   href?: string;
-  handleClick: () => void;
 }
 
 export default function DropDownLink({
   title,
   href = "#",
-  handleClick,
 }: DropDownLinkProps) {
   return (
-    <Link
-      href={href}
-      className="text-neutral hover:text-primary duration-200"
-      onClick={handleClick}
-    >
-      {title}
-    </Link>
+    <li>
+      <Link
+        href={href}
+        className="text-neutral hover:text-primary duration-200"
+      >
+        {title}
+      </Link>
+    </li>
   );
 }
