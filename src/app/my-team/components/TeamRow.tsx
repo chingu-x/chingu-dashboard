6ec6fae14813c8206c9a9f0ba2d48dc6ad703cda
Jane Moroz <Moroz.jane@yahoo.com>
2023-09-07 13:35:13 +0300

    fix: move temp variable to TeamTable

--- src/app/my-team/components/TeamRow.tsx
+++ src/app/my-team/components/TeamRow.tsx
@@ -2,14 +2,12 @@ import { PencilSquareIcon } from "@heroicons/react/24/solid";
 import { TeamMember } from "./fixtures/MyTeam";
 import { Button } from "@/components";
 
-// Temp:
-const currentUserId = "1";
-
 interface TeamRowProps {
   teamMemeber: TeamMember;
+  currentUserId: string;
 }
 
-function TeamRow({ teamMemeber }: TeamRowProps) {
+function TeamRow({ teamMemeber, currentUserId }: TeamRowProps) {
   return (
     <tr>
       <td className="text-black">{teamMemeber.name}</td>
@@ -20,7 +18,7 @@ function TeamRow({ teamMemeber }: TeamRowProps) {
           {teamMemeber.id === currentUserId && (
             <Button
               title={"edit"}
-              customClassName="gap-x-0 px-2 h-8 p-0 min-h-full text-sm font-semibold text-black bg-white border-transparent"
+              customClassName="pl-2 pr-1 h-8 p-0 min-h-full text-sm font-semibold text-black bg-white border-transparent"
             >
               <PencilSquareIcon className="w-4 h-4 text-black" />
             </Button>
