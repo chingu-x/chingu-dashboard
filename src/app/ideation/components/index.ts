9d04bf5630d17c13ea4b1f6154143c33b628a7bc
Dan Ko <danko56666@gmail.com>
2023-09-08 20:29:29 -0400

    fix index file and remove export

--- src/app/ideation/components/index.ts
+++ src/app/ideation/components/index.ts
@@ -4,4 +4,3 @@ export { default as VoteDescriptionCard } from "./VoteDescriptionCard";
 export { default as VoteCard } from "./VoteCard";
 export { default as ContributionCard } from "./ContributionCard";
 export { ideation, type Ideation } from "./fixtures/ideation";
-export { banner_title, banner_description } from "./fixtures/ideation_banner";
