ef0c67e0b3592cf0ce99ca54539d7b57434985dc
Dan Ko <danko56666@gmail.com>
2023-09-08 20:28:09 -0400

    remove fixture because it won't be needed as banner content is static

--- src/app/ideation/components/fixtures/ideation_banner.ts
+++
@@ -1,3 +0,0 @@
-export const banner_title = "Ideation";
-export const banner_description =
-  "Okay, time to put on your thinking caps and channel your inner creativity! What kind of amazing, mind-blowing project idea do you have that will make SpaceX jealous? Let's hear it!";
