45d0a93e5512ab19a895420dda9a6c75d3508531
Dan Ko <danko56666@gmail.com>
2023-09-08 20:25:00 -0400

    finish reusable banner

--- src/app/ideation/components/index.ts
+++ src/app/ideation/components/index.ts
@@ -4,3 +4,4 @@ export { default as VoteDescriptionCard } from "./VoteDescriptionCard";
 export { default as VoteCard } from "./VoteCard";
 export { default as ContributionCard } from "./ContributionCard";
 export { ideation, type Ideation } from "./fixtures/ideation";
+export { banner_title, banner_description } from "./fixtures/ideation_banner";
---
+++ src/components/Banner.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+interface BannerProps {
+  image: string;
+  alt: string;
+  title: string;
+  description: string;
+}
+
+// eslint and prettier conflicting again. object-curly-newline
+// eslint-disable-next-line
+function Banner({ image, alt, title, description }: BannerProps) {
+  return (
+    <div className="card w-[1280px] h-[320px] bg-accent-content flex flex-row px-20">
+      <Image src={image} alt={alt} width={454} height={300} priority={false} />
+      <div className="flex flex-col justify-center pl-24 gap-y-3.5">
+        <h3 className="text-3xl font-bold text-black">{title}</h3>
+        <p className="text-lg font-medium text-black">{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
