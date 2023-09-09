c683476d7307683158fdb64e050b8fbab8118ff9
Jane Moroz <Moroz.jane@yahoo.com>
2023-09-09 10:09:27 +0300

    fix: change averageHour column

--- src/app/directory/components/TeamRow.tsx
+++ src/app/directory/components/TeamRow.tsx
@@ -13,12 +13,12 @@ function TeamRow({ teamMember, currentUserId }: TeamRowProps) {
       <td className="text-black">{teamMember.name}</td>
       <td>{teamMember.discordId}</td>
       <td>
-        <div className="flex items-center justify-between">
-          {teamMember.averageHour}
+        <div className="flex items-center justify-between h-[35px] bg-white rounded-md pl-4 pr-2">
+          {teamMember.averageHour === 0 ? "Add hours" : teamMember.averageHour}
           {teamMember.id === currentUserId && (
             <Button
               title={"edit"}
-              customClassName="pl-2 pr-1 h-8 p-0 min-h-full text-sm font-semibold text-black bg-white border-transparent"
+              customClassName="pl-2 pr-1 h-8 p-0 min-h-0 text-sm font-semibold text-black bg-white border-transparent"
             >
               <PencilSquareIcon className="w-4 h-4 text-black" />
             </Button>
