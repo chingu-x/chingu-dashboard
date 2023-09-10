4456e8aa9e3656b96a4ffb7b56c388782606489d
Timothy Russo <russotimothy@live.it>
2023-09-10 12:47:09 +0200

    feat: add basic structure for the sidebar

--- src/app/sidebar/components/Sidebar.tsx
+++ src/app/sidebar/components/Sidebar.tsx
@@ -1,6 +1,16 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Sidebar() {
+  const [expand, setExpand] = useState(false);
+
   return (
-    <div className="fixed top-[75px] bottom-0 left-0 w-[93px] text-center bg-white border border-black">
+    <div
+      className={`fixed top-[75px] bottom-0 left-0 ${
+        expand ? "w-[300px]" : "w-[93px]"
+      } text-center bg-white border border-black transition-all`}
+    >
       <div className="flex flex-col justify-between h-full">
         <ul className="flex flex-col items-center pt-[24px] h-">
           <li className="w-[24px] h-[24px] bg-black p-[24px] mb-[16px]"></li>
@@ -10,7 +20,11 @@ export default function Sidebar() {
         </ul>
         <div className="flex flex-col items-center">
           <div className="bg-black h-[1px] w-full mb-[56px]"></div>
-          <div className="w-[24px] h-[24px] bg-black p-[24px]"></div>
+          <button
+            type="button"
+            className="w-[24px] h-[24px] bg-black p-[24px]"
+            onClick={() => setExpand(!expand)}
+          ></button>
         </div>
       </div>
     </div>
