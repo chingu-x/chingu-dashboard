b0efc2f8371e0a5b9ed7e9d19c9e041491c63141
Timothy Russo <russotimothy@live.it>
2023-09-10 15:00:10 +0200

    feat: buttons layout

--- src/app/sidebar/components/Sidebar.tsx
+++ src/app/sidebar/components/Sidebar.tsx
@@ -1,30 +1,89 @@
 "use client";
 
 import { useState } from "react";
+import {
+  RectangleGroupIcon,
+  ChartBarIcon,
+  BookmarkSquareIcon,
+  RocketLaunchIcon,
+  ArrowRightOnRectangleIcon,
+  ArrowLeftOnRectangleIcon,
+} from "@heroicons/react/24/solid";
+import { Button } from "@/components";
 
 export default function Sidebar() {
   const [expand, setExpand] = useState(false);
 
   return (
     <div
-      className={`fixed top-[75px] bottom-0 left-0 ${
-        expand ? "w-[300px]" : "w-[93px]"
-      } text-center bg-white border border-black transition-all`}
+      className={`fixed z-0 top-0 bottom-0 left-0 ${
+        expand ? "w-[295px]" : "w-[93px]"
+      } text-center bg-white transition-all box-content`}
     >
-      <div className="flex flex-col justify-between h-full">
-        <ul className="flex flex-col items-center pt-[24px] h-">
-          <li className="w-[24px] h-[24px] bg-black p-[24px] mb-[16px]"></li>
-          <li className="w-[24px] h-[24px] bg-black p-[24px] mb-[16px]"></li>
-          <li className="w-[24px] h-[24px] bg-black p-[24px] mb-[60px]"></li>
-          <li className="w-[24px] h-[24px] bg-black p-[24px]"></li>
+      <div className="flex flex-col justify-between h-full pt-[64px]">
+        <ul
+          className={`flex flex-col ${
+            expand ? "items-start pl-[40px]" : "items-center"
+          } pt-[24px] h-full transition-all`}
+        >
+          <Button
+            title="Dashboard"
+            customClassName={`${
+              expand
+                ? "w-[230px] flex justify-start pl-[24px] transition-all"
+                : "w-[50px]"
+            } h-[50px] mb-[16px] bg-white text-black capitalize border-none`}
+          >
+            <RectangleGroupIcon className="h-[18px]" />
+            {expand ? "Dashboard" : ""}
+          </Button>
+          <Button
+            title="Assessment"
+            customClassName={`${
+              expand
+                ? "w-[230px] flex justify-start pl-[24px] transition-all"
+                : "w-[50px]"
+            } h-[50px] mb-[16px] bg-white text-black capitalize border-none`}
+          >
+            <ChartBarIcon className="h-[18px]" />
+            {expand ? "Assessment" : ""}
+          </Button>
+          <Button
+            title="Resources"
+            customClassName={`${
+              expand
+                ? "w-[230px] flex justify-start pl-[24px] transition-all"
+                : "w-[50px]"
+            } h-[50px] mb-[60px] bg-white text-black capitalize border-none`}
+          >
+            <BookmarkSquareIcon className="h-[18px]" />
+            {expand ? "Resources" : ""}
+          </Button>
+          <Button
+            title="My Voyage"
+            customClassName={`${
+              expand
+                ? "w-[230px] flex justify-start pl-[24px] transition-all"
+                : "w-[50px]"
+            } h-[50px] bg-white text-black capitalize border-none`}
+          >
+            <RocketLaunchIcon className="h-[18px]" />
+            {expand ? "My Voyage" : ""}
+          </Button>
         </ul>
-        <div className="flex flex-col items-center">
-          <div className="bg-black h-[1px] w-full mb-[56px]"></div>
-          <button
-            type="button"
-            className="w-[24px] h-[24px] bg-black p-[24px]"
+        <div className="flex flex-col items-end justify-start border-t border-secondary-focus h-[80px]">
+          <Button
+            title="Expand"
+            customClassName="w-[50px]
+             bg-white text-black capitalize hover:bg-white mr-[8px] border-none"
             onClick={() => setExpand(!expand)}
-          ></button>
+          >
+            {expand ? (
+              <ArrowLeftOnRectangleIcon className="h-[24px]" />
+            ) : (
+              <ArrowRightOnRectangleIcon className="h-[24px]" />
+            )}
+          </Button>
         </div>
       </div>
     </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,7 +6,7 @@ const notificationCount = 4;
 
 export default function Navbar() {
   return (
-    <nav className="navbar bg-primary h-8">
+    <nav className="navbar sticky bg-primary h-8 z-10">
       <div className="flex-1 pl-2">
         <ChinguMenu />
       </div>
