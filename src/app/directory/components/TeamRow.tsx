8d31db92e2e1a91c242b978f67d7356ba7068d67
Jane Moroz <Moroz.jane@yahoo.com>
2023-09-11 13:34:47 +0300

    style: remove hover effects from the edit btn

--- src/app/directory/components/TeamRow.tsx
+++ src/app/directory/components/TeamRow.tsx
@@ -13,12 +13,12 @@ function TeamRow({ teamMember, currentUserId }: TeamRowProps) {
       <td className="text-black">{teamMember.name}</td>
       <td>{teamMember.discordId}</td>
       <td>
-        <div className="flex items-center justify-between h-[35px] bg-white rounded-md pl-4 pr-2">
+        <div className="flex items-center justify-between h-[35px] bg-white rounded-md pl-4">
           {teamMember.averageHour === 0 ? "Add hours" : teamMember.averageHour}
           {teamMember.id === currentUserId && (
             <Button
               title={"edit"}
-              customClassName="pl-2 pr-1 h-8 p-0 min-h-0 text-sm font-semibold text-black bg-white border-transparent"
+              customClassName="pl-2 pr-1 h-full rounded-l-none rounded-r-md p-0 min-h-0 text-sm font-semibold text-black bg-white border-transparent hover:bg-white hover:border-transparent"
             >
               <PencilSquareIcon className="w-4 h-4 text-black" />
             </Button>
