3d82961ac160b151c4e1b8a788813c9e5ffda60c
Jane Moroz <Moroz.jane@yahoo.com>
2023-09-12 14:35:22 +0300

    feat: add ThemeProvider from "next-themes"

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as StoreProvider } from "./StoreProvider";
 export { default as Avatar } from "./Avatar";
 export { default as Navbar } from "./navbar/Navbar";
 export { default as Button } from "./Button";
+export { default as ThemeProvider } from "./ThemeProvider";
