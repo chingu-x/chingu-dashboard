329cc323200ba9414f1e55fd053093a31b69fd01
Timothy Russo <russotimothy@live.it>
2023-09-17 13:34:22 +0200

    feat: refactor the code in reusable components

--- src/app/sidebar/components/Sidebar.tsx
+++ src/app/sidebar/components/Sidebar.tsx
@@ -6,53 +6,58 @@ import {
   ChartBarIcon,
   BookmarkSquareIcon,
   RocketLaunchIcon,
-  ArrowRightOnRectangleIcon,
-  ArrowLeftOnRectangleIcon,
-  LockClosedIcon,
 } from "@heroicons/react/24/solid";
-import { ArrowRightCircleIcon } from "@heroicons/react/20/solid";
-import Link from "next/link";
-import { Button } from "@/components";
+import PageButton from "./PageButton";
+import VoyagePageButton from "./VoyagePageButton";
+import VoyageStatus from "./VoyageStatus";
+import ExpandButton from "./ExpandButton";
 
-type PageProperty = {
+export type PageProperty = {
   name: string;
   marginBottom: string;
   icon: JSX.Element;
 };
 
+export enum MainPages {
+  dashboard = "Dashboard",
+  assessment = "Assessment",
+  resources = "Resources",
+  myVoyage = "My Voyage"
+}
+
+export enum VoyagePages {
+  directory = "Directory",
+  techStack = "Tech Stack",
+  ideation = "Ideation",
+  features = "Features",
+  sprints = "Sprints",
+  resources = "Resources",
+}
+
 const pagesProperties: PageProperty[] = [
   {
-    name: "Dashboard",
+    name: MainPages.dashboard,
     marginBottom: "mb-4",
     icon: <RectangleGroupIcon className="h-[1.125rem]" />,
   },
   {
-    name: "Assessment",
+    name: MainPages.assessment,
     marginBottom: "mb-4",
     icon: <ChartBarIcon className="h-[1.125rem]" />,
   },
   {
-    name: "Resources",
+    name: MainPages.resources,
     marginBottom: "mb-[3.75rem]",
     icon: <BookmarkSquareIcon className="h-[1.125rem]" />,
   },
   {
-    name: "My Voyage",
+    name: MainPages.myVoyage,
     marginBottom: "mb-4",
     icon: <RocketLaunchIcon className="h-[1.125rem]" />,
   },
 ];
 
-const voyagePages: string[] = [
-  "Directory",
-  "Tech Stack",
-  "Ideation",
-  "Features",
-  "Sprints",
-  "Resources",
-];
-
-const voyageStarted: boolean = false;
+const isVoyageStarted: boolean = true;
 
 const voyageData = {
   tier: "Tier 3",
@@ -61,134 +66,58 @@ const voyageData = {
 };
 
 export default function Sidebar() {
-  const [openSidebar, setOpenSidebar] = useState<boolean>(true);
+  const [isOpenSidebar, setIsOpenSidebar] = useState<boolean>(true);
   const [selectedButton, setSelectedButton] = useState<string>("");
   const [hoveredButton, setHoveredButton] = useState<string | null>(null);
-
-  const showIcon = (element: string) => {
-    if (!voyageStarted) {
-      return <LockClosedIcon className="h-[1.125rem]" />;
-    } else if (
-      voyageStarted &&
-      (hoveredButton === element || selectedButton === element)
-    ) {
-      return (
-        <div className="absolute left-[1.4rem]">
-          <ArrowRightCircleIcon className="h-[1.125rem] text-primary" />
-        </div>);
-    }
-  };
-
-  const buttonStyles = `${
-    openSidebar
-      ? "w-[14.375rem] flex justify-start pl-6"
-      : "w-[3.125rem]"
-  } h-[3.125rem] text-black capitalize border-none`;
-
-  const getButtonBackgroundStyle = (page: string) =>
-    selectedButton === page || page === "My Voyage" && voyagePages.includes(selectedButton) ? "bg-neutral-content" : "bg-white";
-
-  const getButtonText = (page: string) => (openSidebar ? page : "");
 
   const handlePageClick = (element: PageProperty | string) => {
     if (typeof element === "string") {
       setSelectedButton(element);
-      setOpenSidebar(false);
-    } else if(element.name === "My Voyage") {
+      setIsOpenSidebar(false);
+    } else if (element.name as MainPages === MainPages.myVoyage) {
       setSelectedButton(element.name);
-      setOpenSidebar(true);
+      setIsOpenSidebar(true);
     } else {
       setSelectedButton(element.name);
-      setOpenSidebar(false);
+      setIsOpenSidebar(false);
     }
   };
 
   return (
     <aside
       className={`fixed z-0 top-0 bottom-0 left-0 ${
-        openSidebar ? "w-[18.4375rem]" : "w-[5.8125rem]"
+        isOpenSidebar ? "w-[18.4375rem]" : "w-[5.8125rem]"
       } text-center bg-white box-content flex flex-col justify-between pt-16`}
     >
       <ul
         className={`flex flex-col ${
-          openSidebar ? "items-start pl-10" : "items-center"
+          isOpenSidebar ? "items-start pl-10" : "items-center"
         } pt-6`}
       >
         {pagesProperties.map((element) => (
-          <li key={element.name}>
-            <Link href={element.name !== "My Voyage" ? `/${element.name.toLowerCase()}` : ""}>
-              <Button
-                title={element.name}
-                customClassName={`${buttonStyles} ${getButtonBackgroundStyle(
-                  element.name,
-                )} ${element.marginBottom}`}
-                onClick={() => handlePageClick(element)}
-              >
-                {element.icon}
-                {getButtonText(element.name)}
-              </Button>
-            </Link>
-          </li>
+          <PageButton
+            key={element.name}
+            element={element}
+            onClick={handlePageClick}
+            selectedButton={selectedButton}
+            isOpen={isOpenSidebar}
+          />
         ))}
-        {openSidebar && (
+        {isOpenSidebar && (
           <ul className="flex flex-col items-center">
-            {voyagePages.map((element) => (
-              <li key={element} className="h-7">
-                <Link href={"#"}>
-                  <Button
-                    title={element}
-                    customClassName={`bg-transparent hover:bg-transparent w-[9.375rem] h-[1.1875rem] min-h-0 mb-2.5 flex justify-start ${
-                      voyageStarted ? "pl-11" : "pl-6"
-                    } text-neutral-focus capitalize border-none relative`}
-                    onMouseEnter={() => setHoveredButton(element)}
-                    onMouseLeave={() => setHoveredButton(null)}
-                    onClick={() => voyageStarted && handlePageClick(element)}
-                  >
-                    {showIcon(element)}
-                    {element}
-                  </Button>
-                </Link>
-              </li>
+            {Object.values(VoyagePages).map((element) => (
+              <VoyagePageButton key={element} element={element} onClick={handlePageClick} hoveredButton={hoveredButton} selectedButton={selectedButton} isVoyageStarted={isVoyageStarted} setHoveredButton={setHoveredButton} />
             ))}
           </ul>
         )}
       </ul>
       <div className="flex-grow flex flex-col justify-end">
-        {openSidebar && (
-          <div className="w-[13.4375rem] h-[9.6875rem] bg-secondary-content rounded-2xl mx-auto px-6 py-4 mb-16">
-            <h3 className="text-black text-xl font-semibold text-left">
-              Voyage Status
-            </h3>
-            {voyageStarted ? (
-              Object.values(voyageData).map((element) => (
-                <p
-                  key={element}
-                  className="mt-[0.6875rem] h-[1.1875rem] text-black text-base font-medium text-left"
-                >
-                  {element}
-                </p>
-              ))
-            ) : (
-              <p className="mt-[0.6875rem] text-black text-base font-medium text-left">
-                Please join a voyage to see your status information.
-              </p>
-            )}
-          </div>
+        {isOpenSidebar && (
+          <VoyageStatus isVoyageStarted={isVoyageStarted} voyageData={voyageData}  />
         )}
       </div>
       <div className="flex flex-col items-end justify-start border-t border-secondary-focus h-20">
-        <Button
-          title="Expand"
-          customClassName="w-[3.125rem]
-             bg-white text-black capitalize hover:bg-white mr-2 border-none"
-          onClick={() => setOpenSidebar(!openSidebar)}
-        >
-          {openSidebar ? (
-            <ArrowLeftOnRectangleIcon className="h-6" />
-          ) : (
-            <ArrowRightOnRectangleIcon className="h-6" />
-          )}
-        </Button>
+        <ExpandButton isOpen={isOpenSidebar} onClick={setIsOpenSidebar} />
       </div>
     </aside>
   );
