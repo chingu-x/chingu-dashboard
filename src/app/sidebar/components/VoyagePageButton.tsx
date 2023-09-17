cff30f34d669789b42596a60ab191bc69d0a40ca
Timothy Russo <russotimothy@live.it>
2023-09-17 13:34:22 +0200

    feat: refactor the code in reusable components

---
+++ src/app/sidebar/components/VoyagePageButton.tsx
@@ -0,0 +1,56 @@
+import { ArrowRightCircleIcon } from "@heroicons/react/20/solid";
+import { LockClosedIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+import { Button } from "@/components";
+
+interface VoyagePageButtonProps {
+  element: string;
+  onClick: (element: string) => void;
+  hoveredButton: string | null;
+  selectedButton: string;
+  isVoyageStarted: boolean;
+  setHoveredButton: (element: string | null) => void;
+}
+
+export default function VoyagePageButton({
+  element,
+  onClick,
+  hoveredButton,
+  selectedButton,
+  isVoyageStarted,
+  setHoveredButton,
+}: VoyagePageButtonProps) {
+  const showIcon = (element: string) => {
+    if (!isVoyageStarted) {
+      return <LockClosedIcon className="h-[1.125rem]" />;
+    } else if (
+      isVoyageStarted &&
+      (hoveredButton === element || selectedButton === element)
+    ) {
+      return (
+        <div className="absolute left-[1.4rem]">
+          <ArrowRightCircleIcon className="h-[1.125rem] text-primary" />
+        </div>
+      );
+    }
+  };
+
+  return (
+    <li className="h-7">
+      <Link href={"#"}>
+        <Button
+          title={element}
+          customClassName={`bg-transparent hover:bg-transparent w-[9.375rem] h-[1.1875rem] min-h-0 mb-2.5 flex justify-start ${
+            isVoyageStarted ? "pl-11" : "pl-6"
+          } text-neutral-focus capitalize border-none relative`}
+          onMouseEnter={() => setHoveredButton(element)}
+          onMouseLeave={() => setHoveredButton(null)}
+          onClick={() => isVoyageStarted && onClick(element)}
+        >
+          {showIcon(element)}
+          {element}
+        </Button>
+      </Link>
+    </li>
+  );
+}
