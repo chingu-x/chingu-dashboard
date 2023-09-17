163786d46b6aacc2ad320b8402ff7a325bdc94cc
Timothy Russo <russotimothy@live.it>
2023-09-17 13:46:12 +0200

    style: add comment for mocked data

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -57,13 +57,14 @@ const pagesProperties: PageProperty[] = [
   },
 ];
 
+//-- Mocked fake data just for testing purpose --//
 const isVoyageStarted: boolean = true;
-
 const voyageData = {
   tier: "Tier 3",
   team: "Team",
   voyage: "V29",
 };
+//-- --//
 
 export default function Sidebar() {
   const [isOpenSidebar, setIsOpenSidebar] = useState<boolean>(true);
