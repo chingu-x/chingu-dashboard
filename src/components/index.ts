5e33c02f0f2e3d8cf7b34fe38ac85b3a892bed4e
Timothy Russo <russotimothy@live.it>
2023-09-17 13:42:50 +0200

    feat: move to the correct folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export { default as StoreProvider } from "./StoreProvider";
 export { default as Avatar } from "./Avatar";
 export { default as Navbar } from "./navbar/Navbar";
 export { default as Button } from "./Button";
+export { default as Sidebar } from "./sidebar/Sidebar";
+
