156b6afa35fbb7a4c9016d37064a5f70e623a9cc
Dan Ko <danko56666@gmail.com>
2023-09-18 19:47:01 -0400

    adjust styles to make main page content adjust with sidebar

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export { default as StoreProvider } from "./StoreProvider";
 export { default as Avatar } from "./Avatar";
 export { default as Button } from "./Button";
 export { default as Banner } from "./Banner";
+export { default as Sidebar } from "./sidebar/Sidebar";
 export * from "./navbar";
---
+++ src/components/sidebar/PageButton.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import { MainPages, PageProperty, VoyagePages } from "./Sidebar";
+import { Button } from "@/components";
+
+interface PageButtonProps {
+  element: PageProperty;
+  onClick: (element: string | PageProperty) => void;
+  selectedButton: string;
+  isOpen: boolean;
+}
+
+export default function PageButton({
+  element,
+  onClick,
+  selectedButton,
+  isOpen,
+}: PageButtonProps) {
+  const buttonStyles = `${
+    isOpen ? "w-[14.375rem] flex justify-start pl-6" : "w-[3.125rem]"
+  } h-[3.125rem] text-black capitalize border-none`;
+
+  const getButtonBackgroundStyle = (page: string) =>
+    selectedButton === page ||
+    ((page as MainPages) === MainPages.myVoyage &&
+      Object.values(VoyagePages).includes(selectedButton as VoyagePages))
+      ? "bg-neutral-content"
+      : "bg-white";
+
+  const getButtonText = (page: string) => (isOpen ? page : "");
+
+  return (
+    <li>
+      <Link
+        href={
+          element.name !== "My Voyage" ? `/${element.name.toLowerCase()}` : ""
+        }
+      >
+        <Button
+          title={element.name}
+          customClassName={`${buttonStyles} ${getButtonBackgroundStyle(
+            element.name,
+          )} ${element.marginBottom}`}
+          onClick={() => onClick(element)}
+        >
+          {element.icon}
+          {getButtonText(element.name)}
+        </Button>
+      </Link>
+    </li>
+  );
+}
