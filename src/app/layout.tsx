b668bbe13e71a7a703ff4c332fc94b6fa1e5aee1
Dan Ko <danko56666@gmail.com>
2023-09-18 20:38:57 -0400

    remove uncessary style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
           <Navbar />
           <StoreProvider>
             <Sidebar />
-            <main className="mt-16 flex-1 w-fit">{children}</main>
+            <main className="mt-16 flex-1">{children}</main>
           </StoreProvider>
         </div>
       </body>
