6eb30eb007921c150f4eb18dd1f9ee85da1dd36a
Timothy Russo <russotimothy@live.it>
2023-09-22 23:20:12 +0200

    feat: update banner dimensions

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -9,8 +9,8 @@ interface BannerProps {
 
 function Banner({ image, alt, title, description }: BannerProps) {
   return (
-    <div className="card w-full h-[320px] bg-accent-content flex flex-row px-20">
-      <Image src={image} alt={alt} width={454} height={300} priority={false} />
+    <div className="card w-full max-h-[320px] bg-accent-content flex flex-row px-20 py-10 box-border">
+      <Image src={image} alt={alt} width={454} height={200} priority={false} />
       <div className="flex flex-col justify-center pl-24 gap-y-3.5">
         <h3 className="text-3xl font-bold text-black">{title}</h3>
         <p className="text-lg font-medium text-black">{description}</p>
