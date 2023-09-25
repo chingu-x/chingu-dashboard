b5c7a22d1274eddb5779b7a7e280f4cde4a1086c
Timothy Russo <russotimothy@live.it>
2023-09-25 16:01:37 +0200

    feat: correct prettier errors

--- src/app/tech-stack/components/TechStackContainer.tsx
+++ src/app/tech-stack/components/TechStackContainer.tsx
@@ -5,9 +5,7 @@ export default function TechStackContainer() {
     <div className="card bg-primary-content p-10 w-full">
       <ul className="grid grid-cols-2 min-[1920px]:grid-cols-3 gap-10 place-items-center">
         {Object.keys(techStack).map((cardType) => (
-          <li
-            key={cardType}
-          >
+          <li key={cardType}>
             <TechStackCard
               title={cardType}
               data={techStack[cardType as keyof typeof techStack]}
