61ae65139e8cc340027e10d719c3cb7e0d543166
Jane Moroz <Moroz.jane@yahoo.com>
2023-10-01 18:14:29 +0300

    feat: add 'react-hook-form' and 'zod' example.

--- src/components/inputs/TextInput.tsx
+++ src/components/inputs/TextInput.tsx
@@ -1,33 +1,42 @@
 "use client";
 
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
 interface TextInputProps {
   id: string;
   label?: string;
   placeholder: string;
-  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
 }
 
 export default function TextInput({
   id,
   label,
   placeholder,
-  required,
+  register,
+  errors,
 }: TextInputProps) {
   return (
     <div className="w-full form-control">
-      {label && (
-        <label htmlFor={id} className="p-0 label">
+      <label htmlFor={id} className="p-0 label">
+        {label && (
           <span className="text-base font-medium capitalize label-text text-base-300">
             {label}
           </span>
-        </label>
-      )}
+        )}
+        <span className="h-6 label-text-alt text-error">
+          {errors[id]?.message as string}
+        </span>
+      </label>
       <input
         id={id}
         type="text"
         placeholder={placeholder}
-        required={required}
-        className="w-full my-2 text-base shadow-sm input input-bordered bg-base-200 text-neutral-focus border-neutral/40 focus-visible:ring-0 focus-visible:bg-base-200"
+        {...register(id)}
+        className={`w-full my-2 text-base shadow-sm input input-bordered bg-base-200 text-neutral-focus border-neutral/40 focus-visible:ring-0 focus-visible:bg-base-200 ${
+          errors[id] ? "border-error" : "border-neutral/40"
+        }`}
       />
     </div>
   );
--- src/components/modals/Example2Modal.tsx
+++ src/components/modals/Example2Modal.tsx
@@ -1,20 +1,60 @@
 "use client";
 
+import { useCallback } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import Modal from "./Modal";
 import { TextInput } from "@/components/inputs";
 
+import { useAppDispatch, useAppSelector } from "@/store";
+import { onClose } from "@/store/features/modal";
+
+const formSchema = z.object({
+  suggestion: z.string().min(1, {
+    message: "This field is required.",
+  }),
+});
+
 export default function Example2Modal() {
+  const { isOpen, type } = useAppSelector((state) => state.modal);
+  const dispatch = useAppDispatch();
+
+  const isModalOpen = isOpen && type === "example2";
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+  };
+
+  const handleClose = useCallback(() => {
+    reset();
+    dispatch(onClose());
+  }, [dispatch]);
+
   return (
     <Modal
+      isOpen={isModalOpen}
       title="add feature"
-      modalType="example2"
       primaryActionLabel="submit"
-      onSubmit={() => {}}
+      onSubmit={handleSubmit(onSubmit)}
+      onClose={handleClose}
     >
       <div className="flex flex-col gap-6">
         <TextInput
           id="suggestion"
           placeholder="What is your tech stack suggestion?"
+          register={register}
+          errors={errors}
         />
       </div>
     </Modal>
