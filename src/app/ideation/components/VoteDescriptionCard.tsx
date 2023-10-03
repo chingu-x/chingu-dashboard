c8a3ad77c7a407dc8001122e4deb2db68e465f5d
Timothy Russo <russotimothy@live.it>
2023-10-03 15:17:58 +0200

    feat: responsive heigth for vote description card

--- src/app/ideation/components/VoteDescriptionCard.tsx
+++ src/app/ideation/components/VoteDescriptionCard.tsx
@@ -1,6 +1,6 @@
 function VoteDescriptionCard({ className }: { className?: string }) {
   return (
-    <div className={`card max-w-[200px] w-full max-[1919px]:max-w-[160px] h-28 bg-primary-content rounded-lg ${className}`}>
+    <div className={`card max-w-[200px] w-full max-[1919px]:max-w-[160px] h-28 max-[1919px]:h-32 bg-primary-content rounded-lg ${className}`}>
       <section className="flex flex-col items-start p-4 gap-y-4">
         <h2 className="text-xl font-semibold text-base-300 leading-[24px]">Votes</h2>
         <p className="text-base font-medium text-base-300 text-left leading-[19px]">
