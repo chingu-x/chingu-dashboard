0a6de5b3c591c069dd131f4584670f8bbe954b71
Timothy Russo <russotimothy@live.it>
2023-10-03 17:11:54 +0200

    feat: responsive contribution card

--- src/app/ideation/components/VoteCard.tsx
+++ src/app/ideation/components/VoteCard.tsx
@@ -3,11 +3,12 @@ import { Avatar, Button } from "@/components";
 interface VoteCardProps {
   users: string[];
   voted: boolean;
+  className?: string;
 }
 
-function VoteCard({ users, voted }: VoteCardProps) {
+function VoteCard({ users, voted, className }: VoteCardProps) {
   return (
-    <div className="card max-w-[200px] max-[1919px]:max-w-[160px] w-full h-fit bg-primary-content rounded-lg">
+    <div className={`card max-w-[200px] max-[1919px]:max-w-[160px] w-full h-fit bg-primary-content rounded-lg ${className}`}>
       <section className="flex flex-col items-start p-4 gap-y-4">
         <h1 className="text-3xl font-semibold text-base-300">{users.length}</h1>
         <h2 className="text-xl font-semibold text-base-300">{`Vote${
@@ -20,7 +21,7 @@ function VoteCard({ users, voted }: VoteCardProps) {
         </div>
         <Button
           title="Vote"
-          customClassName="w-full btn-primary text-base-300 disabled:bg-primary-focus disabled:text-base-200"
+          customClassName="w-full btn-primary text-base-300 disabled:bg-primary-focus disabled:text-base-200 capitalize"
           disabled={voted}
         >
           {voted ? "Voted" : "Vote"}
