e3dd6e068c57e0a9de011a5096602ba3a8cd5782
Timothy Russo <russotimothy@live.it>
2023-10-06 09:20:02 +0200

    feat: update tech stack banner layout

--- src/app/tech-stack/page.tsx
+++ src/app/tech-stack/page.tsx
@@ -5,7 +5,7 @@ export default function TeckStackPage() {
   return (
     <>
       <Banner
-        image="/img/tech_stack_banner.png"
+        image="/img/tech_stack_banner.svg"
         alt="teck_stack_banner"
         title="Tech Stack"
         description="Alright, let's get down to business. We need to figure out which tech stack we're going to use to power this bad boy. Are you a JavaScript junkie, a Python pro, a Java genius, or a Ruby rockstar? Let's vote"
