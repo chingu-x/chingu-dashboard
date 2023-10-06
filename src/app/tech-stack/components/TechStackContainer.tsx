7bd36f5994d55a05abe38dac0066d30e97a53089
Timothy Russo <russotimothy@live.it>
2023-10-06 11:37:20 +0200

    feat: update colors

--- src/app/tech-stack/components/TechStackContainer.tsx
+++ src/app/tech-stack/components/TechStackContainer.tsx
@@ -2,7 +2,7 @@ import { TechStackCard, techStack } from ".";
 
 export default function TechStackContainer() {
   return (
-    <div className="card bg-primary-content p-10 w-full">
+    <div className="card bg-secondary-content p-10 w-full">
       <ul className="grid grid-cols-2 min-[1920px]:grid-cols-3 gap-10 place-items-center">
         {Object.keys(techStack).map((cardType) => (
           <li key={cardType}>
