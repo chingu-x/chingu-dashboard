66c2c2714c9f541b7e9eccf6046488e64250d94e
Timothy Russo <russotimothy@live.it>
2023-10-06 15:08:42 +0200

    feat: max width for the baner and the tech stach container

--- src/app/tech-stack/components/TechStackContainer.tsx
+++ src/app/tech-stack/components/TechStackContainer.tsx
@@ -2,7 +2,7 @@ import { TechStackCard, techStack } from ".";
 
 export default function TechStackContainer() {
   return (
-    <div className="card bg-secondary-content p-10 w-full">
+    <div className="card bg-secondary-content p-10 max-w-[1353px]">
       <ul className="grid grid-cols-2 min-[1920px]:grid-cols-3 gap-10 place-items-center">
         {Object.keys(techStack).map((cardType) => (
           <li key={cardType}>
