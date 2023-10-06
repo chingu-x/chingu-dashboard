85f69aeadef15bf6d12dc1fc03ce86fc011977bb
Timothy Russo <russotimothy@live.it>
2023-10-06 12:25:18 +0200

    feat: add vote button

--- src/app/tech-stack/components/TechStackCard.tsx
+++ src/app/tech-stack/components/TechStackCard.tsx
@@ -32,12 +32,18 @@ export default function TechStackCard({ title, data }: TechStackCardProps) {
                   />
                 ))}
               </div>
+              <Button
+                title={title}
+                customClassName="capitalize w-[62px] h-[32px] p-0 min-h-full text-xs font-medium text-base-300 bg-primary-content border-transparent mr-4 rounded-[32px]"
+              >
+                Vote
+              </Button>
             </li>
           ))}
         </ul>
       </div>
       <Button
-        title={`edit ${title}`}
+        title={`add ${title}`}
         customClassName="mt-6 capitalize w-full h-[42px] p-0 min-h-full text-xs font-medium text-base-300 bg-secondary border-transparent flex justify-start pl-5 items-center"
       >
         <PlusCircleIcon className="h-[18px] w-[18px] text-base-300" />
