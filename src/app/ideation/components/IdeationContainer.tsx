50d078659e6965391e22413c92ea4e3ea5339a14
Timothy Russo <russotimothy@live.it>
2023-10-06 11:25:59 +0200

    feat: fix prettier formatting

--- src/app/ideation/components/IdeationContainer.tsx
+++ src/app/ideation/components/IdeationContainer.tsx
@@ -15,7 +15,11 @@ function IdeationContainer({
         <VoteCard users={users} voted={voted} />
         <ContributionCard own_idea={own_idea} contributed_by={contributed_by} />
       </div>
-      <VoteCard users={users} voted={voted} className="hidden min-[1920px]:block" />
+      <VoteCard
+        users={users}
+        voted={voted}
+        className="hidden min-[1920px]:block"
+      />
       <section className="card-body gap-y-7 p-0 w-[1000px] h-[377px] overflow-y-auto pr-5">
         <h2 className="text-xl font-semibold text-base-300">{title}</h2>
         <h3 className="text-base text-neutral-focus font-semibold">
@@ -29,7 +33,11 @@ function IdeationContainer({
           {vision_statement}
         </p>
       </section>
-      <ContributionCard own_idea={own_idea} contributed_by={contributed_by} className="hidden min-[1920px]:block" />
+      <ContributionCard
+        own_idea={own_idea}
+        contributed_by={contributed_by}
+        className="hidden min-[1920px]:block"
+      />
     </div>
   );
 }
