ff0a4882bac42c2aa64cf5c4811506f544095dbd
Jane Moroz <Moroz.jane@yahoo.com>
2023-10-12 13:50:39 +0300

    refactor: refactor base modal

--- src/components/modals/Example1Modal.tsx
+++ src/components/modals/Example1Modal.tsx
@@ -4,7 +4,9 @@ import { useCallback } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { TrashIcon } from "@heroicons/react/20/solid";
 
+import Button from "@/components/Button";
 import Modal from "@/components/modals/Modal";
 import { TextInput, Textarea } from "@/components/inputs";
 
@@ -61,45 +63,64 @@ export default function Example1Modal() {
   }, [dispatch]);
 
   return (
-    <Modal
-      isOpen={isModalOpen}
-      title="create project"
-      primaryActionLabel="submit"
-      secondaryAction={() => {}}
-      secondaryActionLabel="delete project"
-      onSubmit={handleSubmit(onSubmit)}
-      onClose={handleClose}
-    >
-      <div className="flex flex-col gap-4">
-        <TextInput
-          id="title"
-          label="title"
-          placeholder="Enter you voyage project idea"
-          register={{ ...register("title") }}
-          errors={errors}
-        />
-        <Textarea
-          id="projectIdea"
-          label="project idea"
-          placeholder="Describe your idea. What problem or challenge do you aim to address or solve? What is the primary purpose and goal of your idea? Who are your intemded users?"
-          register={{ ...register("projectIdea") }}
-          errors={errors}
-        />
-        <Textarea
-          id="visionStatement"
-          label="vision statement"
-          placeholder="Share your insoiring vision. How will you provide value and benefits to users? What long term impact do you hope to achieve?"
-          register={{ ...register("visionStatement") }}
-          errors={errors}
-        />
-        <TextInput
-          id="email"
-          label="email"
-          placeholder="Enter your email"
-          register={register("email")}
-          errors={errors}
-        />
-      </div>
+    <Modal isOpen={isModalOpen} title="create project" onClose={handleClose}>
+      {/* FORM */}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col overflow-hidden"
+      >
+        {/* BODY WITH VERTICAL SCROLL */}
+        <div className="flex flex-col pr-2 mr-1 overflow-y-auto min-h-[90px]">
+          <div className="flex flex-col gap-4">
+            <TextInput
+              id="title"
+              label="title"
+              placeholder="Enter you voyage project idea"
+              register={{ ...register("title") }}
+              errors={errors}
+            />
+            <Textarea
+              id="projectIdea"
+              label="project idea"
+              placeholder="Describe your idea. What problem or challenge do you aim to address or solve? What is the primary purpose and goal of your idea? Who are your intemded users?"
+              register={{ ...register("projectIdea") }}
+              errors={errors}
+            />
+            <Textarea
+              id="visionStatement"
+              label="vision statement"
+              placeholder="Share your insoiring vision. How will you provide value and benefits to users? What long term impact do you hope to achieve?"
+              register={{ ...register("visionStatement") }}
+              errors={errors}
+            />
+            <TextInput
+              id="email"
+              label="email"
+              placeholder="Enter your email"
+              register={register("email")}
+              errors={errors}
+            />
+          </div>
+        </div>
+        {/* BUTTONS */}
+        <div className="flex flex-col gap-5 pt-8">
+          <Button
+            type="submit"
+            title="submit"
+            customClassName="text-base gap-x-0 border-none font-semibold capitalize bg-primary text-base-300 hover:bg-primary-focus"
+          >
+            Submit
+          </Button>
+          <Button
+            onClick={() => {}}
+            title="delete"
+            customClassName="text-base border-none font-semibold capitalize bg-error-content text-base-300 hover:bg-error"
+          >
+            <TrashIcon className="w-4 h-4" />
+            Delete
+          </Button>
+        </div>
+      </form>
     </Modal>
   );
 }
