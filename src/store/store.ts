03745bcb2a7e2b3e6d9d54659948e764f3b32966
Dan Ko <danko56666@gmail.com>
2023-10-18 17:49:34 -0400

    remove counter slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./features/counter/counterSlice";
 import modalReducer from "./features/modal/modalSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     modal: modalReducer,
   },
 });
