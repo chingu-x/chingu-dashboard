41c67c83058ddc7f474af2021e0d5381c5873e26
Jane Moroz <Moroz.jane@yahoo.com>
2023-10-23 17:45:29 +0300

    for testing

--- src/app/directory/page.tsx
+++ src/app/directory/page.tsx
@@ -1,25 +1,12 @@
+import { getMembers } from "@/api/routes";
 import TeamDirectory from "./components/TeamDirectory";
 import { TeamMember } from "./components/fixtures/MyTeam";
 import Banner from "@/components/banner/Banner";
 
-async function getMembers(teamId: number) {
-  // Get team members
-  // TODO: need team members ids in the res
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}teams/${teamId}/members`
-  );
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
 async function DirectoryPage() {
   // Get current user logic. For now assume we get current user's object which has user's id and team's id back from some api call, or cookie or etc
   const currentUser = {
-    id: "",
+    id: "b52046f9-db9c-4373-950a-ce8c861307ab",
     teamId: 9,
   };
 
