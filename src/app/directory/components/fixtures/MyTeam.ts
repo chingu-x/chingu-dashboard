f211fe073e4216100c176748b556c18c84bbe19e
Jane Moroz <Moroz.jane@yahoo.com>
2023-10-23 14:58:26 +0300

    fix: change TeamMember interface

--- src/app/directory/components/fixtures/MyTeam.ts
+++ src/app/directory/components/fixtures/MyTeam.ts
@@ -1,53 +1,70 @@
 export interface TeamMember {
-  id: string;
-  name: string;
-  discordId: string;
-  averageHour: number;
-  location: string;
-  timeZone: string;
-  email: string;
-  position: string;
+  member: {
+    firstName: string;
+    lastName: string;
+    avatar: string;
+    discordId: string;
+    countryCode: string;
+    timezone: string;
+    email: string;
+  };
+  hrPerSprint: number;
+  voyageRole: { name: string };
 }
 
 export const teamMembers: TeamMember[] = [
   {
-    id: "1",
-    name: "Danney Trieu",
-    discordId: "danneytrieuwork#2558",
-    averageHour: 0,
-    location: "Denver, CO, USA",
-    timeZone: "MDT",
-    email: "danney@gmail.com",
-    position: "Product Owner",
+    member: {
+      firstName: "Danney",
+      lastName: "Trieu",
+      avatar: "",
+      discordId: "danneytrieuwork#2558",
+      countryCode: "US",
+      timezone: "America/Los_Angeles",
+      email: "danney@gmail.com",
+    },
+    hrPerSprint: 12,
+    voyageRole: { name: "Product Owner" },
   },
   {
-    id: "2",
-    name: "Jane Morez",
-    discordId: "Jan_morez#2341",
-    averageHour: 0,
-    location: "Las Vegas, NY, USA",
-    timeZone: "PDT",
-    email: "jane@gmail.com",
-    position: "Back-end Developer",
+    member: {
+      firstName: "Jane",
+      lastName: "Morez",
+      avatar: "",
+      discordId: "Jan_morez#2341",
+      countryCode: "US",
+      timezone: "America/Los_Angeles",
+      email: "jane@gmail.com",
+    },
+    hrPerSprint: 9,
+    voyageRole: { name: "Back-end Developer" },
   },
+
   {
-    id: "3",
-    name: "Kayla Montre",
-    discordId: "KaylaMon#5678",
-    averageHour: 12,
-    location: "Las Vegas, NY, USA",
-    timeZone: "PDT",
-    email: "kayla@gmail.com",
-    position: "UX/UI Designer",
+    member: {
+      firstName: "Kayla",
+      lastName: "Montre",
+      avatar: "",
+      discordId: "KaylaMon#5678",
+      countryCode: "US",
+      timezone: "America/Las_Vegas",
+      email: "kayla@gmail.com",
+    },
+    hrPerSprint: 12,
+    voyageRole: { name: "UX/UI Designer" },
   },
+
   {
-    id: "4",
-    name: "Jackson Pez",
-    discordId: "jackson#2558",
-    averageHour: 10,
-    location: "Denver, CO, USA",
-    timeZone: "MDT",
-    email: "jackson@gmail.com",
-    position: "Front-end Developer",
+    member: {
+      firstName: "Jackson",
+      lastName: "Pez",
+      avatar: "",
+      discordId: "jackson#2558",
+      countryCode: "US",
+      timezone: "America/Los_Angeles",
+      email: "jackson@gmail.com",
+    },
+    hrPerSprint: 12,
+    voyageRole: { name: "Front-end Developer" },
   },
 ];
