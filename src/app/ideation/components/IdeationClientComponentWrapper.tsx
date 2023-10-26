f964a9d0e1977fc518ce81e687111d9170c6828d
Dan Ko <danko56666@gmail.com>
2023-10-26 19:11:22 -0400

    add ideation modal

---
+++ src/app/ideation/components/IdeationClientComponentWrapper.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import CreateIdeationContainer from "./CreateIdeationContainer";
+import IdeationContainer from "./IdeationContainer";
+import { ideation } from "./fixtures/ideation";
+
+function IdeationClientComponentWrapper() {
+  return (
+    <>
+      <CreateIdeationContainer />
+      {ideation.map((i) => (
+        <IdeationContainer
+          key={i.id}
+          title={i.title}
+          project_idea={i.project_idea}
+          vision_statement={i.vision_statement}
+          users={i.users}
+          voted={i.voted}
+          own_idea={i.own_idea}
+          contributed_by={i.contributed_by}
+        />
+      ))}
+    </>
+  );
+}
+
+export default IdeationClientComponentWrapper;
