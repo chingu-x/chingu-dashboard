97e63e38adacade7456eb643fd172323ea014928
Timothy Russo <russotimothy@live.it>
2023-10-26 18:03:03 +0200

    feat: update modal slice to accept data for the TechStackModal

--- src/store/features/modal/modalSlice.ts
+++ src/store/features/modal/modalSlice.ts
@@ -2,13 +2,19 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 export type ModalType = "example1" | "example2" | "TechStackModal";
 
+interface TechStackModalData {
+  techName: string;
+}
+
 interface ModalState {
   type: ModalType | null;
+  data: TechStackModalData | null;
   isOpen: boolean;
 }
 
 const initialState: ModalState = {
   type: null,
+  data: null,
   isOpen: false,
 };
 
@@ -16,13 +22,23 @@ export const modalSlice = createSlice({
   name: "modal",
   initialState,
   reducers: {
-    onOpen: (state, action: PayloadAction<{ type: ModalType }>) => {
+    onOpen: (
+      state,
+      action: PayloadAction<{ type: ModalType; data?: TechStackModalData }>,
+    ) => {
       state.isOpen = true;
       state.type = action.payload.type;
+
+      if (action.payload.type === "TechStackModal" && action.payload.data) {
+        state.data = action.payload.data;
+      } else {
+        state.data = null;
+      }
     },
     onClose: (state) => {
       state.isOpen = false;
       state.type = null;
+      state.data = null;
     },
   },
 });
