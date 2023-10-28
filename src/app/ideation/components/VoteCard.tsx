9a82301e37ffdebb0b05bb81c62e58e06c4822c4
Dan Ko <danko56666@gmail.com>
2023-10-27 21:27:37 -0400

    render vote or voted text based on current user

--- src/app/ideation/components/VoteCard.tsx
+++ src/app/ideation/components/VoteCard.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Avatar from "@/components/Avatar";
 import Button from "@/components/Button";
 import { ProjectIdeaVotes } from "@/store/features/ideation/ideationSlice";
+import { useCallback, useEffect, useState } from "react";
+
+const USERID = "be650b1e-078a-4ebd-ad07-6a368d5f250a";
 
 interface VoteCardProps {
   users: ProjectIdeaVotes[];
@@ -8,6 +13,22 @@ interface VoteCardProps {
 }
 
 function VoteCard({ users, className }: VoteCardProps) {
+  const [currentUserVoted, setCurrentUserVoted] = useState(false);
+
+  const getVoteUsers = useCallback(
+    () => users.map((user) => user.votedBy.member.id),
+    [users],
+  );
+
+  useEffect(() => {
+    if (getVoteUsers().includes(USERID) === true) {
+      setCurrentUserVoted(true);
+    } else {
+      setCurrentUserVoted(false);
+    }
+
+  }, [getVoteUsers]);
+
   return (
     <div
       className={`card max-w-[200px] max-[1919px]:min-w-[160px] w-full h-fit bg-primary-content rounded-lg ${className}`}
@@ -25,8 +46,10 @@ function VoteCard({ users, className }: VoteCardProps) {
         <Button
           title="Vote"
           customClassName="w-full btn-primary text-base-300 disabled:bg-primary-focus disabled:text-base-200 capitalize"
+          disabled={getVoteUsers().includes(USERID)}
+          onClick={() => {}}
         >
-          {"Vote"}
+          {currentUserVoted ? "Voted" : "Vote"}
         </Button>
       </section>
     </div>
