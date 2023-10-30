98edb6aa680faa332ce31d263391998402dc4d80
Timothy Russo <russotimothy@live.it>
2023-10-30 11:49:05 +0100

    feat: revert modalSlice to its original state

--- src/store/features/modal/modalSlice.ts
+++ src/store/features/modal/modalSlice.ts
@@ -2,19 +2,13 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 export type ModalType = "example1" | "example2" | "TechStackModal";
 
-interface TechStackModalData {
-  techName: string;
-}
-
 interface ModalState {
   type: ModalType | null;
-  data: TechStackModalData | null;
   isOpen: boolean;
 }
 
 const initialState: ModalState = {
   type: null,
-  data: null,
   isOpen: false,
 };
 
@@ -22,23 +16,13 @@ export const modalSlice = createSlice({
   name: "modal",
   initialState,
   reducers: {
-    onOpen: (
-      state,
-      action: PayloadAction<{ type: ModalType; data?: TechStackModalData }>,
-    ) => {
+    onOpen: (state, action: PayloadAction<{ type: ModalType }>) => {
       state.isOpen = true;
       state.type = action.payload.type;
-
-      if (action.payload.type === "TechStackModal" && action.payload.data) {
-        state.data = action.payload.data;
-      } else {
-        state.data = null;
-      }
     },
     onClose: (state) => {
       state.isOpen = false;
       state.type = null;
-      state.data = null;
     },
   },
 });
