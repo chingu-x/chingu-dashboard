f642e203cbe9111b238b12584656bbaecc217e4a
Dan Ko <danko56666@gmail.com>
2023-10-31 01:55:46 -0400

    add loading state

--- src/app/ideation/components/IdeationClientComponentWrapper.tsx
+++ src/app/ideation/components/IdeationClientComponentWrapper.tsx
@@ -1,14 +1,32 @@
-import { Suspense } from "react";
+"use client";
+
+import { useEffect, useState } from "react";
 import IdeationContainer from "./IdeationContainer";
 import { store } from "@/store/store";
+import { IdeationData } from "@/store/features/ideation/ideationSlice";
 // import { ideation } from "./fixtures/ideation";
 
 function IdeationClientComponentWrapper() {
-  const { projectIdeas } = store.getState().ideation;
+  const [projectIdeas, setProjectIdeas] = useState<IdeationData[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  console.log(loading);
+
+  useEffect(() => {
+    setLoading(true);
+    const { projectIdeas } = store.getState().ideation;
+
+    setProjectIdeas(projectIdeas);
+
+    setLoading(false);
+    
+  }, []);
+  
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      {projectIdeas.map((projectIdea) => (
+    <>
+      {loading === true && <div>Loading...</div>}
+      {loading === false && projectIdeas.map((projectIdea) => (
         <IdeationContainer
           key={projectIdea.id}
           title={projectIdea.title}
@@ -18,7 +36,7 @@ function IdeationClientComponentWrapper() {
           contributed_by={projectIdea.contributedBy}
         />
       ))}
-    </Suspense>
+    </>
   );
 }
 
