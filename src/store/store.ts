6cf2eb259fe073118c4a162bb5c9826b3024c898
Timothy Russo <russotimothy@live.it>
2023-11-02 18:26:20 +0100

    fix: lint errors

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice";
 import modalReducer from "./features/modal/modalSlice";
-import userReducer from "./features/user/userSlice";
 import techStackReducer from "./features/techStack/techStackSlice";
 import toastReducer from "./features/toast/toastSlice";
 
@@ -9,7 +8,6 @@ export const store = configureStore({
   reducer: {
     counter: counterReducer,
     modal: modalReducer,
-    user: userReducer,
     techStack: techStackReducer,
     toast: toastReducer,
   },
