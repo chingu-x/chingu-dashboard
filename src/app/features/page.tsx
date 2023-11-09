450127c9a04548f2756b8ecc76eef0ce57d38aab
Jane Moroz <Moroz.jane@yahoo.com>
2023-11-09 16:16:31 +0300

    feat: add banner

---
+++ src/app/features/page.tsx
@@ -0,0 +1,17 @@
+import Banner from "@/components/banner/Banner";
+
+function FeaturesPage() {
+  return (
+    <>
+      <Banner
+        imageLight="/img/features_banner_light.png"
+        imageDark="/img/features_banner_dark.png"
+        alt="features_banner"
+        title="Features"
+        description="What's on the feature menu for our app? We want only the crème de la crème, so prioritize wisely. Remember, we're building an app, not a buffet."
+      />
+    </>
+  );
+}
+
+export default FeaturesPage;
