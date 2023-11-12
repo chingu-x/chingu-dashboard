4e0ab34fb3e943524850c2e4cc7b4a76994d1e46
Jane Moroz <Moroz.jane@yahoo.com>
2023-11-12 15:04:51 +0300

    feat: add featureCardItem

--- src/app/features/components/Features.tsx
+++ src/app/features/components/Features.tsx
@@ -1,11 +1,42 @@
 import FeatureCard from "./FeatureCard";
+import { Feature } from "./fixtures/Features";
 
-export default function Features() {
+interface FeaturesProps {
+  features: Feature[];
+  currentUser: {
+    id: string;
+    teamId: number;
+  };
+}
+
+export default function Features({ features, currentUser }: FeaturesProps) {
+  // TODO: change to feature.category.id maybe if they don't change or leave like this
+  const mustHave = features.filter(
+    (feature) => feature.category.name === "must have",
+  );
+  const shouldHave = features.filter(
+    (feature) => feature.category.name === "should have",
+  );
+  const niceToHave = features.filter(
+    (feature) => feature.category.name === "nice to have",
+  );
   return (
-    <div className="flex gap-x-10">
-      <FeatureCard title="must have" />
-      <FeatureCard title="should have" />
-      <FeatureCard title="nice to have" />
+    <div className="flex items-start gap-x-10">
+      <FeatureCard
+        title="must have"
+        features={mustHave}
+        currentUser={currentUser}
+      />
+      <FeatureCard
+        title="should have"
+        features={shouldHave}
+        currentUser={currentUser}
+      />
+      <FeatureCard
+        title="nice to have"
+        features={niceToHave}
+        currentUser={currentUser}
+      />
     </div>
   );
 }
