d1ed698a31ed6938ae37c20ec3d5c543241860c7
Jane Moroz <Moroz.jane@yahoo.com>
2023-11-13 12:54:28 +0300

    feat: add scrollable droppable area + droppable's bg change according to events

--- src/app/features/components/fixtures/Features.ts
+++ src/app/features/components/fixtures/Features.ts
@@ -125,4 +125,148 @@ export const features: Feature[] = [
       },
     },
   },
+  {
+    id: 55,
+    description: "Charts and Graphs",
+    teamMemberId: 34,
+    category: {
+      id: 26,
+      name: "should have",
+    },
+    addedBy: {
+      member: {
+        id: "3eca1a73-01aa-448c-8410-dce17ff08938",
+        avatar:
+          "https://gravatar.com/avatar/3bfaef00e02a22f99e17c66e7a9fdd31?s=400&d=robohash&r=x",
+        firstName: "Jessica",
+        lastName: "Williamson",
+      },
+    },
+  },
+  {
+    id: 56,
+    description: "Chat",
+    teamMemberId: 34,
+    category: {
+      id: 26,
+      name: "should have",
+    },
+    addedBy: {
+      member: {
+        id: "dff75faa-1f39-446d-a60c-9589e5ead90f",
+        avatar:
+          "https://gravatar.com/avatar/3bfaef00e02a22f99e17c66e7a9fdd31?s=400&d=identicon&r=x",
+        firstName: "Leonarda",
+        lastName: "Rowe",
+      },
+    },
+  },
+  {
+    id: 57,
+    description: "Share images",
+    teamMemberId: 34,
+    category: {
+      id: 26,
+      name: "should have",
+    },
+    addedBy: {
+      member: {
+        id: "3eca1a73-01aa-448c-8410-dce17ff08938",
+        avatar:
+          "https://gravatar.com/avatar/3bfaef00e02a22f99e17c66e7a9fdd31?s=400&d=robohash&r=x",
+        firstName: "Jessica",
+        lastName: "Williamson",
+      },
+    },
+  },
+  {
+    id: 58,
+    description: "Share PDF",
+    teamMemberId: 36,
+    category: {
+      id: 27,
+      name: "nice to have",
+    },
+    addedBy: {
+      member: {
+        id: "dff75faa-1f39-446d-a60c-9589e5ead90f",
+        avatar:
+          "https://gravatar.com/avatar/3bfaef00e02a22f99e17c66e7a9fdd31?s=400&d=identicon&r=x",
+        firstName: "Leonarda",
+        lastName: "Rowe",
+      },
+    },
+  },
+  {
+    id: 59,
+    description: "Upload files",
+    teamMemberId: 36,
+    category: {
+      id: 27,
+      name: "nice to have",
+    },
+    addedBy: {
+      member: {
+        id: "dff75faa-1f39-446d-a60c-9589e5ead90f",
+        avatar:
+          "https://gravatar.com/avatar/3bfaef00e02a22f99e17c66e7a9fdd31?s=400&d=identicon&r=x",
+        firstName: "Leonarda",
+        lastName: "Rowe",
+      },
+    },
+  },
+  {
+    id: 60,
+    description: "Notifications",
+    teamMemberId: 36,
+    category: {
+      id: 26,
+      name: "should have",
+    },
+    addedBy: {
+      member: {
+        id: "dff75faa-1f39-446d-a60c-9589e5ead90f",
+        avatar:
+          "https://gravatar.com/avatar/3bfaef00e02a22f99e17c66e7a9fdd31?s=400&d=identicon&r=x",
+        firstName: "Leonarda",
+        lastName: "Rowe",
+      },
+    },
+  },
+  {
+    id: 61,
+    description: "Mobile app",
+    teamMemberId: 36,
+    category: {
+      id: 26,
+      name: "should have",
+    },
+    addedBy: {
+      member: {
+        id: "dff75faa-1f39-446d-a60c-9589e5ead90f",
+        avatar:
+          "https://gravatar.com/avatar/3bfaef00e02a22f99e17c66e7a9fdd31?s=400&d=identicon&r=x",
+        firstName: "Leonarda",
+        lastName: "Rowe",
+      },
+    },
+  },
+  {
+    id: 62,
+    description: "Stripe integration",
+    teamMemberId: 36,
+    category: {
+      id: 25,
+      name: "must have",
+    },
+    addedBy: {
+      member: {
+        id: "dff75faa-1f39-446d-a60c-9589e5ead90f",
+        avatar:
+          "https://gravatar.com/avatar/3bfaef00e02a22f99e17c66e7a9fdd31?s=400&d=identicon&r=x",
+        firstName: "Leonarda",
+        lastName: "Rowe",
+      },
+    },
+  },
 ];
