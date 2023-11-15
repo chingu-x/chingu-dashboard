400f7e939a397c91d3036c0bdecd942af8fe195f
Jane Moroz <Moroz.jane@yahoo.com>
2023-11-15 16:45:17 +0300

    refactor: remove console.log

--- src/app/features/components/FeaturesContainer.tsx
+++ src/app/features/components/FeaturesContainer.tsx
@@ -49,10 +49,10 @@ export default function FeaturesContainer({
 
     // source and destination lists
     const sourceList = newOrderedData.find(
-      (list) => list.categoryId.toString() === source.droppableId,
+      (list) => list.categoryId.toString() === source.droppableId
     );
     const destList = newOrderedData.find(
-      (list) => list.categoryId.toString() === destination.droppableId,
+      (list) => list.categoryId.toString() === destination.droppableId
     );
 
     if (!sourceList || !destList) {
@@ -65,7 +65,6 @@ export default function FeaturesContainer({
       const reorderedCards = [...sourceList.features];
       const [removed] = reorderedCards.splice(source.index, 1);
       reorderedCards.splice(destination.index, 0, removed);
-      console.log(reorderedCards);
 
       reorderedCards.forEach((card, idx) => {
         card.order = idx;
