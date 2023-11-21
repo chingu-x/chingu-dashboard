60381d426cd9ccc7abb07d63b4c252237000b655
Dan Ko <danko56666@gmail.com>
2023-11-21 15:08:00 -0500

    add auth slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice";
 import modalReducer from "./features/modal/modalSlice";
 import toastReducer from "./features/toast/toastSlice";
+import authReducer from "./features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     modal: modalReducer,
     toast: toastReducer,
+    auth: authReducer,
   },
 });
 
