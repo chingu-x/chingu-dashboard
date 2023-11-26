c4c1ddd5c8bbdde67d0b9cbd3b2aecfbe0f8f794
timothyrusso <russotimothy@live.it>
2023-11-26 13:19:55 +0100

    feat: update width of the login page elements

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,40 +1,43 @@
-"use client";
+'use client';
 
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Component() {
-
   return (
     <div className="flex items-center">
       <div className="flex min-h-[486px] flex-col justify-center">
         <div
           data-hide-on-theme="dark"
-          className="h-[300px] w-[628px] w-full relative shrink-0"
+          className="h-[300px] max-w-[628px] relative shrink-0"
         >
           <Image
             src="/img/login_image_light.png"
             alt="Light login image"
             fill={true}
-            style={{ objectFit: "contain" }}
+            style={{ objectFit: 'contain' }}
             priority={true}
           />
         </div>
         <div
           data-hide-on-theme="light"
-          className="h-[300px] w-[628px] w-full relative shrink-0"
+          className="h-[300px] max-w-[628px] relative shrink-0"
         >
           <Image
             src="/img/login_image_dark.png"
             alt="Dark login image"
             fill={true}
-            style={{ objectFit: "contain" }}
+            style={{ objectFit: 'contain' }}
             priority={true}
           />
         </div>
-        <h3 className="text-primary-focus text-2xl text-center mt-[27px]">Ready to dive in?</h3>
-        <h2 className="text-primary text-3xl text-center">Join Chingu today!</h2>
+        <h3 className="text-primary-focus text-2xl text-center mt-[27px]">
+          Ready to dive in?
+        </h3>
+        <h2 className="text-primary text-3xl text-center">
+          Join Chingu today!
+        </h2>
       </div>
-      <div className="w-[403px] h-[652px] w-full bg-base-200 rounded-2xl"></div>
+      <div className="w-[403px] h-[652px] bg-base-200 rounded-2xl"></div>
     </div>
   );
 }
