215b4a17a9afa187c631854006270d0bbf9bd7e1
timothyrusso <russotimothy@live.it>
2023-11-27 08:00:29 +0100

    feat: update structure of the page layout

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 
 export default function Component() {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center justify-center">
       <div className="flex min-h-[486px] flex-col justify-center">
         <div
           data-hide-on-theme="dark"
-          className="h-[300px] max-w-[628px] relative shrink-0"
+          className="h-[300px] w-[628px] relative shrink-0"
         >
           <Image
             src="/img/login_image_light.png"
@@ -20,7 +20,7 @@ export default function Component() {
         </div>
         <div
           data-hide-on-theme="light"
-          className="h-[300px] max-w-[628px] relative shrink-0"
+          className="h-[300px] w-[628px] relative shrink-0"
         >
           <Image
             src="/img/login_image_dark.png"
@@ -30,14 +30,14 @@ export default function Component() {
             priority={true}
           />
         </div>
-        <h3 className="text-primary-focus text-2xl text-center mt-[27px]">
+        <h3 className="text-primary-focus text-2xl text-center mt-[27px] font-semibold">
           Ready to dive in?
         </h3>
-        <h2 className="text-primary text-3xl text-center">
+        <h2 className="text-primary text-3xl text-center font-semibold">
           Join Chingu today!
         </h2>
       </div>
-      <div className="w-[403px] h-[652px] bg-base-200 rounded-2xl"></div>
+      <div className="w-[403px] h-[652px] bg-base-200 rounded-2xl ml-60"></div>
     </div>
   );
 }
