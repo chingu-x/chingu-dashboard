f9daeff6a347ec99b679e8e4efe8985dee7d6604
timothyrusso <russotimothy@live.it>
2023-12-01 22:39:49 +0100

    feat: update buttons and labels

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
 import { validateTextInput } from "@/helpers/form/validateInput";
 import Button from "@/components/Button";
 import TextInput from "@/components/inputs/TextInput";
@@ -66,8 +67,8 @@ export default function Component() {
           Join Chingu today!
         </h2>
       </div>
-      <div className="w-[403px] h-[652px] bg-base-200 rounded-2xl ml-60 p-6">
-        <p className="text-primary-focus text-2xl text-center mt-[27px] font-semibold">
+      <div className="w-[403px] min-h-[652px] bg-base-200 rounded-2xl ml-60 p-6">
+        <p className="text-base-300 text-2xl text-center mt-2.5 mb-[26px] font-medium">
           Welcome to Chingu
         </p>
         <div className="flex flex-col items-center">
@@ -83,18 +84,23 @@ export default function Component() {
             type="button"
             title="github"
             customClassName="text-base gap-x-0 font-semibold bg-base-200 hover:bg-base-content hover:border-base-100 capitalize text-base-300 h-16 mt-2 mb-2 border-2 border-base-100 w-full"
-            iconSrc="/img/github.png"
+            iconSrc="/img/discord.png"
           >
-            Continue with Github
+            Continue with Discord
           </Button>
           <Button
             type="button"
             title="github"
             customClassName="text-base gap-x-0 font-semibold bg-base-200 hover:bg-base-content hover:border-base-100 capitalize text-base-300 h-16 mt-2 mb-2 border-2 border-base-100 w-full"
-            iconSrc="/img/github.png"
+            iconSrc="/img/linkedin.png"
           >
-            Continue with Github
+            Continue with LinkedIn
           </Button>
+          <div className="flex items-center mt-6 mb-8 w-full">
+            <hr className="flex-grow border-neutral-content w-11/12" />
+            <p className="mx-4 text-base-300 font-semibold">or</p>
+            <hr className="flex-grow border-neutral-content w-11/12" />
+          </div>
         </div>
         <form
           onSubmit={() => console.log("submit")}
@@ -117,6 +123,12 @@ export default function Component() {
                 errors={errors}
                 maxLength={30}
               />
+              <Link
+                href={""}
+                className="font-medium text-xs text-base-300 ml-1"
+              >
+                Forgot your password?
+              </Link>
             </div>
           </div>
           <div className="flex flex-col gap-5 pt-8">
