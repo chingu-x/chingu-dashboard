b974c74ca89c5fae148175f951d132d54b228f09
Dan Ko <danko56666@gmail.com>
2023-12-02 13:59:35 -0300

    revert changes that added check for cookie in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { cookies } from "next/headers";
 import StoreProvider from "@/components/StoreProvider";
 import ThemeProvider from "@/components/ThemeProvider";
 import ModalProvider from "@/components/ModalProvider";
 import ToastProvider from "@/components/ToastProvider";
-import AuthInit from "@/components/AuthInit";
+import AuthProvider from "@/components/AuthProvider";
 
 export const metadata: Metadata = {
   title: "Chingu Dashboard",
@@ -25,8 +24,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const hasCookie = cookies().has("access_token");
-
   return (
     <html
       lang="en"
@@ -36,7 +33,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider storageKey="chingu-theme" disableTransitionOnChange>
           <StoreProvider>
-            <AuthInit hasCookie={hasCookie} />
+            <AuthProvider />
             <ToastProvider />
             <ModalProvider />
             {children}
