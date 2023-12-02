0a25930443b7155d5029a993c8b444998bcac178
timothyrusso <russotimothy@live.it>
2023-12-02 12:05:14 +0100

    feat: add sign up components

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,7 @@
-"use client";
-
 import BannerContainer from "./components/BannerContainer";
 import SignInModalContainer from "./components/SignInModalContainer";
 
-export default function Component() {
+export default function SignInPage() {
   return (
     <div className="flex flex-col xl:flex-row items-center justify-center">
       <BannerContainer />
