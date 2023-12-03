a75bdb15980e3d1eaf3cbc1f19b8c5132a90025a
timothyrusso <russotimothy@live.it>
2023-12-03 11:51:16 +0100

    feat: update layout for mobile devices

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex flex-col h-screen w-screen">
       <Navbar isAuthPage={true} />
-      <main className="flex flex-col items-center flex-1 w-full p-10 overflow-y-auto justify-center">
+      <main className="flex flex-col items-center flex-1 w-full pt-96 xl:pt-0 p-10 overflow-y-auto justify-center">
         <div className="flex flex-col max-w-[1353px] gap-y-9">{children}</div>
       </main>
     </div>
