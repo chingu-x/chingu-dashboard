951165f2cac27b7c16f263cd7d605bb9b819d2b7
Jane Moroz <Moroz.jane@yahoo.com>
2023-12-08 17:02:42 +0300

    fix: fix mode toggle button + add animation

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { useTheme } from "next-themes";
-import { SunIcon, MoonIcon } from "@heroicons/react/20/solid";
 import { ChangeEvent, useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import { AnimatePresence, Variants, motion } from "framer-motion";
+
+import { SunIcon, MoonIcon } from "@heroicons/react/20/solid";
 
 interface ModeToggleProps {
   isAuthPage: boolean;
@@ -24,16 +26,50 @@ export default function ModeToggle({ isAuthPage }: ModeToggleProps) {
     setTheme(e.target.checked === true ? "light" : "dark");
   };
 
+  const modeToggleVariants: Variants = {
+    hidden: {
+      rotate: 25,
+    },
+    show: {
+      rotate: 0,
+    },
+  };
+
   return (
-    <label className={`swap swap-rotate ${isAuthPage ? "mr-10" : ""}`}>
+    <label
+      className={`relative cursor-pointer flex justify-center items-center ${
+        isAuthPage ? "mr-10" : ""
+      }`}
+    >
       <input
+        className="invisible w-0 h-0"
         data-testid="mode-toggle"
         type="checkbox"
         onChange={toggleTheme}
         checked={theme === "light"}
       />
-      <MoonIcon className="w-6 h-6 duration-200 fill-current swap-on text-base-300" />
-      <SunIcon className="w-6 h-6 duration-200 fill-current swap-off text-base-300" />
+      <AnimatePresence mode="wait">
+        {theme === "dark" && (
+          <motion.div
+            key="modeToggleDark"
+            variants={modeToggleVariants}
+            initial="hidden"
+            animate="show"
+          >
+            <MoonIcon className="w-6 h-6 duration-200 fill-current swap-on text-base-300" />
+          </motion.div>
+        )}
+        {theme === "light" && (
+          <motion.div
+            key="modeToggleLight"
+            variants={modeToggleVariants}
+            initial="hidden"
+            animate="show"
+          >
+            <SunIcon className="w-6 h-6 duration-200 fill-current swap-off text-base-300" />{" "}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </label>
   );
 }
