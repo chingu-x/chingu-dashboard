b9abe0dc45c28a963ee0830758e2c56c89497e13
timothyrusso <russotimothy@live.it>
2024-01-04 11:41:48 +0100

    fix: ui problems

--- src/components/sidebar/ExpandButton.tsx
+++ src/components/sidebar/ExpandButton.tsx
@@ -13,7 +13,7 @@ export default function ExpandButton({ isOpen, onClick }: ExpandButtonProps) {
   return (
     <Button
       title="Expand"
-      customClassName="w-[3.125rem] bg-base-200 text-base-300 capitalize hover:bg-base-200 mr-2 border-none"
+      customClassName="bg-base-200 text-base-300 capitalize hover:bg-base-200 mr-6 border-none mt-4 flex items-center"
       onClick={() => onClick(!isOpen)}
     >
       {isOpen ? (
