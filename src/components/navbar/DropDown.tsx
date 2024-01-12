45a520557b7401fcec043fc0cdc8f30b7a467591
Matt <84277551+MattRueter@users.noreply.github.com>
2024-01-12 11:58:14 -0600

    fix layout & make dropdown content absolute relative to parent.

--- src/components/navbar/DropDown.tsx
+++ src/components/navbar/DropDown.tsx
@@ -3,16 +3,16 @@ import DropDownLink from "./DropDownLink";
 
 export default function DropDown({ name }: { name: string }) {
   return (
-    <div className="dropdown py-0 mx-2 dropdown-bottom">
+    <div className="relative dropdown py-0 mx-2 dropdown-bottom">
       <label
         tabIndex={0}
-        className="btn m-0 p-0 bg-transparent border-none hover:border-none hover:bg-transparent text-base-300"
+        className="flex btn m-0 p-0 bg-transparent border-none hover:border-none hover:bg-transparent text-base-300"
       >
         {name} <ChevronDownIcon className="w-4 text-base-300" />
       </label>
       <ul
         tabIndex={0}
-        className="dropdown-content z-[1] menu p-2 mt-2 shadow bg-base-100 right-0 border border-neutral rounded-2xl"
+        className="absolute dropdown-content z-[1] menu p-2 mt-2 shadow bg-base-100 right-0 border border-neutral rounded-2xl"
       >
         <DropDownLink title="Link 1" />
         <DropDownLink title="404???" href="/hello404" />
