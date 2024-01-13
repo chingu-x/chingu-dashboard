435f6b1ae9cf30304b6e4220a92ccbb55578ae0f
Timothy Russo <russotimothy@live.it>
2024-01-13 16:11:03 +0100

    feat: fix layout of social login buttons

--- src/app/(auth)/sign-in/components/SocialLoginButtonsContainer.tsx
+++ src/app/(auth)/sign-in/components/SocialLoginButtonsContainer.tsx
@@ -1,30 +1,46 @@
 import Button from "@/components/Button";
+import Image from "next/image";
 
 function SocialLoginButtonsContainer() {
   return (
     <>
       <Button
         type="button"
-        title="github"
-        customClassName="text-base gap-x-0 font-semibold bg-base-200 hover:bg-base-content hover:border-base-100 capitalize text-base-300 h-16 mt-2 mb-2 border-2 border-base-100 w-full"
-        iconSrc="/img/github.png"
+        title="Github"
+        className="text-base font-semibold bg-base-200 hover:bg-base-content hover:border-base-100 capitalize text-base-300 h-16 mt-2 mb-2 border-2 border-base-100 w-full gap-x-2 active:bg-base-content focus:bg-base-content focus:text-black"
       >
+        <Image
+          src={"/img/github.png"}
+          width={48}
+          height={48}
+          alt="Github icon"
+        />
         Continue with Github
       </Button>
       <Button
         type="button"
-        title="github"
-        customClassName="text-base gap-x-0 font-semibold bg-base-200 hover:bg-base-content hover:border-base-100 capitalize text-base-300 h-16 mt-2 mb-2 border-2 border-base-100 w-full"
-        iconSrc="/img/discord.png"
+        title="Discord"
+        className="text-base font-semibold bg-base-200 hover:bg-base-content hover:border-base-100 capitalize text-base-300 h-16 mt-2 mb-2 border-2 border-base-100 w-full gap-x-2 active:bg-base-content focus:bg-base-content focus:text-black"
       >
+        <Image
+          src={"/img/discord.png"}
+          width={48}
+          height={48}
+          alt="Discord icon"
+        />
         Continue with Discord
       </Button>
       <Button
         type="button"
-        title="github"
-        customClassName="text-base gap-x-0 font-semibold bg-base-200 hover:bg-base-content hover:border-base-100 capitalize text-base-300 h-16 mt-2 mb-2 border-2 border-base-100 w-full"
-        iconSrc="/img/linkedin.png"
+        title="LinkedIn"
+        className="text-base font-semibold bg-base-200 hover:bg-base-content hover:border-base-100 capitalize text-base-300 h-16 mt-2 mb-2 border-2 border-base-100 w-full gap-x-2 active:bg-base-content focus:bg-base-content focus:text-black"
       >
+        <Image
+          src={"/img/linkedin.png"}
+          width={48}
+          height={48}
+          alt="LinkedIn icon"
+        />
         Continue with LinkedIn
       </Button>
     </>
