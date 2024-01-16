0449f5551866cfd938ee552c3d83e4145af40a44
Winnie <winnieng3210@gmail.com>
2024-01-16 14:10:01 -0500

    translated global css style to Tailwind in one reuseable tooltip component

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,17 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 interface TooltipProps {
-  content: any;
-  position: string;
+  content: string;
+  position: "top" | "bottom" | "left" | "right";
   children: any;
+  toolTipWidth: "small" | "medium" | "large";
 }
 
-const Tooltip = ({ content, position, children }: TooltipProps) => {
+// ${hovered ? "opacity-100 visible" : " opacity-0 hidden"}
+
+// TODO:
+// configure tooltip width on tailwind global for custom uses
+// light / dark mode
+// create arrow tip
+
+const Tooltip = ({
+  content,
+  position,
+  children,
+  toolTipWidth,
+}: TooltipProps) => {
+  const [hovered, setHovered] = useState(false);
   return (
-    <div className={`tooltip tooltip--${position}`}>
+    <div
+      className={`relative cursor-pointer border border-red-500`}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
       {children}
-      <div className="tooltip-content">{content}</div>
+      <div
+        className={`absolute transition-opacity duration-300 z-[2]
+        text-white bg-neutral-content bg-black rounded-md p-2 transform 
+        ${
+          toolTipWidth === "small"
+            ? "w-[120px]"
+            : toolTipWidth === "medium"
+              ? "w-[160px]"
+              : "w-[300px]"
+        }
+        
+        ${
+          position === "top" || position === "bottom"
+            ? "-translate-x-1/2"
+            : "-translate-y-1/2"
+        }
+        ${
+          position === "top"
+            ? "bottom-full left-1/2"
+            : position === "bottom"
+              ? "top-full left-1/2"
+              : position === "right"
+                ? "top-1/2 left-full"
+                : "top-1/2 right-full"
+        }`}
+      >
+        {content}
+      </div>
     </div>
   );
 };
