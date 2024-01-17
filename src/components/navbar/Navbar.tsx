231eb4fdc879d8d3f769d9f7931fedbd5d4341ee
Matt <84277551+MattRueter@users.noreply.github.com>
2024-01-17 16:21:16 -0600

    adjust layout of children props in Navbar after merging dev updates.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,11 +6,11 @@ interface NavbarProps {
 
 export default function Navbar({ children }: NavbarProps) {
   return (
-    <nav className="flex navbar bg-base-100">
+    <nav className="flex navbar bg-base-100 p-2">
       <div className="flex flex-1 items-center pl-2">
         <ChinguMenu />
       </div>
-      <div className="flex gap-x-10">{children}</div>
+      <div className="border border-red-500 flex items-center gap-x-10">{children}</div>
     </nav>
   );
 }
