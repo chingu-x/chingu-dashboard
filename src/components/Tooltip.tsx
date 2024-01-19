f739a981633e36a8b040d63b62429b4688ab4281
Winnie <winnieng3210@gmail.com>
2024-01-19 10:32:55 -0500

    position tooltip away from icon

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -4,16 +4,13 @@ import PropTypes from "prop-types";
 interface TooltipProps {
   content: string;
   supportText?: string;
-  position: "none" | "top" | "bottom" | "left" | "right";
+  position: "top" | "bottom" | "left" | "right";
   children: any;
   toolTipWidth: "small" | "medium" | "large";
 }
 
 // TODO:
-// configure tooltip width on tailwind global for custom uses
-// configure tooltip to have a position on tailwind global
 // light / dark mode
-// style to add support text
 
 const Tooltip = ({
   content,
@@ -31,10 +28,10 @@ const Tooltip = ({
     >
       {children}
       <div
-        className={`absolute transition ease-in-out duration-300 z-[2]
-        break-all ${supportText && "text-left"}
+        className={`absolute transition ease-in-out duration-300 z-[2] break-all 
+        ${supportText && "text-left"}
         ${hovered ? "opacity-100" : "opacity-0"}
-        text-white bg-neutral-content bg-black rounded-md px-3 py-3 transform after:absolute after:content-[''] after:border-neutral-content after:border-[7px] after:border-solid
+        text-white bg-neutral-content bg-black rounded-md py-2 px-3 transform after:absolute after:content-[''] after:border-neutral-content after:border-[7px] after:border-solid
         ${
           supportText
             ? "w-[320px]"
@@ -52,15 +49,15 @@ const Tooltip = ({
         }
         ${
           position === "top"
-            ? "bottom-[150%] left-1/2 after:top-full after:border-b-transparent"
+            ? "bottom-full left-1/2 -translate-y-3 after:top-full after:border-b-transparent"
             : position === "bottom"
-              ? "top-[150%] left-1/2 after:bottom-full after:border-t-transparent"
+              ? "top-full left-1/2 translate-y-3 after:bottom-full after:border-t-transparent"
               : position === "right"
-                ? "top-1/2 left-[150%] after:right-full after:border-l-transparent"
-                : "top-1/2 right-[150%] after:left-full after:border-r-transparent"
+                ? "top-1/2 left-full translate-x-3 after:right-full after:border-l-transparent"
+                : "top-1/2 right-full -translate-x-3 after:left-full after:border-r-transparent"
         }`}
       >
-        <div>{content}</div>
+        <div className={`${supportText && "pb-2"}`}>{content}</div>
         {supportText && <div>{supportText}</div>}
       </div>
     </div>
