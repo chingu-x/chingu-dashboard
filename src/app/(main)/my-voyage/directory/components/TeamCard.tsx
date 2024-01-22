8782556d242c258df0f05cd0c0c97690935b3718
Jane Moroz <Moroz.jane@yahoo.com>
2024-01-22 13:57:02 +0300

    style: align list item

--- src/app/(main)/my-voyage/directory/components/TeamCard.tsx
+++ src/app/(main)/my-voyage/directory/components/TeamCard.tsx
@@ -10,23 +10,23 @@ export default function TeamCard({ teamMember, currentUserId }: TeamCardProps) {
   return (
     <div className="box-border flex flex-col items-center p-10 bg-secondary-content rounded-2xl">
       <ul className="flex flex-col gap-6 min-w-[400px]">
-        <li className="grid grid-cols-2 gap-6">
+        <li className="grid items-center grid-cols-2 gap-6">
           <span className="font-semibold">Name</span>
           <span>{teamMember.name}</span>
         </li>
-        <li className="grid grid-cols-2 gap-6">
+        <li className="grid items-center grid-cols-2 gap-6">
           <span className="font-semibold">Discord ID</span>
           <span>{teamMember.discordId}</span>
         </li>
-        <li className="grid grid-cols-2 gap-6">
+        <li className="grid items-center grid-cols-2 gap-6">
           <span className="font-semibold">Average Hour/Sprint</span>
           <EditCell teamMember={teamMember} currentUserId={currentUserId} />
         </li>
-        <li className="grid grid-cols-2 gap-6">
+        <li className="grid items-center grid-cols-2 gap-6">
           <span className="font-semibold">Timezone</span>
           <span>{teamMember.timeZone}</span>
         </li>
-        <li className="grid grid-cols-2 gap-6">
+        <li className="grid items-center grid-cols-2 gap-6">
           <span className="font-semibold">Position</span>
           <span>{teamMember.position}</span>
         </li>
