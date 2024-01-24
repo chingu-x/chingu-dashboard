c2f11b283f67a43f4856a83939058fce6d64895c
Matt <84277551+MattRueter@users.noreply.github.com>
2024-01-24 13:54:07 -0600

    grab logged in user information and show in nav & children.

--- src/components/navbar/DropDown.tsx
+++ src/components/navbar/DropDown.tsx
@@ -1,16 +1,18 @@
 "use client";
 import React, { useState, useRef, useEffect } from "react";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
-import DropDownLink from "./DropDownLink";
 import Button from "@/components/Button";
-import { useAppDispatch } from "@/store/hooks";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { clientSignOut } from "@/store/features/auth/authSlice";
 import { serverSignOut } from "@/app/(auth)/authService";
+import DropDownLink from "./DropDownLink";
+
 
 export default function DropDown({ name, openState }: { name: string, openState:boolean }) {
   const dispatch = useAppDispatch();
-  const menuRef = useRef<HTMLUListElement>(null);
   const [isOpen, setIsOpen] = useState(openState);
+  const currentVoyage  = useAppSelector((state) =>state.user.voyageTeamMembers[0]?.voyageTeam.name)
+  const menuRef = useRef<HTMLUListElement>(null);
   const closed = "hidden";
   const open ="absolute z-[1] p-2 mt-2 shadow bg-base-100 right-0 border border-neutral rounded-2xl";
 
@@ -51,7 +53,8 @@ export default function DropDown({ name, openState }: { name: string, openState:
         {name} <ChevronDownIcon className="w-4 text-base-300" />
       </label>
       <ul tabIndex={0} className={isOpen ? open : closed} ref={menuRef}>
-        <DropDownLink title="Link 1" />
+        {/**Check if user is on voyage. If so, display voyage name/link*/}
+        {currentVoyage &&  <DropDownLink title={currentVoyage} />}
         <DropDownLink title="404???" href="/hello404" />
         <Button title="signout" type="button" onClick={handleClick}>
           Sign Out
