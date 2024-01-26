c803874289e005c8a8e6b8cf36779b2c96a4452e
Timothy Russo <russotimothy@live.it>
2024-01-26 19:05:07 +0100

    fix: push error

--- src/stories/components/Sidebar.stories.tsx
+++ src/stories/components/Sidebar.stories.tsx
@@ -1,42 +1,43 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
 import Sidebar from "@/components/sidebar/Sidebar";
-import userReducer from "@/store/features/user/userSlice";
-import authReducer from "@/store/features/auth/authSlice";
+import { store } from "@/store/store";
+// import { configureStore } from "@reduxjs/toolkit";
+// import userReducer from "@/store/features/user/userSlice";
+// import authReducer from "@/store/features/auth/authSlice";
 
-const mockStore = configureStore({
-  reducer: {
-    user: userReducer,
-    auth: authReducer,
-  },
-  preloadedState: {
-    auth: {
-      isAuthenticated: true,
-    },
-    user: {
-      voyageTeamMembers: [
-        {
-          // @ts-expect-error - This is a mock object
-          voyageTeam: {
-            voyage: {
-              status: {
-                name: "Active",
-              },
-            },
-          },
-        },
-      ],
-    },
-  },
-});
+// const mockStore = configureStore({
+//   reducer: {
+//     user: userReducer,
+//     auth: authReducer,
+//   },
+//   preloadedState: {
+//     auth: {
+//       isAuthenticated: true,
+//     },
+//     user: {
+//       voyageTeamMembers: [
+//         {
+//           // @ts-expect-error - This is a mock object
+//           voyageTeam: {
+//             voyage: {
+//               status: {
+//                 name: "Active",
+//               },
+//             },
+//           },
+//         },
+//       ],
+//     },
+//   },
+// });
 
 const meta: Meta<typeof Sidebar> = {
   title: "Example/Sidebar",
   component: Sidebar,
   decorators: [
     (Story) => (
-      <Provider store={mockStore}>
+      <Provider store={store}>
         <div style={{ height: 700 }}>
           <Story />
         </div>
