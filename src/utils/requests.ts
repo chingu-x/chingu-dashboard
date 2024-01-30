a89768238a29f87927843e1f5eb11dc22e0fe5ad
Dan Ko <danko56666@gmail.com>
2024-01-30 01:49:31 -0300

    refactor get request (make more generic)

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,9 +1,15 @@
 "use server";
 
-export async function GET(url: string, token: string) {
-  return await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${url}`, {
+export async function GET<T>(url: string, token: string): Promise<T> {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${url}`, {
     headers: {
       Cookie: `access_token=${token}`,
     },
   });
+
+  if (!res.ok) {
+    throw new Error(res.statusText);
+  }
+
+  return (await res.json()) as T;
 }
