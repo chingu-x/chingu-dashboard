8024d926cb37f8b74e6a7aa06147e48a351b3895
Matt <84277551+MattRueter@users.noreply.github.com>
2024-01-30 17:22:45 -0600

    grab user's avatar from userSlice.

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -12,7 +12,7 @@ export default function Avatar({
   height = 24,
 }: AvatarProps) {
   return (
-    <div className="px-0 rounded-full cursor-pointer">
+    <div className="border border-1 border-neutral px-0 rounded-full cursor-pointer">
       <Image
         alt="avatar"
         src={
