b9936800bc60b77ae680b370978fc46c20387929
Matt <84277551+MattRueter@users.noreply.github.com>
2024-02-01 11:32:35 -0600

    adjust layout in Storybook and clean up code.

--- src/components/navbar/DropDown.tsx
+++ src/components/navbar/DropDown.tsx
@@ -12,20 +12,25 @@ export default function DropDown({
   placeHolderVoyage,
 }: {
   openState?: boolean;
-  placeHolderVoyage?: string
-}) { 
+  placeHolderVoyage?: string;
+}) {
   const dispatch = useAppDispatch();
   const allVoyages = useAppSelector((state) => state.user.voyageTeamMembers);
-  const activeVoyage = allVoyages?.find((item) => item.voyageTeam.voyage.status.name === "Active");
-  const currentVoyage = placeHolderVoyage ?? activeVoyage?.voyageTeam.name ?? "Please join a voyage to see your status information.";
+  const activeVoyage = allVoyages?.find(
+    (item) => item.voyageTeam.voyage.status.name === "Active",
+  );
+  const currentVoyage =
+    placeHolderVoyage ??
+    activeVoyage?.voyageTeam.name ??
+    "Please join a voyage to see your status information.";
   const closed = "hidden";
   const open =
     "absolute z-[1] w-44 p-4 [&>*]:mt-2 mt-6 shadow bg-base-100 right-0 border border-neutral rounded-2xl";
-    
+
   async function handleClick() {
     await serverSignOut();
     dispatch(clientSignOut());
-  };
+  }
   const handleDropDownClick = (event: React.MouseEvent<HTMLUListElement>) => {
     event.stopPropagation();
   };
@@ -38,22 +43,22 @@ export default function DropDown({
       >
         <ChevronDownIcon className="w-4 text-base-300 cursor-pointer" />
       </label>
-      <ul 
-        tabIndex={0} 
+      <ul
+        tabIndex={0}
         className={openState ? open : closed}
         onClick={handleDropDownClick}
       >
         <li className="bg-secondary-content text-xs p-2 [&>*]:m-1 rounded-lg ">
-          <p className="text-neutral text-xs">
-            My Status:
-          </p>
-          {currentVoyage ? 
+          <p className="text-neutral text-xs">My Status:</p>
+          {currentVoyage ? (
             <p className="text-base-300 border-[1px] border-transparent font-semibold text-base-300">
               {currentVoyage}
-            </p> : 
+            </p>
+          ) : (
             <p className="text-base-300 border-[1px] border-transparent font-semibold text-base-300">
               {currentVoyage}
-            </p>}
+            </p>
+          )}
         </li>
         <DropDownLink title="Settings" href="/hello404" />
         <Button
