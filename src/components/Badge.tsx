5bf6809db05a1e7741ba50caae31dabf474a45c7
Timothy Russo <russotimothy@live.it>
2024-02-04 18:29:50 +0100

    feat: simple badge

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -3,38 +3,22 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { ReactElement } from "react";
 import { cn } from "@/lib/utils";
 
-const badge = cva(
-  "flex bg-base-100 items-center rounded-[100px] h-[25px] gap-x-2 py-[3px] px-[9px]",
-  {
-    variants: {
-      variant: {
-        primary: ["text-primary", "border-primary"],
-        error: ["text-error-content", "border-error-content"],
-        warning: ["text-warning-content", "border-warning-content"],
-        success: ["text-success-content", "border-success-content"],
-      },
-      size: {
-        sm: ["text-[13px]", "py-2.5", "px-[18px]"],
-        md: ["text-[13px]", "py-3", "px-5"],
-        lg: ["text-[13px]", "py-4", "px-6"],
-      },
-    },
-    defaultVariants: {
-      variant: "primary",
-      size: "md",
-    },
-  },
-);
-
-const h2Title = cva("text-base font-medium text-base-300 leading-[19px]", {
+const badge = cva("flex items-center justify-center rounded-[100px]", {
   variants: {
+    variant: {
+      primary: ["bg-base-100"],
+      error: ["bg-error-content"],
+      warning: ["bg-warning-content"],
+      success: ["bg-success-content"],
+    },
     size: {
-      sm: ["text-[13px]"],
-      md: ["text-[15px]"],
-      lg: ["text-[17px]"],
+      sm: ["py-[2px]", "px-[8px]", "h-[20px]"],
+      md: ["py-[3px]", "px-[9px]", "h-[25px]"],
+      lg: ["py-[4px]", "px-[10px]", "h-[32px]"],
     },
   },
   defaultVariants: {
+    variant: "primary",
     size: "md",
   },
 });
@@ -45,16 +29,28 @@ interface BadgeBaseProps {
   avatar?: ReactElement;
 }
 
-export interface BadgeProps
-  extends BadgeBaseProps,
-    VariantProps<typeof badge>,
-    VariantProps<typeof h2Title> {}
+export interface BadgeProps extends BadgeBaseProps, VariantProps<typeof badge> {
+  TextSize?: "sm" | "md" | "lg";
+}
 
-function Badge({ title, avatar, variant, size, className }: BadgeProps) {
+function Badge({
+  title,
+  avatar,
+  variant,
+  size,
+  TextSize,
+  className,
+}: BadgeProps) {
+  const h2Class = {
+    sm: "text-[13px] font-medium",
+    md: "text-base font-medium",
+    lg: "text-xl font-medium",
+  }[TextSize || "md"];
+
   return (
     <div className={cn(badge({ variant, size, className }))}>
       {avatar}
-      <h2 className={cn(h2Title({ size, className }))}>{title}</h2>
+      <h2 className={h2Class}>{title}</h2>
     </div>
   );
 }
