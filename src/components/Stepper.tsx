eea4dfbe9e5f0375ef53db959bb243ebbb161f6d
Winnie <winnieng3210@gmail.com>
2024-02-05 16:30:20 -0500

    dynamic stepper logic

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -1,37 +1,47 @@
+"use client";
+
 import React from "react";
-import Button from "./Button";
+import Button from "@/components/Button";
+import { ButtonProps } from "@/components/Button";
 
 type StepperStyle = "chip" | "indicators";
-const steps = ["1", "2", "3", "4", "5"];
+type StepperStatus = "completed" | "current" | "remaining";
 
-const steppers = [
-  { indicator: "1", status: "completed", onClickEvent: () => console.log("1") },
-  { indicator: "2", status: "completed", onClickEvent: () => console.log("2") },
-  { indicator: "3", status: "current", onClickEvent: () => console.log("3") },
-  { indicator: "4", status: "remaining", onClickEvent: () => console.log("4") },
-  { indicator: "5", status: "remaining", onClickEvent: () => console.log("5") },
-  { indicator: "6", status: "remaining", onClickEvent: () => console.log("6") },
-];
+export interface SteppersItem {
+  indicator: string | React.JSX.Element;
+  status: StepperStatus;
+  onClickEvent: () => void;
+}
 
 export interface StepperProps {
   styleType: StepperStyle;
   stepperWidth: string;
-  // another prop that takes in the array of the stepper icons
-  // will need a very strict rule on how to set the array up for the stepper
+  steppers: SteppersItem[];
 }
 
 export default function Stepper({
-  styleType = "chip",
+  styleType,
   stepperWidth,
+  steppers,
 }: StepperProps) {
   return (
-    // TODO: figure out parent width
     <div className={`${stepperWidth}`}>
-      <ul className="flex">
+      <div className="flex">
         {steppers.map((step, idx) => {
           const { indicator, status, onClickEvent } = step;
+
+          const indicatorDisplay =
+            typeof indicator !== "string" && status === "completed"
+              ? "checked"
+              : indicator;
+
+          let buttonVariant: ButtonProps["variant"] = "accent";
+
+          if (status === "current") buttonVariant = "secondary";
+          if (status === "completed") buttonVariant = "primary";
+
           return (
-            <li
+            <div
               key={idx}
               className={`flex flex-1 last:flex-none relative
               last:after:contents-none after:absolute
@@ -45,23 +55,18 @@ export default function Stepper({
             `}
             >
               <Button
-                // onClick={onClickEvent}
-                className={`text-center 
-                ${styleType === "chip" && "w-[100px] h-[24px] rounded-full"}`}
-                variant={`${
-                  status === "current"
-                    ? "secondary"
-                    : status === "completed"
-                      ? "primary"
-                      : "accent"
+                onClick={onClickEvent}
+                className={`text-center rounded-full ${
+                  styleType === "chip" ? "w-[100px] h-[24px]" : "w-[45px]"
                 }`}
+                variant={buttonVariant}
               >
-                {indicator}
+                {indicatorDisplay}
               </Button>
-            </li>
+            </div>
           );
         })}
-      </ul>
+      </div>
     </div>
   );
 }
