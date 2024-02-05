856b2de5309d570f15334be74196e8ecd14bf316
Winnie <winnieng3210@gmail.com>
2024-02-05 15:09:34 -0500

    stepper color logic

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -1,38 +1,63 @@
 import React from "react";
+import Button from "./Button";
 
 type StepperStyle = "chip" | "indicators";
-const steps = ["1", "2", "3", "4"];
+const steps = ["1", "2", "3", "4", "5"];
+
+const steppers = [
+  { indicator: "1", status: "completed", onClickEvent: () => console.log("1") },
+  { indicator: "2", status: "completed", onClickEvent: () => console.log("2") },
+  { indicator: "3", status: "current", onClickEvent: () => console.log("3") },
+  { indicator: "4", status: "remaining", onClickEvent: () => console.log("4") },
+  { indicator: "5", status: "remaining", onClickEvent: () => console.log("5") },
+  { indicator: "6", status: "remaining", onClickEvent: () => console.log("6") },
+];
 
 export interface StepperProps {
   styleType: StepperStyle;
-
+  stepperWidth: string;
   // another prop that takes in the array of the stepper icons
   // will need a very strict rule on how to set the array up for the stepper
 }
 
-// className = "border border-yellow-500 before:content-[''] before:w-12 before:h-12 before:relative before:z-[1] before:block before:rounded-full flex flex-1 flex-col items-center";
-
-export default function Stepper({ styleType = "chip" }: StepperProps) {
+export default function Stepper({
+  styleType = "chip",
+  stepperWidth,
+}: StepperProps) {
   return (
     // TODO: figure out parent width
-    <div className="w-[1000px]">
+    <div className={`${stepperWidth}`}>
       <ul className="flex">
-        {steps.map((step, idx) => {
+        {steppers.map((step, idx) => {
+          const { indicator, status, onClickEvent } = step;
           return (
             <li
-              className="flex flex-1 last:flex-none relative
-              last:after:content-none after:absolute
-              after:content-[''] after:h-[8px] after:bg-base-100 after:w-full
-              after:translate-y-full after:-z-10 after:left-1
-            "
+              key={idx}
+              className={`flex flex-1 last:flex-none relative
+              last:after:contents-none after:absolute
+              after:content-[''] after:h-[7px]
+              ${
+                status === "completed"
+                  ? "after:bg-primary"
+                  : "after:bg-base-100"
+              }
+              after:w-full after:top-1/2 after:-translate-y-1/2 after:-z-10
+            `}
             >
-              <div
-                className={`text-center text-base-300 bg-base-100 border-base-100 ${
-                  styleType === "chip" && "w-[100px] h-[24px] rounded-full"
+              <Button
+                // onClick={onClickEvent}
+                className={`text-center 
+                ${styleType === "chip" && "w-[100px] h-[24px] rounded-full"}`}
+                variant={`${
+                  status === "current"
+                    ? "secondary"
+                    : status === "completed"
+                      ? "primary"
+                      : "accent"
                 }`}
               >
-                {step}
-              </div>
+                {indicator}
+              </Button>
             </li>
           );
         })}
