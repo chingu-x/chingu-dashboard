8ab67905c6db11c54720cd159101c75b4089784b
Matt <84277551+MattRueter@users.noreply.github.com>
2024-02-05 11:46:07 -0600

    configure mockStore for DropDown.

--- src/stories/components/DropDown.stories.tsx
+++ src/stories/components/DropDown.stories.tsx
@@ -1,37 +1,143 @@
 import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 import type { Meta, StoryObj } from "@storybook/react";
-import { store } from "@/store/store";
+import userReducer from "@/store/features/user/userSlice";
+import authReducer from "@/store/features/auth/authSlice";
 import DropDown from "@/components/navbar/DropDown";
 
-const meta = {
-  title: "Components/DropDown",
+const initialState = {
+  id: "",
+  firstName: "",
+  lastName: "",
+  countryCode: "",
+  discordId: "",
+  githubId: "",
+  twitterId: "",
+  linkedinId: "",
+  email: "",
+  timezone: "",
+  avatar:"",
+};
+
+const user1 = {
+  initialState,
+  voyageTeamMembers: [
+    {
+      id:1,
+      voyageTeamId:1,
+      voyageTeam: {
+        name:"Team 43-tier3-V42",
+        voyage:{
+          status:{
+            name:"Active"
+          }
+        }
+      },
+      voyageRole:{
+        name:"Developer"
+      },
+    }
+  ]
+};
+const user2 = {
+  initialState,
+  voyageTeamMembers: [
+    {
+      id:1,
+      voyageTeamId:1,
+      voyageTeam: {
+        name:"Team 43-tier3-V42",
+        voyage:{
+          status:{
+            name:"Closed"
+          }
+        }
+      },
+      voyageRole:{
+        name:"Developer"
+      },
+    }
+  ]
+};
+
+const mockStore = configureStore({
+  reducer: {
+    user: userReducer,
+    auth: authReducer,
+  },
+  preloadedState: {
+    auth: {
+      isAuthenticated: true,
+    },
+    //@ts-expect-error - This is a mock object
+    user: user1
+  }
+});
+const mockStore2 = configureStore({
+  reducer: {
+    user: userReducer,
+    auth: authReducer,
+  },
+  preloadedState: {
+    auth: {
+      isAuthenticated: true,
+    },
+    //@ts-expect-error - This is a mock object
+    user: user2
+  }
+});
+
+const meta: Meta<typeof DropDown> = {
+  title:"Components/DropDown",
   component: DropDown,
-  tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <Provider store={mockStore}>
+        <Story/>
+      </Provider>
+    )
+  ],
   parameters: {
-    layout: "centered",
+    layout: "centered"
   },
-  decorators: [(story) => 
-    <div style={{ height: 300 }}>
-      <Provider store={store}>{story()}</Provider>
-    </div>],
-} satisfies Meta<typeof DropDown>;
-
+  tags: ["autodocs"]
+};
 export default meta;
-type Story = StoryObj<typeof DropDown>;
+type Story = StoryObj<typeof meta>;
 
-export const MenuOpenedNoVoyage: Story = {
-  args: {
-    openState: true,
-  },
+
+export const Closed: Story = {
+  args:{
+    openState: false
+  }
 };
-export const MenuOpenedWithVoyage: Story = {
+
+export const OpenWithVoyage: Story = {
   args: {
-    openState: true,
-    placeHolderVoyage: "v47-tier2-team-4",
+    openState: true
   },
+  decorators: [
+    (Story) => (
+      <Provider store={mockStore}>
+        <div style={{ height: 300 }}>
+          <Story/>
+        </div>
+      </Provider>
+    )
+  ],
 };
-export const MenuClosed: Story = {
-  args: {
-    openState: false,
+
+export const OpenNoVoyage: Story = {
+  args:{
+    openState:true
   },
-};
+  decorators: [
+    (Story) => (
+      <Provider store={mockStore2}>
+        <div style={{ height: 300 }}>
+          <Story/>
+        </div>
+      </Provider>
+    )
+  ],
+}; 
