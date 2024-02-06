80f2d157ceed837ea9297725d11eee83fa356439
Matt <84277551+MattRueter@users.noreply.github.com>
2024-02-06 14:27:36 -0600

    replace avatar image placeholder with avatar.png for better visualization.

--- src/app/(main)/my-voyage/tech-stack/components/AvatarGroup.tsx
+++ src/app/(main)/my-voyage/tech-stack/components/AvatarGroup.tsx
@@ -1,6 +1,6 @@
 import type { TechItem } from "./fixtures/TechStack";
 import Avatar from "@/components/Avatar";
-
+import myAvatar from "@/public/img/avatar.png"; //temp until BE has data equal to fixtures/TechStacks.ts
 
 interface AvatarGroupProps {
     data : TechItem["users"]
@@ -9,13 +9,23 @@ interface AvatarGroupProps {
 export default function AvatarGroup ( { data }: AvatarGroupProps) {
   return(
     <div className="absolute flex -space-x-2 avatar-group left-28">
-      {/**Avatar image property will need to grab the avatar from each user
-      * as is user array is a string of user's names in the mock data TechItem
+      {/**<Avatar /> image property will need to point at the 
+      * avatar property on each user in array of users (the data passed into <AvatarGroup/>).
+      * As is, array of users from fixtures/TechStack is an array of names (string).
+        Something like the following will be needed...
+        data.map((user, index) =>(
+        <Avatar
+          key={index}
+          image={user.avatar}
+          width={24}
+          height={24}
+        />  
+        ))
       */}
       {data.map((index) => (
         <Avatar
           key={index}
-          image={""}
+          image={myAvatar}
           width={24}
           height={24}
         />
