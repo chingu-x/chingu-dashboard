1acd2b20d6c6dee68931df50d7feb115be1f4220
Dan Ko <danko56666@gmail.com>
2024-02-07 15:30:08 -0300

    add persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,6 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 import counterReducer from "./features/counter/counterSlice";
 import modalReducer from "./features/modal/modalSlice";
 import toastReducer from "./features/toast/toastSlice";
@@ -6,16 +8,28 @@ import authReducer from "./features/auth/authSlice";
 import ideationReducer from "./features/ideation/ideationSlice";
 import userReducer from "./features/user/userSlice";
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    modal: modalReducer,
-    toast: toastReducer,
-    auth: authReducer,
-    user: userReducer,
-    ideation: ideationReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["ideation"],
+};
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  modal: modalReducer,
+  toast: toastReducer,
+  auth: authReducer,
+  user: userReducer,
+  ideation: ideationReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
