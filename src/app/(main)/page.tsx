b1a2eb748f8e072ef918f4a886cce71af0b66a04
Timothy Russo <russotimothy@live.it>
2024-02-10 18:33:53 +0100

    feat: dashboard structure

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,27 @@
 // replace with dashboard page when that's completed
 
-import TempModalControls from "@/components/modals/TempModalControls";
+import Button from '@/components/Button';
+import Banner from '@/components/banner/Banner';
 
 export default function Home() {
-  return <TempModalControls />;
+  return (
+    <>
+      <div className="flex flex-col w-[400px] gap-y-6">
+        <div className="w-full h-[350] p-6 bg-base-200 rounded-2xl">
+          <p>Before your Voyage starts...</p>
+          <p>
+            Explore Chingu's Knowledge Hub to prepare for your Voyage with
+            information on tools, Agile, Scrum, Git, Teamwork, and more.
+          </p>
+        </div>
+        <div className="bg-base-200 rounded-2xl py-6 flex flex-col gap-y-4">
+          <p className="text-center">
+            Need support? Let’s get you back on track!
+          </p>
+          <Button className="self-center">Get Help</Button>
+        </div>
+      </div>
+      <div className="bg-base-200 rounded-2xl flex flex-col"></div>
+    </>
+  );
 }
