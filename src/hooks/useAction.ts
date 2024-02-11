99a36f26b2e3c444cc24a38b48dc07bea917d384
Dan Ko <danko56666@gmail.com>
2024-02-11 19:40:24 -0300

    cleanup hook

--- src/hooks/useAction.ts
+++ src/hooks/useAction.ts
@@ -2,26 +2,13 @@
 
 "use client";
 
-import { SerializedError, AsyncThunkAction } from "@reduxjs/toolkit";
-import { SetStateAction, useCallback, useState, Dispatch } from "react";
-import { useAppDispatch } from "@/store/hooks";
-import { editIdeation } from "@/app/(main)/my-voyage/[teamId]/ideation/ideationService";
-
-type AsyncThunkActionCreator<R, T> = (
-  args: T,
-) => AsyncThunkAction<R, T, object>;
-
-type ThunkHookResult<R, T> = {
-  runThunk: AsyncThunkActionCreator<R, T>;
-  isLoading: boolean;
-  setIsLoading: Dispatch<SetStateAction<boolean>>;
-  error: string | undefined;
-  setError: Dispatch<SetStateAction<string | undefined>>;
-};
+import { useCallback, useState } from "react";
 
 export default function useAction(action: any) {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | undefined>(undefined);
+
+  console.log(error);
 
   const runAction = useCallback(
     async (arg: any) => {
@@ -31,7 +18,7 @@ export default function useAction(action: any) {
 
       if (data.error) setError(data.error);
     },
-    [action],
+    [action]
   );
 
   return { runAction, isLoading, setIsLoading, error, setError };
