63f459b9564340c38e74e359a32c2f3217d10ec1
Dan Ko <danko56666@gmail.com>
2024-02-11 23:29:19 -0300

    example error modal without redux

--- src/app/(main)/my-voyage/[teamId]/ideation/components/IdeationForm.tsx
+++ src/app/(main)/my-voyage/[teamId]/ideation/components/IdeationForm.tsx
@@ -14,17 +14,17 @@ import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import {
   type IdeationData,
   addNewIdeation,
-  editIdeationThunk,
   deleteIdeationThunk,
 } from "@/store/features/ideation/ideationSlice";
 import Spinner from "@/components/Spinner";
 import {
   editIdeation,
   type EditIdeationProps,
+  AppError,
 } from "@/app/(main)/my-voyage/[teamId]/ideation/ideationService";
 import useThunk from "@/hooks/useThunk";
 import useAction from "@/hooks/useAction";
-import { onOpen } from "@/store/features/modal/modalSlice";
+import ErrorModal from "@/components/modals/ErrorModal";
 
 const validationSchema = z.object({
   title: validateTextInput({
@@ -49,12 +49,13 @@ type ValidationSchema = z.infer<typeof validationSchema>;
 
 export default function IdeationForm() {
   const router = useRouter();
-  const params = useParams<{ teamId: string; ideationId: string }>();
-  const { projectIdeas } = useAppSelector((state) => state.ideation);
-  const teamId = +params.teamId;
   const dispatch = useAppDispatch();
+  const params = useParams<{ teamId: string; ideationId: string }>();
+  const teamId = +params.teamId;
+  const { projectIdeas } = useAppSelector((state) => state.ideation);
   const [editMode, setEditMode] = useState<boolean>(false);
-  const [error, setError] = useState(undefined);
+  const [isOpen, setIsOpen] = useState(false);
+  const [editError, setEditError] = useState<string | undefined>(undefined);
   const [ideationData, setIdeationData] = useState<IdeationData>();
   const {
     runAction: editIdeationAction,
@@ -98,8 +99,9 @@ export default function IdeationForm() {
       }
 
       await editIdeationAction(filteredData).then((res) => {
-        if (res.error) {
-          dispatch(onOpen({ type: "error" }));
+        if ((res as AppError).error) {
+          setEditError((res as AppError).error);
+          setIsOpen(true);
           setEditLoading(false);
         } else {
           router.push(`/my-voyage/${teamId}/ideation`);
@@ -110,6 +112,11 @@ export default function IdeationForm() {
       await dispatch(addNewIdeation(payload));
     }
   };
+
+  function handleClose() {
+    setIsOpen(false);
+    setEditError(undefined);
+  }
 
   function handleDelete() {
     const ideationId = +params.ideationId;
@@ -160,75 +167,81 @@ export default function IdeationForm() {
   }
 
   return (
-    <div className="flex flex-col items-center">
-      {error}
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col w-full max-w-[1000px] gap-y-10"
-      >
-        <div className="flex flex-col gap-y-4">
-          <h1 className="text-base-300 text-3xl font-bold">
-            {editMode ? "Edit Project Idea" : "Add Project Idea"}
-          </h1>
-          <p className="text-base-300 text-lg font-medium">
-            Share your project idea with the team.
-          </p>
-        </div>
-        <TextInput
-          id="title"
-          label="title"
-          placeholder="Enter your voyage project idea"
-          {...register("title")}
-          errorMessage={errors.title?.message}
-          maxLength={50}
-          defaultValue={ideationData?.title ?? ""}
-        />
-        <Textarea
-          id="description"
-          label="description"
-          placeholder="Describe your idea. What problem or challenge do you aim to address or solve? What is the primary purpose and goal of your idea? Who are your intemded users?"
-          {...register("description")}
-          errorMessage={errors.description?.message}
-          defaultValue={ideationData?.description ?? ""}
-        />
-        <Textarea
-          id="visionStatement"
-          label="vision statement"
-          placeholder="Share your insoiring vision. How will you provide value and benefits to users? What long term impact do you hope to achieve?"
-          {...register("vision")}
-          errorMessage={errors.vision?.message}
-          defaultValue={ideationData?.vision ?? ""}
-        />
-        <Button
-          type="submit"
-          title="submit"
-          disabled={!isDirty || !isValid || editLoading}
-          size="lg"
-          variant="primary"
+    <>
+      <ErrorModal
+        isOpen={isOpen}
+        handleClose={handleClose}
+        errorMessage={editError!}
+      />
+      <div className="flex flex-col items-center">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col w-full max-w-[1000px] gap-y-10"
         >
-          {renderButtonContent()}
-        </Button>
-        {editMode && (
+          <div className="flex flex-col gap-y-4">
+            <h1 className="text-base-300 text-3xl font-bold">
+              {editMode ? "Edit Project Idea" : "Add Project Idea"}
+            </h1>
+            <p className="text-base-300 text-lg font-medium">
+              Share your project idea with the team.
+            </p>
+          </div>
+          <TextInput
+            id="title"
+            label="title"
+            placeholder="Enter your voyage project idea"
+            {...register("title")}
+            errorMessage={errors.title?.message}
+            maxLength={50}
+            defaultValue={ideationData?.title ?? ""}
+          />
+          <Textarea
+            id="description"
+            label="description"
+            placeholder="Describe your idea. What problem or challenge do you aim to address or solve? What is the primary purpose and goal of your idea? Who are your intemded users?"
+            {...register("description")}
+            errorMessage={errors.description?.message}
+            defaultValue={ideationData?.description ?? ""}
+          />
+          <Textarea
+            id="visionStatement"
+            label="vision statement"
+            placeholder="Share your insoiring vision. How will you provide value and benefits to users? What long term impact do you hope to achieve?"
+            {...register("vision")}
+            errorMessage={errors.vision?.message}
+            defaultValue={ideationData?.vision ?? ""}
+          />
+          <Button
+            type="submit"
+            title="submit"
+            disabled={!isDirty || !isValid || editLoading}
+            size="lg"
+            variant="primary"
+          >
+            {renderButtonContent()}
+          </Button>
+          {editMode && (
+            <Button
+              type="button"
+              size="lg"
+              variant="error"
+              onClick={handleDelete}
+              title="delete"
+            >
+              {renderDeleteButtonContent()}
+            </Button>
+          )}
           <Button
             type="button"
+            title="cancel"
             size="lg"
-            variant="error"
-            onClick={handleDelete}
-            title="delete"
+            variant="link"
+            onClick={() => router.back()}
           >
-            {renderDeleteButtonContent()}
+            Cancel
           </Button>
-        )}
-        <Button
-          type="button"
-          title="cancel"
-          size="lg"
-          variant="link"
-          onClick={() => router.back()}
-        >
-          Cancel
-        </Button>
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   );
 }
--- src/components/modals/ErrorModal.tsx
+++ src/components/modals/ErrorModal.tsx
@@ -1,28 +1,22 @@
 "use client";
 
-import { /*Dispatch, SetStateAction,*/ useCallback } from "react";
 import Button from "@/components/Button";
 import Modal from "@/components/modals/Modal";
-import { onClose } from "@/store/features/modal/modalSlice";
-import { useAppDispatch, useAppSelector } from "@/store/hooks";
 
-// interface ErrorModal {
-//   action: Dispatch<SetStateAction<string>>;
-// }
+interface ErrorModalProps {
+  isOpen: boolean;
+  handleClose: () => void;
+  errorMessage: string;
+}
 
-export default function ErrorModal() {
-  const dispatch = useAppDispatch();
-  const { isOpen, type } = useAppSelector((state) => state.modal);
-
-  const isModalOpen = isOpen && type === "error";
-
-  const handleClose = useCallback(() => {
-    dispatch(onClose());
-  }, [dispatch]);
-
+export default function ErrorModal({
+  isOpen,
+  handleClose,
+  errorMessage,
+}: ErrorModalProps) {
   return (
-    <Modal isOpen={isModalOpen} title={"Error"} onClose={handleClose}>
-      <div className="flex flex-col gap-4">Error</div>
+    <Modal isOpen={isOpen} title={"Error"} onClose={handleClose}>
+      <div className="flex flex-col gap-4">{errorMessage}</div>
       <div className="flex flex-col gap-5 pt-8">
         <Button size="lg" type="button" onClick={handleClose}>
           Close
