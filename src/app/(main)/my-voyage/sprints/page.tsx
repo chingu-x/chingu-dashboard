e47345e0967dc34f7da70854faaf828828992ce9
JaneMoroz <moroz.jane@yahoo.com>
2024-02-14 18:14:05 +0400

    feat: add banner

---
+++ src/app/(main)/my-voyage/sprints/page.tsx
@@ -0,0 +1,15 @@
+import Banner from "@/components/banner/Banner";
+
+export default function SprintsPage() {
+  return (
+    <>
+      <Banner
+        imageLight="/img/sprints_banner_light.png"
+        imageDark="/img/sprints_banner_dark.png"
+        alt="sprints_banner"
+        title="Sprints"
+        description="A sprint agenda helps the team stay on track, communicate well, and improve. Basically, it's like speed dating for developers. Except we're not looking for a soulmate, we're just trying to get some quality work done."
+      />
+    </>
+  );
+}
