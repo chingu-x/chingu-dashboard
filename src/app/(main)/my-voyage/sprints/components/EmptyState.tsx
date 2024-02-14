8f1cf6802340b6f836c4fef252507bfdccc729e8
JaneMoroz <moroz.jane@yahoo.com>
2024-02-14 19:10:54 +0400

    feat: add empty state

---
+++ src/app/(main)/my-voyage/sprints/components/EmptyState.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+export default function EmptyState() {
+  return (
+    <div className="w-full">
+      <div
+        data-hide-on-theme="dark"
+        className="relative h-[390px] 3xl:w-9/12 w-11/12 mx-auto"
+      >
+        <Image
+          src="/img/sprints_empty_light.png"
+          alt="Light login image"
+          fill={true}
+          style={{ objectFit: "contain" }}
+          priority={true}
+        />
+      </div>
+      <div
+        data-hide-on-theme="light"
+        className="relative 3xl:w-9/12 w-11/12 h-[390px] mx-auto"
+      >
+        <Image
+          src="/img/sprints_empty_light.png"
+          alt="Dark login image"
+          fill={true}
+          style={{ objectFit: "contain" }}
+          priority={true}
+        />
+      </div>
+    </div>
+  );
+}
