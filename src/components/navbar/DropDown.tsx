eb99e041901be82f37967f88f215422e9918e240
Dan Ko <danko56666@gmail.com>
2024-02-13 22:12:15 -0300

    finish refactoring signout

--- src/components/navbar/DropDown.tsx
+++ src/components/navbar/DropDown.tsx
@@ -1,17 +1,21 @@
 "use client";
 
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 import DropDownLink from "./DropDownLink";
 import Button from "@/components/Button";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { clientSignOut } from "@/store/features/auth/authSlice";
 import { serverSignOut } from "@/app/(auth)/authService";
+import ErrorModal from "@/components/modals/ErrorModal";
 
 export default function DropDown({ openState }: { openState?: boolean }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [error, setError] = useState<string | undefined>(undefined);
   const dispatch = useAppDispatch();
   const allVoyages = useAppSelector((state) => state.user.voyageTeamMembers);
   const activeVoyage = allVoyages?.find(
-    (item) => item.voyageTeam.voyage.status.name === "Active",
+    (item) => item.voyageTeam.voyage.status.name === "Active"
   );
   const currentVoyage =
     activeVoyage?.voyageTeam.name ??
@@ -23,51 +27,67 @@ export default function DropDown({ openState }: { openState?: boolean }) {
   async function handleClick() {
     const [res, error] = await serverSignOut();
 
-    console.log("res", res);
-    console.log("error", error);
-    dispatch(clientSignOut());
+    if (res) {
+      dispatch(clientSignOut());
+    } else {
+      setError(error?.message);
+      setIsModalOpen(true);
+    }
   }
+
+  function handleClose() {
+    setError(undefined);
+    setIsModalOpen(false);
+  }
+
   const handleDropDownClick = (event: React.MouseEvent<HTMLUListElement>) => {
     event.stopPropagation();
   };
 
   return (
-    <div className="relative py-0 mx-2">
-      <label
-        tabIndex={0}
-        className="flex btn m-0 p-0 bg-transparent border-none hover:border-none hover:bg-transparent text-base-300"
-      >
-        <ChevronDownIcon className="w-4 text-base-300 cursor-pointer" />
-      </label>
-      <ul
-        tabIndex={0}
-        className={openState ? open : closed}
-        onClick={handleDropDownClick}
-      >
-        <li className="bg-secondary-content text-xs p-2 [&>*]:m-1 rounded-lg ">
-          <p className="text-neutral text-xs">My Status:</p>
-          {currentVoyage ? (
-            <p className="text-base-300 border-[1px] border-transparent font-semibold text-base-300">
-              {currentVoyage}
-            </p>
-          ) : (
-            <p className="text-base-300 border-[1px] border-transparent font-semibold text-base-300">
-              {currentVoyage}
-            </p>
-          )}
-        </li>
-        <DropDownLink title="Settings" href="/hello404" />
-        <Button
-          title="signout"
-          type="button"
-          onClick={handleClick}
-          variant="link"
-          size={"lg"}
-          className="hover:bg-neutral-content hover:text-base-300 w-full flex justify-start p-2 m-0"
+    <>
+      <ErrorModal
+        isOpen={isModalOpen}
+        handleClose={handleClose}
+        errorMessage={error!}
+      />
+      <div className="relative py-0 mx-2">
+        <label
+          tabIndex={0}
+          className="flex btn m-0 p-0 bg-transparent border-none hover:border-none hover:bg-transparent text-base-300"
         >
-          Sign Out
-        </Button>
-      </ul>
-    </div>
+          <ChevronDownIcon className="w-4 text-base-300 cursor-pointer" />
+        </label>
+        <ul
+          tabIndex={0}
+          className={openState ? open : closed}
+          onClick={handleDropDownClick}
+        >
+          <li className="bg-secondary-content text-xs p-2 [&>*]:m-1 rounded-lg ">
+            <p className="text-neutral text-xs">My Status:</p>
+            {currentVoyage ? (
+              <p className="text-base-300 border-[1px] border-transparent font-semibold text-base-300">
+                {currentVoyage}
+              </p>
+            ) : (
+              <p className="text-base-300 border-[1px] border-transparent font-semibold text-base-300">
+                {currentVoyage}
+              </p>
+            )}
+          </li>
+          <DropDownLink title="Settings" href="/hello404" />
+          <Button
+            title="signout"
+            type="button"
+            onClick={handleClick}
+            variant="link"
+            size={"lg"}
+            className="hover:bg-neutral-content hover:text-base-300 w-full flex justify-start p-2 m-0"
+          >
+            Sign Out
+          </Button>
+        </ul>
+      </div>
+    </>
   );
 }
