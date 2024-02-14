74eb9111c1c9b59d3e46ffb255257d17dde5987c
Dan Ko <danko56666@gmail.com>
2024-02-13 22:00:50 -0300

    add refresh token and refactor signout server action

--- src/utils/getCookie.ts
+++ src/utils/getCookie.ts
@@ -1,5 +1,13 @@
 import { cookies } from "next/headers";
 
 export function getCookie() {
-  return cookies().get("access_token")?.value || "";
+  const token = cookies().get("access_token")?.value || "";
+
+  return `access_token=${token}`;
+}
+
+export function getRefreshToken() {
+  const token = cookies().get("refresh_token")?.value || "";
+
+  return `refresh_token=${token}`;
 }
