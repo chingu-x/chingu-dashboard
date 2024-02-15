2d06b0f95052d6a0813fbdc9daee8cfdf48e8796
Dan Ko <danko56666@gmail.com>
2024-02-15 01:02:01 -0300

    fix edge case with path parameter matching with team id in data

--- src/utils/handleAsync.ts
+++ src/utils/handleAsync.ts
@@ -4,7 +4,7 @@ type AsyncFunction<X> = () => Promise<X>;
 export type AsyncActionResponse<X> = [X | null, AppError | null];
 
 export async function handleAsync<X>(
-  asyncFn: AsyncFunction<X>
+  asyncFn: AsyncFunction<X>,
 ): Promise<AsyncActionResponse<X>> {
   try {
     const result = await asyncFn();
--- src/utils/routePaths.ts
+++ src/utils/routePaths.ts
@@ -1,1 +1,16 @@
+// Add all routes being used in the project here
 // todo: add routes here
+
+const routePaths = {
+  dashboardPage() {
+    return "/";
+  },
+  signIn() {
+    return "/sign-in";
+  },
+  ideationPage(teamId: string) {
+    return `my-voyage/${teamId}/ideation`;
+  },
+};
+
+export default routePaths;
