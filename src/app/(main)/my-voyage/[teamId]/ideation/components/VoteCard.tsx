a366b681c7d5d01bfa098799d05369cced885704
Dan Ko <danko56666@gmail.com>
2024-02-16 23:53:27 -0300

    fix purge located in useffect

--- src/app/(main)/my-voyage/[teamId]/ideation/components/VoteCard.tsx
+++ src/app/(main)/my-voyage/[teamId]/ideation/components/VoteCard.tsx
@@ -27,7 +27,7 @@ interface VoteCardProps {
 // todo: add delete confirmation modal
 function VoteCard({ teamId, projectIdeaId, users, className }: VoteCardProps) {
   const [currentUserVoted, setCurrentUserVoted] = useState<null | boolean>(
-    null
+    null,
   );
   const { id } = useAppSelector((state) => state.user);
   const { loading } = useAppSelector((state) => state.ideation);
@@ -80,7 +80,7 @@ function VoteCard({ teamId, projectIdeaId, users, className }: VoteCardProps) {
 
   const getVoteUsers = useCallback(
     () => users.map((user) => user.votedBy.member.id),
-    [users]
+    [users],
   );
 
   function buttonContent() {
