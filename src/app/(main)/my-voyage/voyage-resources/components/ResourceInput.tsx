7db8641a18b2dda22f0a0702f14bed1140caf65f
Matt <84277551+MattRueter@users.noreply.github.com>
2024-02-17 12:09:50 -0600

    create resource share input.

---
+++ src/app/(main)/my-voyage/voyage-resources/components/ResourceInput.tsx
@@ -0,0 +1,21 @@
+import { LinkIcon } from "@heroicons/react/24/outline";
+import Button from "@/components/Button";
+import TextInput from "@/components/inputs/TextInput";
+
+export default function ResourceInput () {
+  return (
+    <div className="flex items-center bg-base-200 rounded-lg shadow-lg p-1">
+      
+      <div className=" flex flex-col w-3/4 m-4">
+        <div className="flex">
+          <div className="bg-neutral-content w-11 h-11 rounded-lg">
+            <LinkIcon className="text-base-200 p-2"/>
+          </div>
+          <TextInput className="my-0" id="link" placeholder="Paste your resource link here."/>
+        </div>
+        <TextInput className="my-0" id="title" placeholder="Name your resource here."/>
+      </div>
+      <Button className="h-1/4 w-1/4 m-4">Share Resource</Button>
+    </div>
+  );
+};
