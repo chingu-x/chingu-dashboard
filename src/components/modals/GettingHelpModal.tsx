d03fb710bb8b7bd8a075f95c648ef7944ddc825d
Timothy Russo <russotimothy@live.it>
2024-02-17 11:07:31 +0100

    feat: style the help modal

--- src/components/modals/GettingHelpModal.tsx
+++ src/components/modals/GettingHelpModal.tsx
@@ -20,31 +20,35 @@ export default function GettingHelpModal() {
 
   return (
     <Modal isOpen={isModalOpen} title="Getting Help" onClose={handleClose}>
-      <div className="flex flex-col">
-        <p>Roundtable Discussion</p>
-        <p>
+      <div className="flex flex-col max-w-[700px]">
+        <p className="text-base font-semibold mb-2.5">Roundtable Discussion</p>
+        <p className="text-base">
           If you have questions about Chingu or the Voyage process join one or
           both of the Chingu Roundtable sessions held every:
         </p>
-        <ul className="list-disc ml-6">
+        <ul className="list-disc ml-6 mb-4">
           <li>Wednesday @ 5:00 PM CT (Chicago) </li>
           <li>Saturday @ 9:00 AM CT (Chicago)</li>
         </ul>
-        <p>
+        <p className="text-base mb-10">
           These sessions give you the opportunity to ask your questions in real
           time with a member of the Chingu team. These are held in our Discord
           community in the <span className="font-bold">#townhall-chat</span> &{" "}
           <span className="font-bold">#townhall-voice</span> channels.
         </p>
-        <p>Stuck on a coding issue?</p>
-        <p>
+        <p className="text-base font-semibold mb-2.5">
+          Stuck on a coding issue?
+        </p>
+        <p className="text-base mb-10">
           Chingu&apos;s are famous for helping each other with technical
           problems and you can tap into this generosity by posting your
           technical questions and issues in the{" "}
           <span className="font-bold">#coding-help</span> Discord channel.
         </p>
-        <p>Reaching out to Chingu Admin</p>
-        <p>
+        <p className="text-base font-semibold mb-2.5">
+          Reaching out to Chingu Admin
+        </p>
+        <p className="text-base mb-2.5">
           If you need assistance from our Admin Team, you can ask questions
           anytime in the <span className="font-bold">#admin-feedback</span>{" "}
           channel or you can open up a support ticket by clicking on the{" "}
@@ -59,9 +63,9 @@ export default function GettingHelpModal() {
           </Link>{" "}
           to help us understand and quickly resolve your question.
         </p>
-        <p>
+        <p className="text-base">
           If you need to ask something confidential you can email
-          support@chingu.io
+          <Link href="mailto:support@chingu.io">support@chingu.io</Link>
         </p>
       </div>
     </Modal>
