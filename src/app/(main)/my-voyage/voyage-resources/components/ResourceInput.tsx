6d83cf96527b289db3e3b4680ee6f9c84e24b718
Matt <84277551+MattRueter@users.noreply.github.com>
2024-02-17 13:29:24 -0600

    add SortingButton to resources page.

--- src/app/(main)/my-voyage/voyage-resources/components/ResourceInput.tsx
+++ src/app/(main)/my-voyage/voyage-resources/components/ResourceInput.tsx
@@ -2,10 +2,11 @@ import { LinkIcon } from "@heroicons/react/24/outline";
 import Button from "@/components/Button";
 import TextInput from "@/components/inputs/TextInput";
 
+//Input group will replace the TextInputs in this form
+
 export default function ResourceInput () {
   return (
-    <div className="flex items-center bg-base-200 rounded-lg shadow-lg p-1">
-      
+    <form className="flex items-center bg-base-200 rounded-xl shadow-lg p-1 w-5/6">      
       <div className=" flex flex-col w-3/4 m-4">
         <div className="flex">
           <div className="bg-neutral-content w-11 h-11 rounded-lg">
@@ -16,6 +17,6 @@ export default function ResourceInput () {
         <TextInput className="my-0" id="title" placeholder="Name your resource here."/>
       </div>
       <Button className="h-1/4 w-1/4 m-4">Share Resource</Button>
-    </div>
+    </form>
   );
 };
