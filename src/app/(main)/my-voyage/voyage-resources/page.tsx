eed51e2ab46be8c63f76f720a9c444440694ddcb
Matt <84277551+MattRueter@users.noreply.github.com>
2024-02-17 12:09:50 -0600

    create resource share input.

--- src/app/(main)/my-voyage/voyage-resources/page.tsx
+++ src/app/(main)/my-voyage/voyage-resources/page.tsx
@@ -1,16 +1,18 @@
+import ResourceInput from "./components/ResourceInput";
 import Banner from "@/components/banner/Banner";
 
 export default function ResourcesPage() {
-    return  (
-      <>
-        <Banner
-        //replace tech_stack pngs with resource page png's
-          imageLight="/img/tech_stack_banner_light.png"
-          imageDark="/img/tech_stack_banner_dark.png"
-          alt="resources_banner"
-          title="Resources"
-          description="This resources page is your secret weapon for this voyage! Take a look at what your team is sharing or share your own resources for this voyage. Go ahead and be the first to post a new resource for you and your peers!"
-        />
-      </>
-    )
+  return  (
+    <>
+      <Banner
+      //replace tech_stack pngs with resource page png's
+        imageLight="/img/tech_stack_banner_light.png"
+        imageDark="/img/tech_stack_banner_dark.png"
+        alt="resources_banner"
+        title="Resources"
+        description="This resources page is your secret weapon for this voyage! Take a look at what your team is sharing or share your own resources for this voyage. Go ahead and be the first to post a new resource for you and your peers!"
+      />
+      <ResourceInput />
+    </>
+  );
 }
