207d7f66bfd26890f4371282f640e1fb386f92b1
Matt <84277551+MattRueter@users.noreply.github.com>
2024-02-19 11:24:07 -0600

    add fake data.

---
+++ src/app/(main)/my-voyage/voyage-resources/components/fixtures/resources.ts
@@ -0,0 +1,48 @@
+
+export const resources =[
+  {
+    title:"Short Title.",
+    owner: "Badge Component",
+    date: new Date().toString()
+  },
+  {
+    title:"Med Length Title.",
+    owner: "Badge Component",
+    date: new Date().toString()
+  },
+  {
+    title:"A Somewhat Longer Title.",
+    owner: "Badge Component",
+    date: new Date().toString()
+  },
+  {
+    title:"A Much Longer Title Than the Others.",
+    owner: "Badge Component",
+    date: new Date().toString()
+  },
+  {
+    title:"A Very Very Very Very Very Very Very Very Very Very Very Very Very Very Long Title",
+    owner: "Badge Component",
+    date: new Date().toString()
+  },
+  {
+    title:"Title",
+    owner: "Badge Component",
+    date: new Date().toString()
+  },
+  {
+    title:"Title",
+    owner: "Badge Component",
+    date: new Date().toString()
+  },
+  {
+    title: "Title",
+    owner: "Badge Component",
+    date: new Date().toString()
+  },
+  {
+    title:"Title",
+    owner: "Badge Component",
+    date: new Date().toString()
+  },
+];
