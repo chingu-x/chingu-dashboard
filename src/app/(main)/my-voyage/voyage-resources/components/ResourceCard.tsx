44fe9d6280360b68ff5f7f2f0a1fb5719c4e6343
Matt <84277551+MattRueter@users.noreply.github.com>
2024-02-19 11:00:09 -0600

    adjust style & layout of ResourceCard.

--- src/app/(main)/my-voyage/voyage-resources/components/ResourceCard.tsx
+++ src/app/(main)/my-voyage/voyage-resources/components/ResourceCard.tsx
@@ -2,14 +2,15 @@ import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
 import { TrashIcon } from "@heroicons/react/20/solid";
 
 export default function ResourceCard () {
+  
   return (
-    <div className="flex items-center justify-between bg-base-200 border border-base-100 rounded-xl shadow-sm hover:shadow-md h-20 p-4">
+    <div className="flex items-center justify-between h-20 p-4 bg-base-200 rounded-xl shadow-sm hover:shadow-md hover:border hover:border-base-100">
       <ArrowTopRightOnSquareIcon className="w-8 h-8 stroke-1 hover:stroke-2"/>
-      <div className="border border-red-500 flex flex-col justify-center h-full w-3/4">
-        <h1 className="text-xl font-bold">title</h1>
-        <div className="flex">
-          <p>Shared by: </p>
-          <div>Badge Here...</div>
+      <div className="flex flex-col justify-center w-3/4">
+        <h1 className="text-xl font-bold truncate">Title</h1>
+        <div className="flex [&>*]:mr-8">
+          <div>Shared by Badge goes Here</div>
+          <div>Added: Timestamp</div>
         </div>
       </div>
       <div className="w-10 h-10 hover:bg-base-100 flex justify-center items-center rounded-full">
