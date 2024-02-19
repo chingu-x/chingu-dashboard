3b66ea9c434dee3d97920f481bf11d0e746eb750
Matt <84277551+MattRueter@users.noreply.github.com>
2024-02-19 10:38:24 -0600

    add ResourceCard component & clean up code in sibling files.

--- src/app/(main)/my-voyage/voyage-resources/components/ResourceInput.tsx
+++ src/app/(main)/my-voyage/voyage-resources/components/ResourceInput.tsx
@@ -4,19 +4,27 @@ import TextInput from "@/components/inputs/TextInput";
 
 //Input group will replace the TextInputs in this form
 
-export default function ResourceInput () {
+export default function ResourceInput() {
   return (
-    <form className="flex items-center bg-base-200 rounded-xl shadow-lg p-1 w-5/6">      
+    <form className="flex items-center bg-base-200 rounded-xl shadow-lg p-1 w-5/6">
       <div className=" flex flex-col w-3/4 m-4">
         <div className="flex">
           <div className="bg-neutral-content w-11 h-11 rounded-lg">
-            <LinkIcon className="text-base-200 p-2"/>
+            <LinkIcon className="text-base-200 p-2" />
           </div>
-          <TextInput className="my-0" id="link" placeholder="Paste your resource link here."/>
+          <TextInput
+            className="my-0"
+            id="link"
+            placeholder="Paste your resource link here."
+          />
         </div>
-        <TextInput className="my-0" id="title" placeholder="Name your resource here."/>
+        <TextInput
+          className="my-0"
+          id="title"
+          placeholder="Name your resource here."
+        />
       </div>
       <Button className="h-1/4 w-1/4 m-4">Share Resource</Button>
     </form>
   );
-};
+}
--- src/app/(main)/my-voyage/voyage-resources/components/SortingButton.tsx
+++ src/app/(main)/my-voyage/voyage-resources/components/SortingButton.tsx
@@ -3,18 +3,16 @@ import Button from "@/components/Button";
 
 interface SortingButtonProps {
   onClick: () => void;
-  type: boolean
+  type: boolean;
 }
-export default function SortingButton ( { onClick, type }:SortingButtonProps ) {
+export default function SortingButton({ onClick, type }: SortingButtonProps) {
   const buttonText = type ? "Newest First" : "Oldest First";
-  const ArrowIcon  = type ? <ArrowDownIcon/> : <ArrowUpIcon/>;
+  const ArrowIcon = type ? <ArrowDownIcon /> : <ArrowUpIcon />;
 
-  return(
+  return (
     <Button onClick={onClick} className="h-1/4 m-1" variant="neutral">
       {buttonText}
-      <div className="w-4 h-4">
-        {ArrowIcon}
-      </div>
+      <div className="w-4 h-4">{ArrowIcon}</div>
     </Button>
   );
-};
+}
