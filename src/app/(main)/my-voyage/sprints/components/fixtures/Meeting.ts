0ec1ce9db94c25a3849cb550f9421c6cb42fe889
JaneMoroz <moroz.jane@yahoo.com>
2024-02-20 20:50:28 +0400

    chore: add mock data

---
+++ src/app/(main)/my-voyage/sprints/components/fixtures/Meeting.ts
@@ -0,0 +1,66 @@
+export interface Agenda {
+  id: number;
+  title: string;
+  description: string;
+  status: boolean;
+}
+
+export interface Meeting {
+  id: number;
+  sprint: {
+    id: number;
+    number: number;
+    startDate: string;
+    endDate: string;
+  };
+  title: string;
+  dateTime: string;
+  meetingLink: string;
+  notes: string;
+  agendas: Agenda[];
+}
+
+export const Meeting: Meeting = {
+  id: 1,
+  sprint: {
+    id: 1,
+    number: 1,
+    startDate: "2023-11-06T00:00:00.000Z",
+    endDate: "2023-11-12T00:00:00.000Z",
+  },
+  title: "First sprint kickoff meeting",
+  dateTime: "2023-11-07T00:00:00.000Z",
+  meetingLink: "meet.google.com/abcdefg",
+  notes:
+    "Title\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque. Sed sed lacinia lectus. Duis sit amet sodales felis.",
+  agendas: [
+    {
+      id: 1,
+      title: "Project Management Tools",
+      description:
+        "Walk the team through how the Jira board is organized and how we will coordinate communications and tickets.",
+      status: false,
+    },
+    {
+      id: 2,
+      title: "Milestone for this week",
+      description:
+        "FE Team - make homepage responsive\nBE Team - create endpoints for user profile\nDE Team - user flow for the modals",
+      status: false,
+    },
+    {
+      id: 3,
+      title: "FE Team",
+      description:
+        "Title\nPR pushed this week\n- Modals\n- Components\n- Homepage",
+      status: false,
+    },
+    {
+      id: 4,
+      title: "BE Team",
+      description:
+        "Title\nEndpoints created this week\n- Homepage\n- User profile\n- Settings",
+      status: false,
+    },
+  ],
+};
