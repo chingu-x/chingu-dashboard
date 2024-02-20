5f54148f3ed5b8808e66d5807171ccba38079b46
JaneMoroz <moroz.jane@yahoo.com>
2024-02-20 21:35:58 +0400

    feat: add `WeeklyCheckingFrom` (textarea inputs only)

--- src/app/(main)/my-voyage/sprints/components/SprintActions.tsx
+++ src/app/(main)/my-voyage/sprints/components/SprintActions.tsx
@@ -12,14 +12,17 @@ import Button from "@/components/Button";
 export default function SprintActions() {
   return (
     <div className="flex justify-between p-5 shadow-md bg-base-200 rounded-2xl">
+      {/* TODO: add animated variant to Button.tsx */}
       <Button variant="secondary" size="lg" className="group">
         <RocketLaunchIcon className="h-[18px] w-[18px]" /> Submit Voyage
         <ArrowRightIcon className="h-[18px] w-0 group-hover:w-[18px] transition-all" />
       </Button>
-      <Button variant="primary" size="lg" className="group">
-        <DocumentCheckIcon className="h-[18px] w-[18px]" /> Submit Check-in
-        <ArrowRightIcon className="h-[18px] w-0 group-hover:w-[18px] transition-all" />
-      </Button>
+      <Link href="/my-voyage/sprints/weekly-checkin">
+        <Button variant="primary" size="lg" className="group">
+          <DocumentCheckIcon className="h-[18px] w-[18px]" /> Submit Check-in
+          <ArrowRightIcon className="h-[18px] w-0 group-hover:w-[18px] transition-all" />
+        </Button>
+      </Link>
       <Link href="/my-voyage/sprints/create-meeting">
         <Button variant="outline" size="lg" className="group">
           <CalendarIcon className="h-[18px] w-[18px]" /> Create Meeting
