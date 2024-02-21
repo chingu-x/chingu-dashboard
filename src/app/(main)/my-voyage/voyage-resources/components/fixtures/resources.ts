224c8443c0b4968ada1148e235414be9f0a2751c
Matt <84277551+MattRueter@users.noreply.github.com>
2024-02-21 11:37:39 -0600

    conditionally render trashcan if currentUser's resource.

--- src/app/(main)/my-voyage/voyage-resources/components/fixtures/resources.ts
+++ src/app/(main)/my-voyage/voyage-resources/components/fixtures/resources.ts
@@ -3,63 +3,72 @@ export const resources =[
   {
     id:0,
     title:"Short Title.",
-    owner: "Badge Component",
+    badge: "Badge Component",
+    currentUser: true,
     date: new Date().toString(),
     link:"https://www.mozilla.org/en-US/"
   },
   {
     id:1,
     title:"Med Length Title.",
-    owner: "Badge Component",
+    badge: "Badge Component",
+    currentUser: false,
     date: new Date().toString(),
     link: "https://www.mozilla.org/en-US/"
   },
   {
     id:2,
     title:"A Somewhat Longer Title.",
-    owner: "Badge Component",
+    badge: "Badge Component",
+    currentUser: false,
     date: new Date().toString(),
     link: "https://www.mozilla.org/en-US/"
   },
   {
     id:3,
     title:"A Much Longer Title Than the Others.",
-    owner: "Badge Component",
+    badge: "Badge Component",
+    currentUser: true,
     date: new Date().toString(),
     link: "https://www.mozilla.org/en-US/"
   },
   {
     id:4,
     title:"A Very Very Very Very Very Very Very Very Very Very Very Very Very Very Long Title",
-    owner: "Badge Component",
+    badge: "Badge Component",
+    currentUser: true,
     date: new Date().toString(),
     link: "https://www.mozilla.org/en-US/"
   },
   {
     id:5,
     title:"Title",
-    owner: "Badge Component",
+    badge: "Badge Component",
+    currentUser: false,
     date: new Date().toString(),
     link: "https://www.mozilla.org/en-US/"
   },
   {
     id:6,
     title:"Title",
-    owner: "Badge Component",
+    badge: "Badge Component",
+    currentUser: false,
     date: new Date().toString(),
     link: "https://www.mozilla.org/en-US/"
   },
   {
     id:7,
     title: "Title",
-    owner: "Badge Component",
+    badge: "Badge Component",
+    currentUser: false,
     date: new Date().toString(),
     link: "https://www.mozilla.org/en-US/"
   },
   {
     id:8,
     title:"Title",
-    owner: "Badge Component",
+    badge: "Badge Component",
+    currentUser: true,
     date: new Date().toString(),
     link: "https://www.mozilla.org/en-US/"
   },
