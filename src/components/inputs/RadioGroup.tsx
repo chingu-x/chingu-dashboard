553be0c2dce9db3ba9be7d7304e93bebc7880f6a
JaneMoroz <moroz.jane@yahoo.com>
2024-02-21 22:07:33 +0400

    style: remove `RadioGroup` styles from global styles

--- src/components/inputs/RadioGroup.tsx
+++ src/components/inputs/RadioGroup.tsx
@@ -19,18 +19,18 @@ const RadioGroupItem = React.forwardRef<HTMLInputElement, RadioGroupItemProps>(
         altLayout && "w-auto",
       )}
     >
-      <input
-        id={id}
-        type="radio"
-        ref={ref}
-        {...props}
-        className="hidden peer"
-      />
       <Label
         htmlFor={id}
         className="flex items-center normal-case cursor-pointer text-neutral-focus gap-x-4"
       >
-        <span className="inline-block w-6 h-6 border rounded-full bg-base-200 border-neutral/40"></span>
+        <input
+          id={id}
+          type="radio"
+          ref={ref}
+          {...props}
+          className="hidden peer"
+        />
+        <span className="inline-block w-6 h-6 border rounded-full bg-base-200 border-neutral/40 transition-all [&>*]:hidden peer-checked:shadow-[inset_0_0_0_6px] peer-checked:[&>*]:shadow-color-base-200" />
         {!altLayout && label}
       </Label>
     </div>
