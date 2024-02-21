1934a3a45abb9731288c427f67afbc8c996aa26a
Matt <84277551+MattRueter@users.noreply.github.com>
2024-02-21 14:30:52 -0600

    add EmptyBanner component.

--- src/app/(main)/my-voyage/voyage-resources/page.tsx
+++ src/app/(main)/my-voyage/voyage-resources/page.tsx
@@ -5,8 +5,10 @@ import SortingButton from "./components/SortingButton";
 import ResourceCard from "./components/ResourceCard";
 import ViewModal from "./components/ViewModal";
 import DeleteModal from "./components/DeleteModal";
+import EmptyBanner from "./components/EmptyBanner";
 import { resources } from "./components/fixtures/resources";
 import Banner from "@/components/banner/Banner";
+//const resources = null // temp var for toggling empty banner or resource cards.
 
 export default function ResourcesPage() {
   const [ byNewest, setByNewest ] = useState(true);
@@ -31,32 +33,34 @@ export default function ResourcesPage() {
     setSelectedResource({ id, title, link });
     setViewModalState(true);
   };
- 
+  
   return (
     <>
       <Banner
-        //replace tech_stack pngs with resource page png's
-        imageLight="/img/tech_stack_banner_light.png"
-        imageDark="/img/tech_stack_banner_dark.png"
+        imageLight="/img/resources_banner_light.png"
+        imageDark="/img/resources_banner_dark.png"
         alt="resources_banner"
         title="Resources"
         description="This resources page is your secret weapon for this voyage! Take a look at what your team is sharing or share your own resources for this voyage. Go ahead and be the first to post a new resource for you and your peers!"
-      />      
+      /> 
       <div className="flex  items-center">
         <ResourceInput />
         <SortingButton onClick={sort} type={byNewest} />
-      </div>
-      {resources.map((item) => (
-        <ResourceCard 
-          key={item.id} 
-          deleteResource={() => deleteResource(item.id,item.title, item.link) }
-          viewResource={() => viewResource(item.id,item.title, item.link)}
-          title={item.title} 
-          badge={item.badge}
-          currentUser={item.currentUser}
-          date={item.date} 
-        />
-      ))}
+      </div>      
+      {!resources ? 
+        <EmptyBanner /> : 
+        resources.map((item) => (
+          <ResourceCard 
+            key={item.id} 
+            deleteResource={() => deleteResource(item.id,item.title, item.link) }
+            viewResource={() => viewResource(item.id,item.title, item.link)}
+            title={item.title} 
+            badge={item.badge}
+            currentUser={item.currentUser}
+            date={item.date} 
+          />
+        ))   
+      }
       <DeleteModal
         selectedResource={selectedResource}
         viewing={deleteModalState}
