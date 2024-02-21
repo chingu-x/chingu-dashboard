1854dfa690287dd27012ab7a95fa4fa2a2a46161
Jane Moroz <Moroz.jane@yahoo.com>
2024-02-21 16:03:30 +0300

    feat: add `RadioGroup` with alt layout

---
+++ src/app/(main)/my-voyage/sprints/components/forms/WeeklyCheckIn/radioGroupData.ts
@@ -0,0 +1,114 @@
+export const communicationOptions = [
+  {
+    id: "no_communication",
+    value: "no_communication",
+    label: "I didn't communicate with my team",
+  },
+  {
+    id: "team_channel_only",
+    value: "team_channel_only",
+    label: "Only in Team Channel",
+  },
+  {
+    id: "team_meetings_only",
+    value: "team_meetings_only",
+    label: "Only in Team Meeting(s)",
+  },
+  {
+    id: "team_channel_meetings",
+    value: "team_channel_meetings",
+    label: "Team Channel + Team Meeting(s)",
+  },
+];
+
+export const pairProgrammingTimeOptions = [
+  {
+    id: "pair_0",
+    value: "pair_0",
+    label: "0 hrs.",
+  },
+  {
+    id: "pair_1_4",
+    value: "pair_1_4",
+    label: "1-4 hrs.",
+  },
+  {
+    id: "pair_5-8",
+    value: "pair_5-8",
+    label: "5-8 hrs.",
+  },
+  {
+    id: "pair_8+",
+    value: "pair_8+",
+    label: "8+ hrs.",
+  },
+];
+
+export const onMyOwnTimeOptions = [
+  {
+    id: "single_0",
+    value: "single_0",
+    label: "0 hrs.",
+  },
+  {
+    id: "single_1_4",
+    value: "single_1_4",
+    label: "1-4 hrs.",
+  },
+  {
+    id: "single_5-8",
+    value: "single_5-8",
+    label: "5-8 hrs.",
+  },
+  {
+    id: "single_8+",
+    value: "single_8+",
+    label: "8+ hrs.",
+  },
+];
+
+export const learningTimeOptions = [
+  {
+    id: "learn_0",
+    value: "learn_0",
+    label: "0 hrs.",
+  },
+  {
+    id: "learn_1_4",
+    value: "learn_1_4",
+    label: "1-4 hrs.",
+  },
+  {
+    id: "learn_5-8",
+    value: "learn_5-8",
+    label: "5-8 hrs.",
+  },
+  {
+    id: "learn_8+",
+    value: "learn_8+",
+    label: "8+ hrs.",
+  },
+];
+
+export const teamTimeOptions = [
+  {
+    id: "team_0",
+    value: "team_0",
+    label: "0 hrs.",
+  },
+  {
+    id: "team_1_4",
+    value: "team_1_4",
+    label: "1-4 hrs.",
+  },
+  {
+    id: "team_5-8",
+    value: "team_5-8",
+    label: "5-8 hrs.",
+  },
+  {
+    id: "team_8+",
+    value: "team_8+",
+    label: "8+ hrs.",
+  },
+];
--- src/components/inputs/RadioGroup.tsx
+++ src/components/inputs/RadioGroup.tsx
@@ -2,19 +2,29 @@
 
 import React from "react";
 import Label from "./Label";
+import { cn } from "@/lib/utils";
 
 interface RadioGroupItemProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   id: string;
   value: string;
   label: string;
+  altLayout?: boolean;
 }
 
 const RadioGroupItem = React.forwardRef<HTMLInputElement, RadioGroupItemProps>(
-  ({ id, value, label, ...props }, ref) => (
-    <div className="relative flex items-center w-full gap-x-4">
+  ({ id, value, label, altLayout, ...props }, ref) => (
+    <div
+      className={cn(
+        "relative flex items-center w-full gap-x-4",
+        altLayout && "w-auto",
+      )}
+    >
       <input id={id} value={value} type="radio" ref={ref} {...props} />
-      <Label htmlFor={id} className="text-neutral-focus">
+      <Label
+        htmlFor={id}
+        className={`text-neutral-focus ${altLayout && "w-0 h-0 opacity-0"}`}
+      >
         {label}
       </Label>
     </div>
@@ -25,23 +35,49 @@ RadioGroupItem.displayName = "RadioGroupItem";
 
 interface RadioGroupProps {
   options: RadioGroupItemProps[];
+  title?: string;
 }
 
 const RadioGroup = React.forwardRef<HTMLInputElement, RadioGroupProps>(
-  ({ options, ...props }, ref) => (
-    <div className="flex flex-col gap-y-5">
-      {options.map(({ id, value, label }) => (
-        <RadioGroupItem
-          key={id}
-          id={id}
-          value={value}
-          label={label}
-          {...props}
-          ref={ref}
-        />
-      ))}
-    </div>
-  ),
+  ({ options, title, ...props }, ref) => {
+    if (title) {
+      return (
+        <div className="flex flex-col gap-y-5">
+          <div className="w-full bg-neutral-content px-4 py-[23px] grid grid-cols-[150px_1fr] gap-x-4 items-center justify-between rounded-lg">
+            <span className="text-base font-medium text-base-300">{title}</span>
+            <div className="flex justify-between px-6">
+              {options.map(({ id, value, label }) => (
+                <RadioGroupItem
+                  key={id}
+                  id={id}
+                  value={value}
+                  label={label}
+                  {...props}
+                  ref={ref}
+                  altLayout={true}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="flex flex-col gap-y-5">
+          {options.map(({ id, value, label }) => (
+            <RadioGroupItem
+              key={id}
+              id={id}
+              value={value}
+              label={label}
+              {...props}
+              ref={ref}
+            />
+          ))}
+        </div>
+      );
+    }
+  },
 );
 
 RadioGroup.displayName = "RadioGroup";
