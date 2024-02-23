53773fd180629b1b6d6dffc0161cf27522bfff3b
Timothy Russo <russotimothy@live.it>
2024-02-23 18:20:33 +0100

    feat: dashboard redirect

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,5 @@
 // replace with dashboard page when that's completed
 
-import PreVoyageDashboard from "./components/PreVoyageDashboard";
-
 export default function Home() {
-  return <PreVoyageDashboard />;
+  return <></>;
 }
