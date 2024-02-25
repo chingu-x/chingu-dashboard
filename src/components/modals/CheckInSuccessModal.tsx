dbc2732af4e1a0beb24c3c0d13c9ad114a77d707
JaneMoroz <moroz.jane@yahoo.com>
2024-02-25 19:12:44 +0400

    fix: replace `react-lottie` with `lottie-react` + fix lottie error

--- src/components/modals/CheckInSuccessModal.tsx
+++ src/components/modals/CheckInSuccessModal.tsx
@@ -1,18 +1,18 @@
 "use client";
 
 import { useCallback } from "react";
-// import Lottie from "react-lottie";
-// import { useTheme } from "next-themes";
-// import checkmarkLight from "@/public/lotties/checkmark_light.json";
-// import checkmarkDark from "@/public/lotties/checkmark_dark.json";
+import dynamic from "next/dynamic";
+import checkmarkLight from "@/public/lotties/checkmark_light.json";
+import checkmarkDark from "@/public/lotties/checkmark_dark.json";
 
 import Modal from "@/components/modals/Modal";
 
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { onClose } from "@/store/features/modal/modalSlice";
 
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+
 export default function CheckInSuccessModal() {
-  // const { theme } = useTheme();
   const { isOpen, type } = useAppSelector((state) => state.modal);
   const dispatch = useAppDispatch();
 
@@ -22,17 +22,9 @@ export default function CheckInSuccessModal() {
     dispatch(onClose());
   }, [dispatch]);
 
-  // const checkmarkOptions = {
-  //   loop: false,
-  //   autoplay: true,
-  //   rendererSettings: {
-  //     preserveAspectRatio: "xMidYMid slice",
-  //   },
-  // };
-
   return (
     <Modal isOpen={isModalOpen} title="" onClose={handleClose}>
-      <div className="grid grid-cols-2 max-w-[700px] gap-x-10 items-center">
+      <div className="grid grid-cols-[1fr_200px] max-w-[700px] gap-x-10 items-center">
         <div className="flex flex-col py-10 gap-y-4 text-base-300">
           <h3 className="text-[30px] font-bold">
             Check In Submission Recieved
@@ -42,20 +34,16 @@ export default function CheckInSuccessModal() {
           </p>
         </div>
         <div className="relative flex items-center justify-center">
-          {/* {theme === "light" && (
-            <Lottie
-              options={{ ...checkmarkOptions, animationData: checkmarkLight }}
-              height={220}
-              width={220}
-            />
-          )}
-          {theme === "dark" && (
-            <Lottie
-              options={{ ...checkmarkOptions, animationData: checkmarkDark }}
-              height={220}
-              width={220}
-            />
-          )} */}
+          <Lottie
+            animationData={checkmarkLight}
+            loop={false}
+            data-hide-on-theme="dark"
+          />
+          <Lottie
+            animationData={checkmarkDark}
+            loop={false}
+            data-hide-on-theme="light"
+          />
         </div>
       </div>
     </Modal>
