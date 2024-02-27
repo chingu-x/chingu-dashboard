034220a66f3d97c51dca85bf86e7145d36e72df8
Winnie <winnieng3210@gmail.com>
2024-02-27 14:03:21 -0500

    stepper stories refactored

--- src/stories/components/Stepper.stories.tsx
+++ src/stories/components/Stepper.stories.tsx
@@ -1,33 +1,40 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Stepper, { SteppersItem } from "@/components/Stepper";
+import { EnvelopeIcon } from "@heroicons/react/20/solid";
 
 const steppers: SteppersItem[] = [
   {
+    icon: <EnvelopeIcon className="h-[1.125rem]" />,
     name: "1",
     status: "completed",
     onClickEvent: () => console.log("1"),
   },
   {
+    icon: <EnvelopeIcon className="h-[1.125rem]" />,
     name: "2",
     status: "completed",
     onClickEvent: () => console.log("2"),
   },
   {
+    icon: <EnvelopeIcon className="h-[1.125rem]" />,
     name: "3",
     status: "current",
     onClickEvent: () => console.log("3"),
   },
   {
+    icon: <EnvelopeIcon className="h-[1.125rem]" />,
     name: "4",
     status: "remaining",
     onClickEvent: () => console.log("4"),
   },
   {
+    icon: <EnvelopeIcon className="h-[1.125rem]" />,
     name: "5",
     status: "remaining",
     onClickEvent: () => console.log("5"),
   },
   {
+    icon: <EnvelopeIcon className="h-[1.125rem]" />,
     name: "6",
     status: "remaining",
     onClickEvent: () => console.log("6"),
