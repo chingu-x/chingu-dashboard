092f26a6be0472ef32ff0558845e081a2b2bb1fb
Matt <84277551+MattRueter@users.noreply.github.com>
2024-02-28 12:08:33 -0600

    refactor modals and add to Provider.

--- src/app/(main)/my-voyage/voyage-resources/components/DeleteModal.tsx
+++ src/app/(main)/my-voyage/voyage-resources/components/DeleteModal.tsx
@@ -1,19 +1,26 @@
 import ModalSection from "./ModalSection";
 import Modal from "@/components/modals/Modal";
 import Button from "@/components/Button";
+import { onClose } from "@/store/features/modal/modalSlice";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
 
-interface DeleteModalProps {
-  selectedResource: { title:string };
-  viewing:boolean;
-  handleClose: () => void;
-}
+export default function DeleteModal () {
+  const dispatch = useAppDispatch();
+  const { isOpen, type } = useAppSelector((state) => state.modal);
+  const isModalOpen = isOpen && type === "deleteResource";
 
-export default function DeleteModal ( { selectedResource,viewing,handleClose }: DeleteModalProps) {
+  //TODO: replace title with actual data.
+  const title = "title here";
+  
+  const handleClose = () => {  
+    dispatch(onClose());
+  };
+
   return (
-    <Modal isOpen={viewing} title="Confirm Deletion?" onClose={handleClose}>
+    <Modal isOpen={isModalOpen} title="Confirm Deletion?" onClose={handleClose}>
       <form>
         <ModalSection heading="Are you sure you would like to visit this resource?">
-          <p className="text-neutral">{selectedResource.title}</p>
+          <p className="text-neutral">{title}</p>
         </ModalSection>         
         <div className="flex justify-between w-full h-16">
           <Button size="lg" variant="neutral" onClick={handleClose} className="w-3/6 m-1">Go Back</Button>
--- src/store/features/modal/modalSlice.ts
+++ src/store/features/modal/modalSlice.ts
@@ -1,6 +1,6 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-export type ModalType = "example1" | "example2" | "feature";
+export type ModalType = "example1" | "example2" | "feature" |"viewResource" | "deleteResource";
 
 interface ModalState {
   type: ModalType | null;
