688ac8501a28785be12f4b5137d06e8d06c287b8
JaneMoroz <moroz.jane@yahoo.com>
2024-02-28 20:03:10 +0400

    style: fix font color

--- src/components/inputs/RadioGroup/RadioGroupItem.tsx
+++ src/components/inputs/RadioGroup/RadioGroupItem.tsx
@@ -36,7 +36,9 @@ const RadioGroupItem = React.forwardRef<HTMLInputElement, RadioGroupItemProps>(
           className="hidden peer"
         />
         <span className="inline-block w-6 h-6 border rounded-full bg-base-200 border-neutral/40 transition-all group-hover:bg-base-100 group-hover:border group-hover:border-neutral peer-disabled:bg-base-100 peer-checked:border-0 peer-checked:bg-base-200 peer-checked:shadow-[inset_0_0_0_7px] peer-checked:shadow-base-300 group-hover:peer-checked:shadow-neutral peer-disabled:peer-checked:bg-neutral-focus peer-disabled:peer-checked:shadow-neutral" />
-        {groupLayout !== "horizontal" && label}
+        <span className="peer-checked:text-base-300">
+          {groupLayout !== "horizontal" && label}
+        </span>
       </Label>
     </div>
   ),
