307ad394e521b20f1f0f56474d605fb6ba98d196
Dan Ko <danko56666@gmail.com>
2024-02-27 23:43:13 -0300

    update banner in pre-voyage page

--- src/app/(main)/dashboard/components/PreVoyageBanner.tsx
+++ src/app/(main)/dashboard/components/PreVoyageBanner.tsx
@@ -1,33 +1,15 @@
-import React from "react";
-import Image from "next/image";
+import Banner from "@/components/banner/Banner";
 
 function PreVoyageBanner() {
   return (
     <div className="bg-base-200 rounded-2xl flex flex-col flex-grow-1 w-full pt-[70px] pb-6 pl-[80px] pr-[80px]">
-      <div
-        data-hide-on-theme="dark"
-        className="flex h-[437px] w-full relative shrink-0"
-      >
-        <Image
-          src="/img/pre_voyage_light.png"
-          alt="Light pre Voyage image"
-          fill={true}
-          style={{ objectFit: "contain" }}
-          priority={true}
-        />
-      </div>
-      <div
-        data-hide-on-theme="light"
-        className="flex h-[437px] w-full relative shrink-0"
-      >
-        <Image
-          src="/img/pre_voyage_dark.png"
-          alt="Light pre Voyage image"
-          fill={true}
-          style={{ objectFit: "contain" }}
-          priority={true}
-        />
-      </div>
+      <Banner
+        imageLight="/img/pre_voyage_light.png"
+        imageDark="/img/pre_voyage_dark.png"
+        alt="Pre Voyage banner image"
+        height="[437px]"
+        width="full"
+      />
       <p className="text-center text-[25px] font-semibold">Are you ready?</p>
       <p className="text-center text-base font-medium	">
         Your Voyage starts on May 2, 2024.
