2f7ed0c846d98463d3e74fde3f17ce187981d482
Matt <84277551+MattRueter@users.noreply.github.com>
2024-02-28 11:51:53 -0600

    adjust layout.

--- src/app/(main)/my-voyage/voyage-resources/components/EmptyBanner.tsx
+++ src/app/(main)/my-voyage/voyage-resources/components/EmptyBanner.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function EmptyBanner () {
   return(
-    <div className="flex items-center justify-between">
-      <div className="text-lg font-medium text-base-300">
+    <div className="flex items-center w-5/6 m-6">
+      <div className="text-lg font-medium text-base-300 w-1/2 m-2">
         <h1 className="text-lg">Be The First to Share!</h1>
         <p className="text-sm">It looks like no one has posted anything yet, but don&apos;t worry, you can be the first to share your favorite and most valuable resource with your fellow Chingus! 
           <br></br>
