b5cbe919611b816387dd3e8b5d45370b0e6dd9e0
Dan Ko <danko56666@gmail.com>
2024-02-28 17:25:00 -0300

    update colors in create ideation container and edd/edit ideation page

--- src/app/(main)/my-voyage/[teamId]/ideation/components/VoteDescriptionCard.tsx
+++ src/app/(main)/my-voyage/[teamId]/ideation/components/VoteDescriptionCard.tsx
@@ -6,7 +6,7 @@ interface VoteDescriptionCardProps {
 
 function VoteDescriptionCard({ className }: VoteDescriptionCardProps) {
   return (
-    <div className={cn("w-full bg-primary-content rounded-lg", className)}>
+    <div className={cn("w-full bg-base-100 rounded-lg", className)}>
       <section className="flex flex-col items-start p-4 gap-y-4">
         <h2 className="text-xl font-semibold text-base-300 leading-[24px]">
           Votes
