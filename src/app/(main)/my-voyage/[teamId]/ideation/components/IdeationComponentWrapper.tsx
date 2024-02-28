15d6beab05de7c4cdc912c25132a624982dacd88
Dan Ko <danko56666@gmail.com>
2024-02-27 23:50:41 -0300

    fix issue with dark mode banner image

--- src/app/(main)/my-voyage/[teamId]/ideation/components/IdeationComponentWrapper.tsx
+++ src/app/(main)/my-voyage/[teamId]/ideation/components/IdeationComponentWrapper.tsx
@@ -28,7 +28,7 @@ export async function fetchProjectIdeas({
       `api/v1/voyages/${teamId}/ideations`,
       token,
       "force-cache",
-      CacheTag.ideation,
+      CacheTag.ideation
     );
 
   return await handleAsync(fetchProjectIdeasAsync);
@@ -50,7 +50,7 @@ export default async function IdeationComponentWrapper({
 
   if (user) {
     currentVoyageTeam = user.voyageTeamMembers.find(
-      (voyage) => voyage.voyageTeam.voyage.status.name === "Active",
+      (voyage) => voyage.voyageTeam.voyage.status.name === "Active"
     );
   }
 
@@ -86,9 +86,9 @@ export default async function IdeationComponentWrapper({
         <Banner
           imageLight="/img/ideation_banner_light.png"
           imageDark="/img/ideation_banner_dark.png"
+          alt="ideation_banner"
           height="[200px]"
           width="[276px]"
-          alt="ideation_banner"
         />
       </VoyagePageBannerContainer>
       <div className="flex flex-col items-center gap-y-10">
