5f7455592df68c736c2e73a7129dc14325e0145f
Winnie <winnieng3210@gmail.com>
2024-02-29 16:40:25 -0500

    remove reset-password page

--- src/app/(auth)/reset-password/page.tsx
+++
@@ -1,9 +0,0 @@
-// import NewPasswordContainer from "../sign-in/components/NewPasswordContainer";
-
-// export default function ResetPasswordPage() {
-//   return (
-//     <div className="flex flex-col xl:flex-row items-center justify-center">
-//       <NewPasswordContainer />
-//     </div>
-//   );
-// }
