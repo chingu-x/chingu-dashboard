6f0c2dd0c2a5ea9717118778f122975d7f0a7800
Timothy Russo <russotimothy@live.it>
2024-03-01 07:31:19 +0100

    feat: add reusable dashbord widget

--- src/app/(main)/dashboard/components/voyage-dashboard/VoyageDashboard.tsx
+++ src/app/(main)/dashboard/components/voyage-dashboard/VoyageDashboard.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { DocumentCheckIcon } from "@heroicons/react/24/outline";
-import Image from "next/image";
+import DashboardWidget from "./DashboardWidget";
 import VoyageSupport from "@/app/(main)/dashboard/components/shared/VoyageSupport";
 import Badge from "@/components/badge/Badge";
 import Button from "@/components/Button";
@@ -10,12 +10,12 @@ import Button from "@/components/Button";
 function VoyageDashboard() {
   return (
     <div className="flex flex-row gap-x-6">
-      <div className="flex flex-col gap-y-6 flex-grow-1 w-full">
+      <div className="flex flex-col gap-y-6 flex-grow-2 w-full">
         <div className="w-full h-full p-6 bg-base-200 rounded-2xl flex flex-row">
-          <div className="w-[500px] h-[475px] flex justify-center items-center border">
+          <div className="w-max-[500px] min-h-[475px] flex justify-center items-center border">
             Calendar placeholder
           </div>
-          <div className="pl-6 flex flex-col justify-between">
+          <div className="pl-6 flex flex-col justify-between w-full">
             <div>
               <p className="text-xl font-semibold pb-3">Wednesday, May 4</p>
               <p className="rounded-lg bg-primary-content p-3 text-base font-medium w-[200px]">
@@ -42,74 +42,47 @@ function VoyageDashboard() {
         </div>
         <VoyageSupport />
       </div>
-      <div className="flex flex-grow-1 flex-col w-full bg-base-200 rounded-2xl p-4">
+      <div className="flex flex-grow-1 flex-col w-max-[650px] w-full bg-base-200 rounded-2xl p-4">
         <p className="text-[25px] font-semibold mb-[23px]">
           My Voyage Overview
         </p>
         <div className="flex flex-col gap-y-4">
-          <div className="rounded-lg bg-base-100 p-4">
-            <p className="text-[13px] font-semibold text-neutral-focus pb-4">
-              Ideation
-            </p>
-            <div className="flex flex-row">
-              <div className="flex flex-col gap-y-4">
-                <p className="text-xl font-semibold">
-                  What is your Voyage project idea & vision?
-                </p>
-                <p className="text-base font-medium">
-                  Share your ideas on what the team Voyage should be. Describe
-                  your vision to capture what it does and the benefit it will
-                  bring to users.
-                </p>
-                <Button variant="outline" className="text-base font-semibold">
-                  Go to Ideation
-                </Button>
-              </div>
-              <div className="w-full">
-                <div
-                  data-hide-on-theme="dark"
-                  className="flex h-[140px] w-full relative shrink-0"
-                >
-                  <Image
-                    src="/img/discover_light.png"
-                    alt="Light pre Voyage image"
-                    fill={true}
-                    style={{ objectFit: "contain" }}
-                    priority={true}
-                  />
-                </div>
-                <div
-                  data-hide-on-theme="light"
-                  className="flex h-[140px] w-full relative shrink-0"
-                >
-                  <Image
-                    src="/img/discover_dark.png"
-                    alt="Light pre Voyage image"
-                    fill={true}
-                    style={{ objectFit: "contain" }}
-                    priority={true}
-                  />
-                </div>
-              </div>
+          <DashboardWidget
+            imageLight="/img/discover_light.png"
+            imageDark="/img/discover_dark.png"
+            title="What is your Voyage project idea & vision?"
+            link="Ideation"
+            buttonTitle="Go to Ideation"
+            description="Share your ideas on what the team Voyage should be. Describe your
+            vision to capture what it does and the benefit it will bring to
+            users."
+          />
+          <div className="flex flex-row justify-between gap-x-4">
+            <div className="flex flex-grow-1 w-full">
+              <DashboardWidget
+                title="What features will you develop?"
+                link="Features"
+                buttonTitle="Go to Features"
+                description="Brainstorm and prioritize the features that will be included in the scope of your project. "
+              />
+            </div>
+            <div className="flex flex-grow-1 w-full">
+              <DashboardWidget
+                title="Choose your tech stack "
+                link="Tech Stack"
+                buttonTitle="Go to Tech Stack"
+                description="Choose the programming languages, frameworks, and tools that will serve as the foundation of your project."
+              />
             </div>
           </div>
-          <div className="flex flex-row justify-between">
-            <div className="rounded-lg bg-base-100 p-4">
-              <p className="text-[13px] font-semibold text-neutral-focus pb-4">
-                Features
-              </p>
-            </div>
-            <div className="rounded-lg bg-base-100 p-4">
-              <p className="text-[13px] font-semibold text-neutral-focus pb-4">
-                Tech Stack
-              </p>
-            </div>
-          </div>
-          <div className="rounded-lg bg-base-100 p-4">
-            <p className="text-[13px] font-semibold text-neutral-focus pb-4">
-              Resources
-            </p>
-          </div>
+          <DashboardWidget
+            imageLight="/img/share_link_light.png"
+            imageDark="/img/share_link_dark.png"
+            title="Share resources with your team"
+            link="Resources"
+            buttonTitle="Go to Resources"
+            description="Share links of helpful resources to your team for the Voyage. Contribute to the collective knowledgebase to empower your team."
+          />
         </div>
       </div>
     </div>
