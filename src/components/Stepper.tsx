a1d8270fc86dcece0ae2cd4eede7ae36aaeb3b5f
Winnie <winnieng3210@gmail.com>
2024-03-03 16:19:51 -0500

    add active state with variants

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React from "react";
 import Button from "@/components/Button";
 import { cn } from "@/lib/utils";
@@ -18,16 +17,23 @@ const statusColor = {
   current: "bg-success",
 };
 
+const statusVariant: Record<string, "neutral" | "primary"> = {
+  remaining: "neutral",
+  completed: "primary",
+  current: "primary",
+};
+
 export interface SteppersItem {
   icon?: React.ReactNode;
   status: StepperStatus;
   onClickEvent?: () => void;
   name: string;
+  isActive: boolean;
 }
 
 export interface StepperProps {
   styleType: StepperStyle;
-  stepperWidth: string;
+  stepperWidth?: string;
   steppers: SteppersItem[];
 }
 
@@ -37,43 +43,44 @@ export default function Stepper({
   steppers,
 }: StepperProps) {
   return (
-    <div className={`${stepperWidth}`}>
-      <div className="flex">
-        {steppers.map((step) => {
-          const { icon, status, onClickEvent, name } = step;
+    <div className="flex justify-center">
+      <div className={`${stepperWidth ? stepperWidth : "w-full"}`}>
+        <div className="flex">
+          {steppers.map((step) => {
+            const { icon, status, onClickEvent, name, isActive } = step;
 
-          return (
-            <div
-              key={name}
-              className={cn(
-                "flex flex-1 last:flex-none relative last:after:contents-none after:absolute after:content-[''] after:h-[7px] after:w-full after:top-1/2 after:-translate-y-1/2 after:-z-10",
-                "after:bg-base-100"
-              )}
-            >
-              {styleType === "icons" ? (
-                <Button
-                  onClick={onClickEvent}
-                  className={cn(
-                    "text-center",
-                    styleType === "icons" && "flex",
-                    statusColor[status]
-                  )}
-                >
-                  <div>{icon}</div>
-                  <span>{name}</span>
-                </Button>
-              ) : (
-                <div
-                  className={cn(
-                    "text-center w-[100px] h-[18px] rounded-full",
-                    status === "current" && "border-2 border-neutral h-[20px]",
-                    statusColor[status]
-                  )}
-                ></div>
-              )}
-            </div>
-          );
-        })}
+            return (
+              <div
+                key={name}
+                className={cn(
+                  "flex flex-1 last:flex-none relative last:after:contents-none after:absolute after:content-[''] after:h-[7px] after:w-full after:top-1/2 after:-translate-y-1/2 after:-z-10",
+                  "after:bg-base-100"
+                )}
+              >
+                {styleType === "icons" ? (
+                  <Button
+                    onClick={onClickEvent}
+                    className={cn("min-w-[75px]")}
+                    disabled={status === "remaining"}
+                    variant={isActive ? "outline" : statusVariant[status]}
+                  >
+                    {status === "current" && <div>{icon}</div>}
+                    <span>{name}</span>
+                  </Button>
+                ) : (
+                  <div
+                    className={cn(
+                      "text-center w-[100px] h-[18px] rounded-full",
+                      status === "current" &&
+                        "border-2 border-neutral h-[20px]",
+                      statusColor[status]
+                    )}
+                  ></div>
+                )}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
