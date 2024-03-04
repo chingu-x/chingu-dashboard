bebcf22ac327bf645d452a059000e2fc73a9e644
Timothy Russo <russotimothy@live.it>
2024-03-04 18:23:59 +0100

    fix: lint

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import PreVoyageDashboard from "./components/pre-voyage-dashboard/PreVoyageDashboard";
-import VoyageDashboard from "./components/voyage-dashboard/VoyageDashboard";
 
 function DashboardPage() {
-  // return <PreVoyageDashboard />;
-  return <VoyageDashboard />;
+  return <PreVoyageDashboard />;
 }
 
 export default DashboardPage;
