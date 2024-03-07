f8ec5c62dcc298ae6c5a99186bb7f15ac559b6bd
Dan Ko <danko56666@gmail.com>
2024-03-06 23:17:06 -0300

    adjust styling of error modal

--- src/components/modals/ErrorModal.tsx
+++ src/components/modals/ErrorModal.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useCallback } from "react";
+import { XMarkIcon } from "@heroicons/react/20/solid";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 import Modal from "./Modal";
 import Button from "@/components/Button";
 import { onCloseModal } from "@/store/features/modal/modalSlice";
@@ -18,9 +20,18 @@ export default function ErrorModal() {
   }, [dispatch]);
 
   return (
-    <Modal isOpen={isModalOpen} title={"Error"} onClose={handleClose}>
+    <Modal isOpen={isModalOpen} onClose={handleClose}>
+      <div className="flex items-center justify-between p-6 bg-error-content">
+        <div className="flex items-center">
+          <ExclamationTriangleIcon className="w-8 mr-2" />
+          <h3 className="text-xl font-semibold capitalize">Submission Error</h3>
+        </div>
+        <button type="button" aria-label="close modal" onClick={handleClose}>
+          <XMarkIcon className="w-6 h-6 fill-current" />
+        </button>
+      </div>
       <div className="flex flex-col overflow-hidden">
-        <div className="flex flex-col pr-2 mr-1 overflow-y-auto min-h-[90px]">
+        <div className="flex flex-col p-6 mr-1 overflow-y-auto min-h-[90px] relative">
           <div className="flex flex-col gap-4">{content.message}</div>
           <div className="flex flex-col gap-5 pt-8">
             <Button
