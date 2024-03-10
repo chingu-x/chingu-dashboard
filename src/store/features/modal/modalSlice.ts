3a30a2a611497c6dfc5e0e10c75cb92fc863ba35
Dan Ko <danko56666@gmail.com>
2024-03-10 18:20:03 -0300

    refactor modal slice to have required payloads for certain types

--- src/store/features/modal/modalSlice.ts
+++ src/store/features/modal/modalSlice.ts
@@ -12,13 +12,25 @@ interface ModalState {
   type: ModalType | undefined;
   isOpen: boolean;
   isEditing?: boolean;
-  content: ContentPayload;
+  content?: ContentPayload;
 }
 
-export interface ModalOpenActionPayload {
-  type: ModalType;
-  content: ContentPayload;
+export interface BaseModalOpenActionPayload {
+  type: Exclude<ModalType, "error" | "confirmation">;
+  content?: ContentPayload;
   isEditing?: boolean;
+}
+
+export interface ErrorModalOpenActionPayload
+  extends Omit<BaseModalOpenActionPayload, "type"> {
+  type: "error";
+  content: Required<Pick<ContentPayload, "message">>;
+}
+
+export interface ConfirmationModalOpenActionPayload
+  extends Omit<BaseModalOpenActionPayload, "type"> {
+  type: "confirmation";
+  content: Required<ContentPayload>;
 }
 
 export interface ContentPayload {
@@ -27,6 +39,11 @@ export interface ContentPayload {
   confirmationText?: string;
   cancelText?: string;
 }
+
+export type ModalOpenActionPayload =
+  | BaseModalOpenActionPayload
+  | ErrorModalOpenActionPayload
+  | ConfirmationModalOpenActionPayload;
 
 const initialState: ModalState = {
   type: undefined,
