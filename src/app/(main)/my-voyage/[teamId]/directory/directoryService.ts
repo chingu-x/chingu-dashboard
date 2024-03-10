4f0b6f69200150a09b451d4f05b15f0e1d4acf31
Dan Ko <danko56666@gmail.com>
2024-03-10 20:40:31 -0300

    implement edit hour server action

---
+++ src/app/(main)/my-voyage/[teamId]/directory/directoryService.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { revalidateTag } from "next/cache";
+import { getAccessToken } from "@/utils/getCookie";
+import { AsyncActionResponse, handleAsync } from "@/utils/handleAsync";
+import { PATCH } from "@/utils/requests";
+import { CacheTag } from "@/utils/cacheTag";
+
+interface EditHoursProps {
+  teamId: number;
+  hrPerSprint: number;
+}
+
+type EditHoursBody = Pick<EditHoursProps, "hrPerSprint">;
+
+interface EditHoursResponse extends EditHoursBody {
+  id: number;
+  userId: string;
+  voyageTeamId: number;
+  voyageRoleId: number;
+  statusId: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export async function editHours({
+  teamId,
+  hrPerSprint,
+}: EditHoursProps): Promise<AsyncActionResponse<EditHoursResponse>> {
+  const token = getAccessToken();
+
+  console.log(hrPerSprint, teamId);
+
+  const editHoursAsync = () =>
+    PATCH<EditHoursBody, EditHoursResponse>(
+      `api/v1/teams/${teamId}/members`,
+      token,
+      "default",
+      { hrPerSprint }
+    );
+
+  const [res, error] = await handleAsync(editHoursAsync);
+
+  if (res) {
+    revalidateTag(CacheTag.directory);
+  }
+
+  return [res, error];
+}
