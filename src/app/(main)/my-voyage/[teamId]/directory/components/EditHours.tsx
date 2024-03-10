b0be087e68ad9507b3520fb2e2cc54cda83f3107
Dan Ko <danko56666@gmail.com>
2024-03-10 19:40:16 -0300

    update placeholder text with actual hrs per sprint

--- src/app/(main)/my-voyage/[teamId]/directory/components/EditHours.tsx
+++ src/app/(main)/my-voyage/[teamId]/directory/components/EditHours.tsx
@@ -1,15 +1,20 @@
 "use client";
 
 import TextInput from "@/components/inputs/TextInput";
+import { useDirectory } from "@/store/hooks";
 
-export default function EditHours() {
+interface EditHoursProps {
+  hrPerSprint: number;
+}
+
+export default function EditHours({ hrPerSprint }: EditHoursProps) {
   return (
     <form onSubmit={() => {}}>
       <TextInput
         clearInputAction={() => {}}
         id="textInput"
         onChange={() => {}}
-        placeholder="Placeholder"
+        placeholder={`${hrPerSprint}`}
         submitButtonText="Save"
         value=""
       />
