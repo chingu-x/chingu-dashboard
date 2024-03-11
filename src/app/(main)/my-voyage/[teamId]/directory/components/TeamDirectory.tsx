ebd6efac83cc9b4ec9778726d10fdbb7d368254d
JaneMoroz <moroz.jane@yahoo.com>
2024-03-11 20:52:38 +0400

    style: update styles

--- src/app/(main)/my-voyage/[teamId]/directory/components/TeamDirectory.tsx
+++ src/app/(main)/my-voyage/[teamId]/directory/components/TeamDirectory.tsx
@@ -5,11 +5,11 @@ import TextInput from "@/components/inputs/TextInput";
 import { cn } from "@/lib/utils";
 
 // Temp:
-const currentUserId = "4";
+const currentUserId = "1";
 
 export default function TeamDirectory() {
   return (
-    <div className="flex flex-col w-full p-10 pb-4 bg-transparent border-separate border-none rounded-2xl 3xl:bg-secondary-content text-base-300 gap-y-10 3xl:gap-y-0">
+    <div className="flex flex-col w-full p-10 pb-4 bg-transparent border border-transparent border-base-100 rounded-2xl 3xl:bg-base-200 text-base-300 gap-y-10 3xl:gap-y-0">
       {/* header - table only */}
       <div className="items-center hidden mb-6 text-xl font-semibold text-base-300 3xl:grid 3xl:grid-cols-5">
         <h2>Name</h2>
@@ -27,7 +27,7 @@ export default function TeamDirectory() {
           <div
             key={teamMember.id}
             className={cn(
-              "flex flex-col items-center p-8 text-base font-medium 3xl:grid 3xl:grid-cols-5 text-base-300 bg-secondary-content 3xl:bg-transparent 3xl:p-0 rounded-2xl 3xl:rounded-none",
+              "flex flex-col items-center p-8 text-base font-medium border border-base-100 3xl:border-none 3xl:grid 3xl:grid-cols-5 text-base-300 bg-base-200 3xl:bg-transparent 3xl:p-0 rounded-2xl 3xl:rounded-none",
               isCurrentUser && "pt-8 pb-3",
             )}
           >
