0091019f6e2fab69208e68d38c42be4ba8ec6b16
Dan Ko <danko56666@gmail.com>
2024-03-11 22:26:09 -0300

    implement toggling of input

--- src/app/(main)/my-voyage/[teamId]/directory/components/EditHours.tsx
+++ src/app/(main)/my-voyage/[teamId]/directory/components/EditHours.tsx
@@ -2,6 +2,7 @@ import * as z from "zod";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useParams } from "next/navigation";
+import { PencilSquareIcon } from "@heroicons/react/24/outline";
 import TextInput from "@/components/inputs/TextInput";
 import { validateTextInput } from "@/helpers/form/validateInput";
 import { useAppDispatch } from "@/store/hooks";
@@ -9,9 +10,12 @@ import { editHours } from "@/app/(main)/my-voyage/[teamId]/directory/directorySe
 import { onOpenModal } from "@/store/features/modal/modalSlice";
 import useServerAction from "@/hooks/useServerAction";
 import Spinner from "@/components/Spinner";
+import Button from "@/components/Button";
 
 interface EditHoursProps {
   hrPerSprint: number;
+  isEditing: boolean;
+  handleclick: () => void;
 }
 
 const validationSchema = z.object({
@@ -24,7 +28,11 @@ const validationSchema = z.object({
 
 type ValidationSchema = z.infer<typeof validationSchema>;
 
-export default function EditHours({ hrPerSprint }: EditHoursProps) {
+export default function EditHours({
+  hrPerSprint,
+  isEditing,
+  handleclick,
+}: EditHoursProps) {
   const params = useParams<{ teamId: string }>();
   const teamId = +params.teamId;
   const dispatch = useAppDispatch();
@@ -56,12 +64,14 @@ export default function EditHours({ hrPerSprint }: EditHoursProps) {
     setEditHoursLoading(false);
   };
 
-  return (
+  function handleClearInputAction() {
+    reset({ avgHours: hrPerSprint.toString() });
+  }
+
+  return isEditing ? (
     <form onSubmit={handleSubmit(onSubmit)}>
       <TextInput
-        clearInputAction={() => {
-          reset({ avgHours: hrPerSprint.toString() });
-        }}
+        clearInputAction={handleClearInputAction}
         id="avgHours"
         {...register("avgHours")}
         errorMessage={errors.avgHours?.message}
@@ -71,5 +81,14 @@ export default function EditHours({ hrPerSprint }: EditHoursProps) {
         buttonDisabled={!isDirty || !isValid}
       />
     </form>
+  ) : (
+    <Button
+      variant="link"
+      className="flex justify-between border-2 w-full outline-none rounded-lg px-3.5 mt-2 mb-5 shadow-transparent shadow-[0px_0px_0px_3px] bg-base-200 text-base-300 disabled:cursor-not-allowed border-neutral/40 group-hover:border-neutral-focus group-hover:focus-visible:border-neutral/40 focus-visible:border-neutral/40 focus-visible:shadow-neutral/30 disabled:bg-base-100 disabled:group-hover:border-neutral/40"
+      onClick={handleclick}
+    >
+      Add Hours
+      <PencilSquareIcon className="w-3.5" />
+    </Button>
   );
 }
