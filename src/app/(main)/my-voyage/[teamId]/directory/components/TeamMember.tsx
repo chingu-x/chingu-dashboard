db9ec4dfbddbc9b5b16f4404000366dfcb7da3b3
Dan Ko <danko56666@gmail.com>
2024-03-11 22:26:09 -0300

    implement toggling of input

--- src/app/(main)/my-voyage/[teamId]/directory/components/TeamMember.tsx
+++ src/app/(main)/my-voyage/[teamId]/directory/components/TeamMember.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
 import TeamMemberDataItemWrapper from "./TeamMemberDataItemWrapper";
 import EditHours from "./EditHours";
 import { cn } from "@/lib/utils";
@@ -16,12 +17,31 @@ export default function TeamMember({ teamMember }: TeamMemberProps) {
   const { firstName, lastName, discordId, currentTime } = teamMember.member;
   const { id, hrPerSprint, voyageRole } = teamMember;
   const isCurrentUser = user.some((user) => user.id === id);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const newRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  });
+
+  const handleOutsideClick = (e: MouseEvent | TouchEvent) => {
+    if (newRef.current && !newRef.current.contains(e.target as Node)) {
+      setIsEditing(false);
+    }
+  };
+
+  function handleclick() {
+    setIsEditing(true);
+  }
 
   return (
     <div
       className={cn(
         "flex flex-col items-center p-8 text-base font-medium border border-base-100 3xl:border-none 3xl:grid 3xl:grid-cols-5 text-base-300 bg-base-200 3xl:bg-transparent 3xl:p-0 rounded-2xl 3xl:rounded-none",
-        isCurrentUser && "pt-8 pb-3",
+        isCurrentUser && "pt-8 pb-3"
       )}
     >
       <TeamMemberDataItemWrapper label="Name">
@@ -40,9 +60,16 @@ export default function TeamMember({ teamMember }: TeamMemberProps) {
         {voyageRole.name}
       </TeamMemberDataItemWrapper>
       <TeamMemberDataItemWrapper label="Average Hour/Sprint" className="pb-0">
-        <div className={`max-w-[227px] ${isCurrentUser && "pt-4"}`}>
+        <div
+          className={`max-w-[227px] ${isCurrentUser && "pt-4"}`}
+          ref={newRef}
+        >
           {isCurrentUser ? (
-            <EditHours hrPerSprint={hrPerSprint} />
+            <EditHours
+              hrPerSprint={hrPerSprint}
+              isEditing={isEditing}
+              handleclick={handleclick}
+            />
           ) : (
             hrPerSprint
           )}
