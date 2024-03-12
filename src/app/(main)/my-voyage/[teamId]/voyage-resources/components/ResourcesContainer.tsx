6e8f5550d4e640a14c7b80aff907c4f1fc3d9990
Matt <84277551+MattRueter@users.noreply.github.com>
2024-03-12 14:04:26 -0500

    add sorting functionality v-1.

--- src/app/(main)/my-voyage/[teamId]/voyage-resources/components/ResourcesContainer.tsx
+++ src/app/(main)/my-voyage/[teamId]/voyage-resources/components/ResourcesContainer.tsx
@@ -7,18 +7,52 @@ import EmptyBanner from "./EmptyBanner";
 import { resources } from "./fixtures/resources";
 //const resources = null // temp var for toggling empty banner or resource cards.
 
-export default function ResourcesContainer() {
-  const [byNewest, setByNewest] = useState(true);
+//TODO: Tidy up Types in ResourceComponentWrapper and ResourceContainer components.
+//Should come from Redux store. See Ideation for example.
+interface ResourcesContainerProps {
+  data: [
+    {
+      id: number;
+      teamMemberId: number;
+      url: string;
+      title: string;
+      createdAt: Date;
+      updatedAt: Date;
+      addedBy: { member: [Object] };
+    },
+  ];
+}
 
-  const sort = () => {
+export default function ResourcesContainer({ data }: ResourcesContainerProps) {
+  const [byNewest, setByNewest] = useState(true);
+  const [voyageResources, setVoyageResources] = useState(data);
+  console.log(voyageResources);
+  const handleClick = () => {
     setByNewest(!byNewest);
+    setVoyageResources(sortResources());
+  };
+  const sortResources = () => {
+    voyageResources.sort(function (a, b) {
+      if (byNewest) {
+        console.log("by newest true. Sorting by oldest.");
+        console.log("b: ", new Date(b.updatedAt));
+        console.log("a: ", new Date(a.updatedAt));
+        return new Date(b.updatedAt) - new Date(a.updatedAt);
+      } else {
+        console.log("Currently showing by oldest. Sorting by newest.");
+        console.log("a: ", new Date(a.updatedAt));
+        console.log("b: ", new Date(b.updatedAt));
+        return new Date(a.updatedAt) - new Date(b.updatedAt);
+      }
+    });
+    return voyageResources;
   };
 
   return (
     <>
       <div className="grid grid-cols-[1fr_150px] items-center">
         <ResourceInput />
-        <SortingButton onClick={sort} type={byNewest} />
+        <SortingButton onClick={handleClick} type={byNewest} />
       </div>
       {!resources ? (
         <EmptyBanner />
