3d68026648594c79ce99f7234d8cc0d9f1c59bd9
Jane Moroz <Moroz.jane@yahoo.com>
2024-03-23 18:25:08 +0300

    same

--- src/utils/getCurrentSprint.ts
+++ src/utils/getCurrentSprint.ts
@@ -1,15 +1,19 @@
-// import { addDays, isAfter, isBefore, parseISO } from "date-fns";
+import { isWithinInterval, parseISO } from "date-fns";
 import { Sprint } from "@/store/features/sprint/sprintSlice";
 
 export default function getCurrentSprint(sprints: Sprint[]) {
-  // TODO: uncomment later
-  // const currentDate = addDays(new Date(), 1);
-  // const currentSprint = sprints.filter(
-  //   (sprint) =>
-  //     isAfter(currentDate, parseISO(sprint.startDate)) &&
-  //     isBefore(currentDate, parseISO(sprint.endDate)),
-  // );
-  // return currentSprint[0];
-
-  return sprints[5];
+  // TODO: change to new Date() later
+  const currentDate = new Date("2024-02-05T00:00:00.000");
+  const currentSprint = sprints.find((sprint) => {
+    const startDate = parseISO(
+      sprint.startDate.substring(0, sprint.startDate.length - 1),
+    );
+    const endDate = parseISO(
+      sprint.endDate.substring(0, sprint.endDate.length - 1),
+    );
+    if (isWithinInterval(currentDate, { start: startDate, end: endDate }))
+      return true;
+    return false;
+  });
+  return currentSprint;
 }
