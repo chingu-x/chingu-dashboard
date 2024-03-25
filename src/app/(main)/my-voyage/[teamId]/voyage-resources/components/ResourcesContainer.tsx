6efe62ea99876b8ad9998d95dba582473a12789d
Matt <84277551+MattRueter@users.noreply.github.com>
2024-03-25 14:30:50 -0500

    get resources data from redux and display in ui.

--- src/app/(main)/my-voyage/[teamId]/voyage-resources/components/ResourcesContainer.tsx
+++ src/app/(main)/my-voyage/[teamId]/voyage-resources/components/ResourcesContainer.tsx
@@ -1,49 +1,71 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ResourceInput from "./ResourceInput";
 import SortingButton from "./SortingButton";
 import ResourceCard from "./ResourceCard";
 import EmptyBanner from "./EmptyBanner";
-import { resources } from "./fixtures/resources";
 import { ResourceData } from "@/store/features/resources/resourcesSlice";
-//const resources = null // temp var for toggling empty banner or resource cards based on 'fake data'
+import { useSelector } from "react-redux";
 
+type Resources ={
+  resources: ResourceData[]
+}
 interface ResourcesContainerProps {
-  data: ResourceData[];
+  resources: Resources
 }
 
-export default function ResourcesContainer({ data }: ResourcesContainerProps) {
-  const [byNewest, setByNewest] = useState(true);
+export default function ResourcesContainer() {
+  const [byNewest, setByNewest] = useState(true);  
   {
-    /*TODO: replace 'data' with call to Redux store resources slice.*/
-  }
-  const [voyageResources, setVoyageResources] = useState(data);
+    /*
+    TODO: 
+          1. getUser() to check if actual user or not (for deleting ability) *pass this from Wrapper parent server component.
+    */
+  }  
+  const [ voyageResources, setVoyageResources ]= useState(useSelector( (state: ResourcesContainerProps ) => state.resources.resources))
+  //const [ voyageResources, setVoyageResources ]= useState(null)
 
-  const handleClick = () => {
-    setVoyageResources(sortResources());
-    setByNewest(!byNewest);
-  };
+  const testList = voyageResources.map((item)=>{
+      return {
+        key: item.id,
+        title:item.title,
+        url:item.url,
+        user:{
+          name: item.addedBy.member.firstName,
+          image: item.addedBy.member.avatar
+        },
+        date: new Date(item.updatedAt).toString(),
+        currentUser:true //check if actual current user here if this is best ???      
+    }
+  });
 
-  const sortResources = () => {
-    voyageResources.sort(function (a, b) {
-      const prev = new Date(a.updatedAt);
-      const next = new Date(b.updatedAt);
-      if (byNewest) {
-        return next.getTime() - prev.getTime();
-      } else {
-        return prev.getTime() - next.getTime();
-      }
-    });
+const handleClick = () => {
+  setByNewest(!byNewest);
+};
 
-    return voyageResources;
-  };
+useEffect(() => {
+  setVoyageResources(sortResources());
+},[byNewest])
+
+const sortResources = () => {
+  const sortedResources = [...voyageResources].sort(function (a, b) {
+    const prev = new Date(a.updatedAt);
+    const next = new Date(b.updatedAt);
+    if (byNewest) {
+      return next.getTime() - prev.getTime();
+    } else {
+      return prev.getTime() - next.getTime();
+    }
+  });
+  return sortedResources;
+};
 
   return (
     <>
       <div className="grid grid-cols-[1fr_150px] items-center">
         <ResourceInput />
         {/*TODO: replace 'resources' (fake data) with 'voyageResources'*/}
-        {!resources ? (
+        {!voyageResources ? (
           <SortingButton
             onClick={handleClick}
             type={byNewest}
@@ -58,12 +80,12 @@ export default function ResourcesContainer({ data }: ResourcesContainerProps) {
         )}
       </div>
       {/*TODO: replace 'resources' (fake data) with 'voyageResources'*/}
-      {!resources ? (
+      {!voyageResources ? (
         <EmptyBanner />
       ) : (
-        resources.map((item) => (
+        testList.map((item) => (
           <ResourceCard
-            key={item.id}
+            key={item.key}
             title={item.title}
             user={item.user}
             currentUser={item.currentUser}
