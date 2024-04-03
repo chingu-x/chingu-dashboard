918cfb068fec13bcb0e72da491b9377639175ac7
Dan Ko <danko56666@gmail.com>
2024-04-02 21:37:58 -0400

    add finalize ideation button

--- src/app/(main)/my-voyage/[teamId]/ideation/components/CreateIdeationContainer.tsx
+++ src/app/(main)/my-voyage/[teamId]/ideation/components/CreateIdeationContainer.tsx
@@ -1,9 +1,10 @@
 import VoteDescriptionCard from "./VoteDescriptionCard";
 import AddProjectIdeaButton from "./AddProjectIdeaButton";
+import FinalizeIdeation from "./FinalizeIdeation";
 
 function CreateIdeationContainer() {
   return (
-    <div className="grid grid-cols-[180px_1fr] 3xl:grid-cols-[200px_1fr_200px] items-center justify-items-center gap-y-7 3xl:gap-x-[110px] 2xl:gap-x-20 gap-x-10 w-full p-10 bg-base-200 rounded-2xl">
+    <div className="grid grid-cols-[185px_1fr] 3xl:grid-cols-[200px_1fr_200px] items-center justify-items-center gap-y-7 3xl:gap-x-[110px] 2xl:gap-x-20 gap-x-10 w-full p-10 bg-base-200 rounded-2xl">
       <VoteDescriptionCard />
       <section className="w-full row-span-2 gap-y-7 3xl:row-auto">
         <h2 className="text-xl font-semibold text-base-300">
@@ -15,7 +16,10 @@ function CreateIdeationContainer() {
           bring to users.
         </p>
       </section>
-      <AddProjectIdeaButton />
+      <div className="flex flex-col gap-y-4">
+        <AddProjectIdeaButton />
+        <FinalizeIdeation />
+      </div>
     </div>
   );
 }
