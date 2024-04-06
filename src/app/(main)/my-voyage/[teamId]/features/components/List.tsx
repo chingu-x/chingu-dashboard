6448a05905ce49c8980498d241a1a1486b64c5a6
Dan Ko <danko56666@gmail.com>
2024-04-05 20:24:45 -0400

    fix alignment of title and card and remove unused styles

--- src/app/(main)/my-voyage/[teamId]/features/components/List.tsx
+++ src/app/(main)/my-voyage/[teamId]/features/components/List.tsx
@@ -36,54 +36,52 @@ export default function List({ id, title, features, currentUser }: ListProps) {
     );
   }
   return (
-    <div className="flex flex-col w-full px-8 py-10 font-semibold card bg-base-200 rounded-2xl text-base-300">
-      <div className="p-0 card-body gap-y-6">
-        <h4 className="mb-2 text-xl capitalize card-title">{title}</h4>
-        {/* Features container / drag and drop area */}
-        <Droppable droppableId={id}>
-          {(provided: DroppableProvided, snapshot: DroppableStateSnapshot) => (
-            <div
-              className={`max-h-[300px] overflow-y-auto overflow-x-hidden pr-2 rounded-lg ${
-                snapshot.draggingFromThisWith && "bg-secondary/20"
-              } ${snapshot.isDraggingOver && "bg-base-100/40"}`}
+    <div className="flex flex-col w-full px-8 py-10 font-semibold bg-base-200 rounded-2xl text-base-300">
+      <h4 className="mb-2 text-xl capitalize pl-3">{title}</h4>
+      {/* Features container / drag and drop area */}
+      <Droppable droppableId={id}>
+        {(provided: DroppableProvided, snapshot: DroppableStateSnapshot) => (
+          <div
+            className={`max-h-[300px] overflow-y-auto overflow-x-hidden px-3 rounded-lg ${
+              snapshot.draggingFromThisWith && "bg-secondary-focus"
+            } ${snapshot.isDraggingOver && "bg-base-content"}`}
+          >
+            <ul
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="flex flex-col"
             >
-              <ul
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-                className="flex flex-col"
-              >
-                {/* Empty list suggestion */}
-                {features.length === 0 && !snapshot.isDraggingOver && (
-                  <span className="text-neutral-focus bg-base-200 py-[14px] px-[22px] rounded-lg">
-                    Share your suggestions!
-                  </span>
-                )}
-                {features.map((feature, index) => (
-                  <Card
-                    key={feature.id}
-                    index={index}
-                    feature={feature}
-                    currentUserId={currentUser.id}
-                  />
-                ))}
-                {provided.placeholder}
-              </ul>
-            </div>
-          )}
-        </Droppable>
-        {/* Similiar to tech stack button, need to be a shared component */}
-        <Button
-          variant="link"
-          size="lg"
-          className="text-base h-10 justify-between p-0 w-full outline-none rounded-lg text-base-300 shadow-md border border-base-100 mt-6"
-          onClick={handleClick}
-        >
-          <div className="pl-3">Add Feature</div>
-          <div className="flex justify-center items-center w-12 h-full bg-neutral rounded-br-lg rounded-tr-lg">
-            <PlusCircleIcon className="w-6 h-6 text-base-200" />
+              {/* Empty list suggestion */}
+              {features.length === 0 && !snapshot.isDraggingOver && (
+                <span className="text-neutral-focus bg-base-200 py-[14px] px-[22px] rounded-lg">
+                  Share your suggestions!
+                </span>
+              )}
+              {features.map((feature, index) => (
+                <Card
+                  key={feature.id}
+                  index={index}
+                  feature={feature}
+                  currentUserId={currentUser.id}
+                />
+              ))}
+              {provided.placeholder}
+            </ul>
           </div>
-        </Button>
-      </div>
+        )}
+      </Droppable>
+      {/* Similiar to tech stack button, need to be a shared component */}
+      <Button
+        variant="link"
+        size="lg"
+        className="text-base h-10 justify-between p-0 w-full outline-none rounded-lg text-base-300 shadow-md border border-base-100 mt-6"
+        onClick={handleClick}
+      >
+        <div className="pl-3">Add Feature</div>
+        <div className="flex justify-center items-center w-12 h-full bg-neutral rounded-br-lg rounded-tr-lg">
+          <PlusCircleIcon className="w-6 h-6 text-base-200" />
+        </div>
+      </Button>
     </div>
   );
 }
