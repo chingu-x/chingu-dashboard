8ce799c669ad0bb40dc75023102eafa7db882584
Dan Ko <danko56666@gmail.com>
2024-04-05 20:24:45 -0400

    fix alignment of title and card and remove unused styles

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -26,7 +26,7 @@ export default async function Layout({ children }: LayoutProps) {
       <div className="relative flex flex-1 overflow-hidden">
         <Sidebar />
         <main className="flex flex-col items-center flex-1 w-full p-10 overflow-y-auto">
-          <div className="flex flex-col w-full max-w-[1353px] gap-y-10">
+          <div className="flex flex-col w-full max-w-[1500px] gap-y-10">
             {children}
           </div>
         </main>
