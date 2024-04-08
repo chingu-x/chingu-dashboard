b61e34783c906435145ceb7ce4d406801eab6009
Matt <84277551+MattRueter@users.noreply.github.com>
2024-04-08 16:19:07 -0500

    add delete logic to service file.

--- src/app/(main)/my-voyage/[teamId]/voyage-resources/resourcesService.ts
+++ src/app/(main)/my-voyage/[teamId]/voyage-resources/resourcesService.ts
@@ -2,10 +2,12 @@
 
 import { revalidateTag } from "next/cache";
 import { getAccessToken } from "@/utils/getCookie";
-import { POST } from "@/utils/requests";
+import { POST, DELETE } from "@/utils/requests";
 import { AsyncActionResponse, handleAsync } from "@/utils/handleAsync";
 import { CacheTag } from "@/utils/cacheTag";
 
+//TODO: Cleanup TS interfaces
+console.log("clean up TS interfaces!");
 interface ResourceProps {
   teamId: number;
 }
@@ -27,6 +29,10 @@ export interface AddResourceResponse extends ResourceResponse {
   url: string;
   title: string;
 }
+export interface DeleteResourceProps {
+  resourceId: number | undefined;
+}
+export interface DeleteResourceResponse extends AddResourceResponse {}
 
 export async function addResource({
   teamId,
@@ -44,6 +50,26 @@ export async function addResource({
     );
 
   const [res, error] = await handleAsync(addResourceAsync);
+
+  if (res) {
+    revalidateTag(CacheTag.resources);
+  }
+
+  return [res, error];
+}
+
+export async function deleteResource({
+  resourceId,
+}: DeleteResourceProps): Promise<AsyncActionResponse<AddResourceResponse>> {
+  const token = getAccessToken();
+  console.log(token);
+  const deleteResourceAsync = () =>
+    DELETE<AddResourceResponse>(
+      `api/v1/voyages/resources/${resourceId}`,
+      token,
+      "default",
+    );
+  const [res, error] = await handleAsync(deleteResourceAsync);
 
   if (res) {
     revalidateTag(CacheTag.resources);
