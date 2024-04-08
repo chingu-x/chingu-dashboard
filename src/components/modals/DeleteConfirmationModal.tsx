74bc4792d95e69d3301c4306c8df421da15d06cc
Matt <84277551+MattRueter@users.noreply.github.com>
2024-04-08 16:27:01 -0500

    add condition to DeleteConfirmationModal to determine what to delete.

--- src/components/modals/DeleteConfirmationModal.tsx
+++ src/components/modals/DeleteConfirmationModal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useCallback } from "react";
-import { useParams, useRouter } from "next/navigation";
+import { useParams, useRouter, usePathname } from "next/navigation";
 import { TrashIcon } from "@heroicons/react/20/solid";
 import Modal from "./Modal";
 import Spinner from "@/components/Spinner";
@@ -11,14 +11,23 @@ import { useAppDispatch, useModal } from "@/store/hooks";
 import useServerAction from "@/hooks/useServerAction";
 import { deleteIdeation } from "@/app/(main)/my-voyage/[teamId]/ideation/ideationService";
 import routePaths from "@/utils/routePaths";
+import { deleteResource } from "@/app/(main)/my-voyage/[teamId]/voyage-resources/resourcesService";
 
 export default function DeleteConfirmationModal() {
-  const { isOpen, type } = useModal();
+  const { isOpen, type, id } = useModal();
   const modal = useModal();
   const dispatch = useAppDispatch();
   const params = useParams<{ teamId: string; ideationId: string }>();
   const teamId = +params.teamId;
+  const path = usePathname();
   const router = useRouter();
+
+  let handleDelete;
+  const {
+    runAction: deleteResourceAction,
+    isLoading: deleteResourceLoading,
+    setIsLoading: setDeleteResourceLoading,
+  } = useServerAction(deleteResource);
 
   const {
     runAction: deleteIdeationAction,
@@ -26,38 +35,59 @@ export default function DeleteConfirmationModal() {
     setIsLoading: setDeleteIdeationLoading,
   } = useServerAction(deleteIdeation);
 
+  if (path === `/my-voyage/${teamId}/voyage-resources`) {
+    console.log("delete resource branch");
+    //delete resource logic here....
+    handleDelete = useCallback(async () => {
+      const [res, error] = await deleteResourceAction({ resourceId: id });
+
+      if (res) {
+        dispatch(onCloseModal());
+      }
+
+      if (error) {
+        dispatch(
+          onOpenModal({ type: "error", content: { message: error.message } }),
+        );
+        setDeleteResourceLoading(false);
+      }
+    }, [deleteResourceAction, dispatch, id, setDeleteResourceLoading]);
+  } else {
+    console.log("delete ideation branch.");
+    //delete ideation logic here....
+    handleDelete = useCallback(async () => {
+      const ideationId = +params.ideationId;
+      const [res, error] = await deleteIdeationAction({ teamId, ideationId });
+
+      if (res) {
+        dispatch(onCloseModal());
+        router.push(routePaths.ideationPage(teamId.toString()));
+      }
+
+      if (error) {
+        dispatch(
+          onOpenModal({ type: "error", content: { message: error.message } }),
+        );
+        setDeleteIdeationLoading(false);
+      }
+    }, [
+      dispatch,
+      deleteIdeationAction,
+      params.ideationId,
+      router,
+      setDeleteIdeationLoading,
+      teamId,
+    ]);
+  }
+
   const isModalOpen = isOpen && type === "confirmation";
 
   const handleClose = () => {
     dispatch(onCloseModal());
   };
 
-  const handleDelete = useCallback(async () => {
-    const ideationId = +params.ideationId;
-    const [res, error] = await deleteIdeationAction({ teamId, ideationId });
-
-    if (res) {
-      dispatch(onCloseModal());
-      router.push(routePaths.ideationPage(teamId.toString()));
-    }
-
-    if (error) {
-      dispatch(
-        onOpenModal({ type: "error", content: { message: error.message } })
-      );
-      setDeleteIdeationLoading(false);
-    }
-  }, [
-    dispatch,
-    deleteIdeationAction,
-    params.ideationId,
-    router,
-    setDeleteIdeationLoading,
-    teamId,
-  ]);
-
   function renderDeleteButtonContent() {
-    if (deleteIdeationLoading) {
+    if (deleteIdeationLoading || deleteResourceLoading) {
       return <Spinner />;
     }
 
@@ -92,7 +122,7 @@ export default function DeleteConfirmationModal() {
           size="lg"
           variant="error"
           type="button"
-          disabled={deleteIdeationLoading}
+          disabled={deleteIdeationLoading || deleteResourceLoading}
           onClick={handleDelete}
           className="w-1/2"
         >
