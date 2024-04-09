10540bc8103869bdbaf139cb50c86ca3895964de
Dan Ko <danko56666@gmail.com>
2024-04-09 18:47:02 -0400

    associate feature cards with current user

--- src/app/(main)/my-voyage/[teamId]/features/components/Card.tsx
+++ src/app/(main)/my-voyage/[teamId]/features/components/Card.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-// import Image from "next/image";
+import Image from "next/image";
 import { Draggable, DraggableProvided } from "@hello-pangea/dnd";
-// import { EllipsisVerticalIcon } from "@heroicons/react/24/solid";
+import { EllipsisVerticalIcon } from "@heroicons/react/24/solid";
 
 import { Feature } from "./fixtures/Features";
+import { useUser } from "@/store/hooks";
+import Avatar from "@/components/avatar/Avatar";
 
 // import { useAppDispatch } from "@/store/hooks";
 // import { onOpenModal } from "@/store/features/modal/modalSlice";
@@ -15,6 +17,13 @@ interface CardProps {
 }
 
 export default function Card({ feature, index }: CardProps) {
+  const userId = useUser().id;
+  const {
+    addedBy: {
+      member: { id, firstName, lastName, avatar },
+    },
+  } = feature;
+  const isCurrentUser = userId === id;
   // const dispatch = useAppDispatch();
   // const userFullName =
   //   feature.addedBy.member.firstName + " " + feature.addedBy.member.lastName;
@@ -44,17 +53,15 @@ export default function Card({ feature, index }: CardProps) {
           <div className="flex items-center justify-between">
             <div className="flex flex-col gap-y-1">
               <h5 className="text-base font-semibold">{feature.description}</h5>
-              {/* <span className="text-[10px] text-neutral">{`Added by ${
-                feature.addedBy.member.id === currentUserId
-                  ? "you"
-                  : userFullName
-              }`}</span> */}
+              <span className="text-[10px] text-neutral">{`Added by ${
+                isCurrentUser ? "you" : firstName + " " + lastName
+              }`}</span>
             </div>
-            {/* {feature.addedBy.member.id === currentUserId ? (
+            {isCurrentUser ? (
               // Edit Button
               <button
                 type="button"
-                onClick={handleClick}
+                // onClick={handleClick}
                 className="p-0 m-0 bg-transparent border-none hover:bg-transparent gap-x-0"
                 aria-label="feature menu"
               >
@@ -62,16 +69,12 @@ export default function Card({ feature, index }: CardProps) {
               </button>
             ) : (
               // Creator's avatar
-              <div className="overflow-hidden rounded-full">
-                <Image
-                  width={24}
-                  height={24}
-                  style={{ objectFit: "cover" }}
-                  src={feature.addedBy.member.avatar}
-                  alt={userFullName}
-                />
-              </div>
-            )} */}
+              <Avatar
+                image={avatar}
+                width={24}
+                height={24}
+              />
+            )}
           </div>
         </li>
       )}
