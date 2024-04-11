cba213f990d7cb3fc58c449c802111ca8a4b621a
Dan Ko <danko56666@gmail.com>
2024-04-11 19:05:55 -0400

    add disabled state to finalize ideation button when ideation is empty

--- src/app/(main)/my-voyage/[teamId]/ideation/components/FinalizeIdeationButton.tsx
+++ src/app/(main)/my-voyage/[teamId]/ideation/components/FinalizeIdeationButton.tsx
@@ -4,13 +4,23 @@ import Link from "next/link";
 import { useParams } from "next/navigation";
 import Button from "@/components/Button";
 import routePaths from "@/utils/routePaths";
+import { useIdeation } from "@/store/hooks";
 
 export default function FinalizeIdeationButton() {
   const { teamId } = useParams<{ teamId: string }>();
+  const ideation = useIdeation().projectIdeas;
 
   return (
-    <Link href={routePaths.finalizeIdeationPage(teamId)} className="w-full">
-      <Button variant="secondary" size="lg" className="w-full">
+    <Link
+      href={routePaths.finalizeIdeationPage(teamId)}
+      className="w-full"
+    >
+      <Button
+        variant="secondary"
+        size="lg"
+        className="w-full"
+        disabled={ideation.length === 0}
+      >
         Finalize Selection
       </Button>
     </Link>
