64426dfdaea86ac867b8d98d30e02400bb090466
Dan Ko <danko56666@gmail.com>
2024-04-20 14:53:06 -0400

    fix flex styling

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -28,8 +28,8 @@ export default async function Layout({ children }: LayoutProps) {
       </Navbar>
       <div className="relative flex flex-1 overflow-hidden">
         <Sidebar />
-        <main className="flex flex-col flex-1 w-full p-10 overflow-y-auto">
-          <div className="flex flex-col items-center w-full max-w-[1500px] gap-y-10">
+        <main className="flex flex-col items-center flex-1 w-full p-10 overflow-y-auto">
+          <div className="flex flex-col w-full max-w-[1500px] gap-y-10">
             {children}
           </div>
         </main>
