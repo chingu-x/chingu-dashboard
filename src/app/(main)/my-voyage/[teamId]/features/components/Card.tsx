144fcfc67be971a57147d840966f96ce956e9208
Dan Ko <danko56666@gmail.com>
2024-04-19 20:26:26 -0400

    close popover when clicking outside

--- src/app/(main)/my-voyage/[teamId]/features/components/Card.tsx
+++ src/app/(main)/my-voyage/[teamId]/features/components/Card.tsx
@@ -2,7 +2,7 @@
 
 import { Draggable, DraggableProvided } from "@hello-pangea/dnd";
 import { EllipsisVerticalIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Feature } from "./fixtures/Features";
 import EditPopover from "./EditPopover";
 import { useUser } from "@/store/hooks";
@@ -15,6 +15,7 @@ interface CardProps {
 
 export default function Card({ feature, index }: CardProps) {
   const [showPopover, setShowPopover] = useState<boolean>(false);
+  const newRef = useRef<HTMLDivElement>(null);
   const userId = useUser().id;
   const {
     addedBy: {
@@ -27,46 +28,63 @@ export default function Card({ feature, index }: CardProps) {
     setShowPopover(true);
   }
 
+  function handleOutsideClick(e: MouseEvent | TouchEvent) {
+    if (newRef.current && !newRef.current.contains(e.target as Node)) {
+      setShowPopover(false);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  });
+
   return (
     <Draggable
       draggableId={feature.id.toString()}
       index={index}
     >
       {(provided: DraggableProvided) => (
-        <li
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          className="relative bg-base-100 py-[14px] px-[22px] rounded-lg text-base-300 my-3"
-        >
-          {showPopover && <EditPopover />}
-          <div className="flex items-center justify-between">
-            <div className="flex flex-col gap-y-1">
-              <h5 className="text-base font-semibold">{feature.description}</h5>
-              <span className="text-[10px] text-neutral">{`Added by ${
-                isCurrentUser ? "you" : firstName + " " + lastName
-              }`}</span>
+        <div ref={newRef}>
+          <li
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            className="relative bg-base-100 py-[14px] px-[22px] rounded-lg text-base-300 my-3"
+          >
+            {showPopover && <EditPopover />}
+            <div className="flex items-center justify-between">
+              <div className="flex flex-col gap-y-1">
+                <h5 className="text-base font-semibold">
+                  {feature.description}
+                </h5>
+                <span className="text-[10px] text-neutral">{`Added by ${
+                  isCurrentUser ? "you" : firstName + " " + lastName
+                }`}</span>
+              </div>
+              {isCurrentUser ? (
+                // Edit Button
+                <button
+                  type="button"
+                  onClick={handleClick}
+                  className="p-0 m-0 bg-transparent border-none hover:bg-transparent gap-x-0"
+                  aria-label="feature menu"
+                >
+                  <EllipsisVerticalIcon className="w-5 h-5 text-base-300" />
+                </button>
+              ) : (
+                // Creator's avatar
+                <Avatar
+                  image={avatar}
+                  width={24}
+                  height={24}
+                />
+              )}
             </div>
-            {isCurrentUser ? (
-              // Edit Button
-              <button
-                type="button"
-                onClick={handleClick}
-                className="p-0 m-0 bg-transparent border-none hover:bg-transparent gap-x-0"
-                aria-label="feature menu"
-              >
-                <EllipsisVerticalIcon className="w-5 h-5 text-base-300" />
-              </button>
-            ) : (
-              // Creator's avatar
-              <Avatar
-                image={avatar}
-                width={24}
-                height={24}
-              />
-            )}
-          </div>
-        </li>
+          </li>
+        </div>
       )}
     </Draggable>
   );
