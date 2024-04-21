cf5ebd33f0c6f1db403636823a79b508702e55e3
JaneMoroz <moroz.jane@yahoo.com>
2024-04-21 20:16:01 +0300

    feature: add user/currentTeam check to forms pages

--- src/app/(main)/my-voyage/[teamId]/sprints/[sprintNumber]/meeting/[meetingId]/agendas/[agendaId]/edit/page.tsx
+++ src/app/(main)/my-voyage/[teamId]/sprints/[sprintNumber]/meeting/[meetingId]/agendas/[agendaId]/edit/page.tsx
@@ -1,5 +1,29 @@
+import { redirect } from "next/navigation";
 import TopicForm from "@/myVoyage/sprints/components/forms/AgendaTopicForm";
+import { getCurrentVoyageTeam } from "@/utils/getCurrentVoyageTeam";
+import routePaths from "@/utils/routePaths";
+import { getUser } from "@/utils/getUser";
 
-export default function EditTopicPage() {
-  return <TopicForm />;
+interface EditTopicPageProps {
+  params: {
+    teamId: string;
+  };
+}
+
+export default async function EditTopicPage({ params }: EditTopicPageProps) {
+  const teamId = Number(params.teamId);
+
+  const [user, error] = await getUser();
+
+  const { err, currentTeam } = getCurrentVoyageTeam({ user, error, teamId });
+
+  if (err) {
+    return err;
+  }
+
+  if (currentTeam) {
+    return <TopicForm />;
+  }
+
+  redirect(routePaths.dashboardPage());
 }
