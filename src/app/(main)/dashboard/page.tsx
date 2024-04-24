14844bad484995426e2fe908be65a84a7a5b83f3
Timothy Russo <russotimothy@live.it>
2024-04-24 08:03:09 +0200

    feat: dashboard redirect

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,9 +1,23 @@
+import { redirect } from "next/navigation";
 import PreVoyageDashboard from "./components/pre-voyage-dashboard/PreVoyageDashboard";
 import VoyageDashboardPage from "./components/voyage-dashboard/VoyageDashboardPage";
+import { getUser } from "@/utils/getUser";
 
-function DashboardPage() {
+async function DashboardPage() {
   // TODO: Mocked temporary value
   const isVoyageStarted = true;
+
+  const [user] = await getUser();
+
+  const teamMember = user?.voyageTeamMembers.find(
+    (voyage) => voyage.voyageTeam.voyage.status.name === "Active",
+  );
+
+  const teamId = teamMember?.voyageTeamId;
+
+  if (teamId && isVoyageStarted) {
+    redirect(`/dashboard/${teamId}`);
+  }
 
   return isVoyageStarted ? <VoyageDashboardPage /> : <PreVoyageDashboard />;
 }
