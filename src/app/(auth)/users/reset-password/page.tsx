59c2efc9b14c53d1a63486009879399798528c8d
Winnie <winnieng3210@gmail.com>
2024-04-30 17:52:36 -0400

    banner

--- src/app/(auth)/users/reset-password/page.tsx
+++ src/app/(auth)/users/reset-password/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ResetPasswordContainer from "@/app/(auth)/users/components/ResetPasswordContainer";
-
+import AuthBannerContainer from "@/app/(auth)/components/AuthBannerContainer";
 export default function ResetPasswordPage() {
-  return <ResetPasswordContainer />;
+  return (
+    <div className="flex flex-col xl:flex-row items-center justify-center">
+      <AuthBannerContainer />
+      <ResetPasswordContainer />
+    </div>
+  );
 }
