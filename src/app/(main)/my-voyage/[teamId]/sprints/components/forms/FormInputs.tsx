326fb7528a26e974851c282d4b5076b806f72bae
Jane Moroz <Moroz.jane@yahoo.com>
2024-05-06 17:15:48 +0300

    feat: add `radioGroup` form input

--- src/app/(main)/my-voyage/[teamId]/sprints/components/forms/FormInputs.tsx
+++ src/app/(main)/my-voyage/[teamId]/sprints/components/forms/FormInputs.tsx
@@ -10,8 +10,7 @@ import Textarea from "@/components/inputs/Textarea";
 import RadioGroupVertical from "@/components/inputs/RadioGroup/RadioGroupVertical";
 import CheckboxGroupVertical from "@/components/inputs/CheckBoxGroup/CheckboxGroupVertical";
 import { RadioGroupItemProps } from "@/components/inputs/RadioGroup/RadioGroupItem";
-
-import { getQuestionType } from "@/utils/getQuestionType";
+import RadioGroupHorizontal from "@/components/inputs/RadioGroup/RadioGroupHorizontal";
 
 interface FormInputsProps {
   question: Question;
@@ -28,48 +27,102 @@ export default function FormInputs({
   register,
   errors,
 }: FormInputsProps) {
-  const { id, text, description, optionGroup } = question;
-  const [isRadioGroup, isCheckboxGroup, isTextArea] = getQuestionType(question);
+  const {
+    inputType: { name },
+    id,
+    text,
+    description,
+    optionGroup,
+    subQuestions,
+  } = question;
   const options: RadioGroupItemProps[] = [];
 
-  if (optionGroup && optionGroup.optionChoices.length !== 0) {
+  if (
+    name !== "radioGroup" &&
+    optionGroup &&
+    optionGroup.optionChoices.length !== 0
+  ) {
     optionGroup.optionChoices.forEach((option) => {
       const id = option.id.toString();
       options.push({ id, label: option.text, value: id });
     });
   }
-  return (
-    <>
-      {/* Radio Group */}
-      {isRadioGroup && (
-        <FormItem>
-          <Label className="font-semibold normal-case">{text}</Label>
-          <RadioGroupVertical options={options} {...register(id.toString())} />
-        </FormItem>
-      )}
-      {/* CheckBox Group */}
-      {isCheckboxGroup && (
-        <FormItem>
-          <Label className="font-semibold normal-case">{text}</Label>
-          <CheckboxGroupVertical
-            options={options}
-            {...register(id.toString())}
-          />
-        </FormItem>
-      )}
-      {/* TextArea */}
-      {isTextArea && (
-        <FormItem isTextField>
-          <Label className="font-semibold normal-case">{text}</Label>
-          <Textarea
-            id={`input${id.toString()}`}
-            placeholder={description ? description : "Your answer"}
-            {...register(id.toString())}
-            errorMessage={errors[id.toString()]?.message}
-            rows={2}
-          />
-        </FormItem>
-      )}
-    </>
-  );
+
+  if (name === "radio") {
+    return (
+      <FormItem isError={!!errors[id.toString()]}>
+        <Label className="font-semibold normal-case">{text}</Label>
+        <RadioGroupVertical options={options} {...register(id.toString())} />
+      </FormItem>
+    );
+  }
+
+  if (name === "radioGroup" && subQuestions.length !== 0) {
+    return (
+      <FormItem isError={!!errors[id.toString()]}>
+        <Label className="font-semibold normal-case">{text}</Label>
+        {/* TOP LABELS */}
+        <div className="flex flex-col gap-y-5">
+          <div className="w-full grid grid-cols-[150px_1fr] gap-x-4 items-center justify-between">
+            <span></span>
+            <div className="flex justify-between pl-6 pr-4">
+              {optionGroup?.optionChoices.map(({ text }) => (
+                <span
+                  key={text}
+                  className="text-base font-medium text-base-300"
+                >
+                  {text}
+                </span>
+              ))}
+            </div>
+          </div>
+          {subQuestions.map((question) => {
+            const { id: questionId, text } = question;
+            // Create an option group, ids must be unique even though labels and values are the same
+            const options: RadioGroupItemProps[] = [];
+
+            if (optionGroup && optionGroup.optionChoices.length !== 0) {
+              optionGroup.optionChoices.forEach((option) => {
+                const id = questionId + "" + option.id.toString();
+                options.push({ id, label: option.text, value: id });
+              });
+            }
+
+            return (
+              <RadioGroupHorizontal
+                key={text}
+                title={text}
+                options={options}
+                {...register(questionId.toString())}
+              />
+            );
+          })}
+        </div>
+      </FormItem>
+    );
+  }
+
+  if (name === "checkbox") {
+    return (
+      <FormItem>
+        <Label className="font-semibold normal-case">{text}</Label>
+        <CheckboxGroupVertical options={options} {...register(id.toString())} />
+      </FormItem>
+    );
+  }
+
+  if (name === "text") {
+    return (
+      <FormItem isTextField>
+        <Label className="font-semibold normal-case">{text}</Label>
+        <Textarea
+          id={`input${id.toString()}`}
+          placeholder={description ? description : "Your answer"}
+          {...register(id.toString())}
+          errorMessage={errors[id.toString()]?.message}
+          rows={2}
+        />
+      </FormItem>
+    );
+  }
 }
