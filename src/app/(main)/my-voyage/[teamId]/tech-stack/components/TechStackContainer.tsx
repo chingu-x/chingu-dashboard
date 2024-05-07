afd35480bd49dce4d2417538b82246ad74a5f949
Matt <84277551+MattRueter@users.noreply.github.com>
2024-05-07 12:13:24 -0500

    populate TechStackCard with data.

--- src/app/(main)/my-voyage/[teamId]/tech-stack/components/TechStackContainer.tsx
+++ src/app/(main)/my-voyage/[teamId]/tech-stack/components/TechStackContainer.tsx
@@ -1,5 +1,4 @@
 import TechStackCard from "./TechStackCard";
-import { techStack } from "./fixtures/TechStack";
 import { TechStackData } from "@/store/features/techStack/techStackSlice";
 
 interface TechStackContainerProps {
@@ -7,17 +6,16 @@ interface TechStackContainerProps {
 }
 
 export default function TechStackContainer({ data }: TechStackContainerProps) {
-  console.log(data);
+  const techCardData = data.map((item) => {
+    return { title: item.name, techItems: item.teamTechStackItems };
+  });
 
   return (
     <div className="card bg-secondary-content p-10 w-full">
       <ul className="grid grid-cols-2 min-[1920px]:grid-cols-3 gap-10 place-items-center">
-        {Object.keys(techStack).map((cardType) => (
-          <li key={cardType}>
-            <TechStackCard
-              title={cardType}
-              data={techStack[cardType as keyof typeof techStack]}
-            />
+        {techCardData.map((item, index) => (
+          <li key={index}>
+            <TechStackCard title={item.title} data={item.techItems} />
           </li>
         ))}
       </ul>
