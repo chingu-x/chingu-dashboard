b35b5bdcfbfff82d2a3fa229c4cdf1762d434ff2
Jane Moroz <Moroz.jane@yahoo.com>
2024-05-08 16:56:12 +0300

    fix: prettier

--- src/utils/convertStringToDate.ts
+++ src/utils/convertStringToDate.ts
@@ -3,7 +3,7 @@ import { utcToZonedTime } from "date-fns-tz";
 // Returns a Date which will format as a specific UTC time or date in the given time zone (a user's timezone).
 export default function convertStringToDate(
   dateTime: string,
-  timezone: string
+  timezone: string,
 ) {
   return utcToZonedTime(dateTime, timezone);
 }
