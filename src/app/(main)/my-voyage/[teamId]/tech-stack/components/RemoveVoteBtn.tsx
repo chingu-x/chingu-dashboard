0a0e64adad04ca6e8d66bbd324ce2ee7a3400fee
Matt <84277551+MattRueter@users.noreply.github.com>
2024-05-09 12:08:20 -0500

    move smaller componets from TechStackCard to separate file.

---
+++ src/app/(main)/my-voyage/[teamId]/tech-stack/components/RemoveVoteBtn.tsx
@@ -0,0 +1,13 @@
+import Button from "@/components/Button";
+
+export default function RemoveVoteBtn() {
+  return (
+    <Button
+      variant="error"
+      size="xs"
+      className="w-11/12 rounded-3xl col-span-2 justify-self-end"
+    >
+      Remove Vote
+    </Button>
+  );
+}
