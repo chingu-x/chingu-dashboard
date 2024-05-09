9fe71d1c10e7e0607392ce4a6e37d820912f9f43
Matt <84277551+MattRueter@users.noreply.github.com>
2024-05-08 19:15:25 -0500

    update TechStackCard to match design changes (vote & remove-vote buttons & add tech button/form).

--- src/app/(main)/my-voyage/[teamId]/tech-stack/components/TechStackCard.tsx
+++ src/app/(main)/my-voyage/[teamId]/tech-stack/components/TechStackCard.tsx
@@ -1,8 +1,19 @@
-import { PlusCircleIcon } from "@heroicons/react/24/outline";
+"use client";
+import { useRef, useState, FormEvent } from "react";
 import AvatarGroup from "@/components/avatar/AvatarGroup";
 import Avatar from "@/components/avatar/Avatar";
 import Button from "@/components/Button";
 import { TechStackItem } from "@/store/features/techStack/techStackSlice";
+import TextInput from "@/components/inputs/TextInput";
+import { useUser } from "@/store/hooks";
+import {
+  ComputerDesktopIcon,
+  SwatchIcon,
+  CodeBracketSquareIcon,
+  ChartPieIcon,
+  CloudIcon,
+  ServerStackIcon,
+} from "@heroicons/react/24/solid";
 
 interface TechStackCardProps {
   title: string;
@@ -10,9 +21,59 @@ interface TechStackCardProps {
 }
 
 export default function TechStackCard({ title, data }: TechStackCardProps) {
+  const [isInput, setIsInput] = useState(false);
+  const [isDuplicate, setIsDuplicate] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const items = data.map((item) => item.name.toLowerCase());
+  const userId = useUser().id;
+
+  function getIcon(cardTitle: string) {
+    if (cardTitle === "Frontend") {
+      return <ComputerDesktopIcon className="w-1/12 h-1/12 mr-2" />;
+    }
+    if (cardTitle === "CSS Library") {
+      return <SwatchIcon className="w-1/12 h-1/12 mr-2" />;
+    }
+    if (cardTitle === "Backend") {
+      return <CodeBracketSquareIcon className="w-1/12 h-1/12 mr-2" />;
+    }
+    if (cardTitle === "Project Management") {
+      return <ChartPieIcon className="w-1/12 h-1/12 mr-2" />;
+    }
+    if (cardTitle === "Cloud Provider") {
+      return <CloudIcon className="w-1/12 h-1/12 mr-2" />;
+    }
+    if (cardTitle === "Hosting") {
+      return <ServerStackIcon className="w-1/12 h-1/12 mr-2" />;
+    }
+  }
+
+  const toggleInput = () => {
+    setIsInput(!isInput);
+  };
+
+  const clearAction = () => {
+    setIsInput(!isInput);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  const handleOnChange = () => {
+    const currentValue = inputRef.current?.value.toLowerCase();
+    if (currentValue && !isDuplicate) {
+      items.includes(currentValue) ? setIsDuplicate(true) : null;
+    }
+    if (currentValue && isDuplicate) {
+      items.includes(currentValue) ? null : setIsDuplicate(false);
+    }
+  };
+
   return (
     <div className="card min-w-[400px] sm:w-96 text-base-300 bg-base-200 rounded-lg px-6 py-5">
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row justify-start">
+        {getIcon(title)}
         <h3 className="self-center text-xl font-semibold text-base-300">
           {title}
         </h3>
@@ -21,7 +82,7 @@ export default function TechStackCard({ title, data }: TechStackCardProps) {
         <ul className="text-base-300">
           {data.map((element) => (
             <li
-              className="text-base mb-5 last:mb-0 grid grid-cols-3 items-center"
+              className="text-base mb-5 grid grid-cols-3 items-center"
               key={element.id}
             >
               {element.name}
@@ -35,20 +96,52 @@ export default function TechStackCard({ title, data }: TechStackCardProps) {
                   />
                 ))}
               </AvatarGroup>
-              <button
-                type="button"
-                className="capitalize w-[62px] h-[32px] p-0 min-h-full text-xs font-medium text-base-300 bg-primary-content border-transparent mr-4 rounded-[32px] hover:bg-primary hover:border-transparent gap-x-0 place-self-end"
-              >
-                Vote
-              </button>
+              {/*Check to see if current user has voted on item*/}
+              {element.teamTechStackItemVotes
+                .map((item) => item.votedBy.member.id)
+                .includes(userId) ? (
+                <Button
+                  variant="error"
+                  size="xs"
+                  className="p-2 rounded-3xl last:justify-self-end "
+                >
+                  Remove Vote
+                </Button>
+              ) : (
+                <Button
+                  variant="primary"
+                  size="xs"
+                  className="rounded-3xl rounded-3xl last:justify-self-end "
+                >
+                  Add Vote
+                </Button>
+              )}
             </li>
           ))}
         </ul>
       </div>
-      <Button variant="secondary" className="justify-start w-full">
-        <PlusCircleIcon className="h-[18px] w-[18px] text-base-300" />
-        Add Tech Stack
-      </Button>
+      {isInput ? (
+        <form onSubmit={handleSubmit}>
+          <TextInput
+            id={title}
+            ref={inputRef}
+            placeholder="Add Tech Stack"
+            submitButtonText="Save"
+            errorMessage={isDuplicate ? "Duplicate Item" : ""}
+            isClearBtnVisible={true}
+            clearInputAction={clearAction}
+            onChange={handleOnChange}
+          />
+        </form>
+      ) : (
+        <Button
+          variant="outline"
+          className="justify-center w-full"
+          onClick={toggleInput}
+        >
+          Add Tech Stack
+        </Button>
+      )}
     </div>
   );
 }
