8cb0b3e62b4e1f9722f78ecaadf18ceb594f41b2
Matt <84277551+MattRueter@users.noreply.github.com>
2024-05-09 12:59:37 -0500

    open editing input.

--- src/app/(main)/my-voyage/[teamId]/tech-stack/components/SettingsMenu.tsx
+++ src/app/(main)/my-voyage/[teamId]/tech-stack/components/SettingsMenu.tsx
@@ -5,10 +5,20 @@ import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/solid";
 
 interface SettingsMenuProps {
   onClose: () => void;
+  setIsEditing: (value: number) => void;
+  id: number;
 }
 
-export default function SettingsMenu({ onClose }: SettingsMenuProps) {
+export default function SettingsMenu({
+  onClose,
+  setIsEditing,
+  id,
+}: SettingsMenuProps) {
   const menuRef = useRef<HTMLDivElement>(null);
+
+  const openEdit = () => {
+    setIsEditing(id);
+  };
 
   useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
@@ -28,7 +38,12 @@ export default function SettingsMenu({ onClose }: SettingsMenuProps) {
         ref={menuRef}
         className="absolute z-10 ml-20 mt-28 bg-base-200 border-2 border-base-100 rounded-xl  w-40 h-36 flex flex-col justify-evenly p-2"
       >
-        <Button variant="outline" size="xs" className="flex justify-start">
+        <Button
+          variant="outline"
+          size="xs"
+          className="flex justify-start"
+          onClick={openEdit}
+        >
           <PencilSquareIcon className="w-3/12 h-3/12 " />
           Edit
         </Button>
