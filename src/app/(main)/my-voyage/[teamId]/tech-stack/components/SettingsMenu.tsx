b314686a4b45b1cbd474f9698cc058aea51d02e8
Matt <84277551+MattRueter@users.noreply.github.com>
2024-05-09 12:23:00 -0500

    add click outside to close menu logic.

--- src/app/(main)/my-voyage/[teamId]/tech-stack/components/SettingsMenu.tsx
+++ src/app/(main)/my-voyage/[teamId]/tech-stack/components/SettingsMenu.tsx
@@ -1,11 +1,33 @@
 "use client";
+import { useEffect, useRef } from "react";
 import Button from "@/components/Button";
 import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/solid";
 
-export default function SettingsMenu() {
+interface SettingsMenuProps {
+  onClose: () => void;
+}
+
+export default function SettingsMenu({ onClose }: SettingsMenuProps) {
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        onClose();
+      }
+    }
+    document.body.addEventListener("click", handleClickOutside);
+    return () => {
+      document.body.removeEventListener("click", handleClickOutside);
+    };
+  }, [onClose]);
+
   return (
     <>
-      <div className="absolute z-10 ml-20 mt-28 bg-base-200 border-2 border-base-100 rounded-xl  w-40 h-36 flex flex-col justify-evenly p-2">
+      <div
+        ref={menuRef}
+        className="absolute z-10 ml-20 mt-28 bg-base-200 border-2 border-base-100 rounded-xl  w-40 h-36 flex flex-col justify-evenly p-2"
+      >
         <Button variant="outline" size="xs" className="flex justify-start">
           <PencilSquareIcon className="w-3/12 h-3/12 " />
           Edit
