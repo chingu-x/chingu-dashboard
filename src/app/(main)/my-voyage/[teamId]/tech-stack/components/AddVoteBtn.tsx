8fd57b0822768043b0ddca80aa8d43a0dceeb80b
Matt <84277551+MattRueter@users.noreply.github.com>
2024-05-14 14:18:58 -0500

    fix button styles / layout.

--- src/app/(main)/my-voyage/[teamId]/tech-stack/components/AddVoteBtn.tsx
+++ src/app/(main)/my-voyage/[teamId]/tech-stack/components/AddVoteBtn.tsx
@@ -1,23 +1,9 @@
-import { EllipsisVerticalIcon } from "@heroicons/react/24/solid";
 import Button from "@/components/Button";
 
-interface AddVoteBtnProps {
-  id: number;
-  openMenu: (value: number) => void;
-}
-
-export default function AddVoteBtn({ id, openMenu }: AddVoteBtnProps) {
-  const handleClick = () => {
-    openMenu(id);
-  };
-
+export default function AddVoteBtn() {
   return (
-    <div className="flex col-span-2">
-      <EllipsisVerticalIcon
-        className="w-1/6 h-1/6 hover:cursor-pointer"
-        onClick={handleClick}
-      />
-      <Button variant="primary" size="xs" className="rounded-3xl w-5/6">
+    <div className="col-span-2 flex justify-end">
+      <Button variant="primary" size="xs" className="rounded-3xl">
         Add Vote
       </Button>
     </div>
--- src/app/(main)/my-voyage/[teamId]/tech-stack/components/RemoveVoteBtn.tsx
+++ src/app/(main)/my-voyage/[teamId]/tech-stack/components/RemoveVoteBtn.tsx
@@ -1,13 +1,29 @@
+import { EllipsisVerticalIcon } from "@heroicons/react/24/solid";
 import Button from "@/components/Button";
 
-export default function RemoveVoteBtn() {
+interface RemoveVoteBtnProps {
+  id: number;
+  openMenu: (value: number) => void;
+}
+
+export default function RemoveVoteBtn({ id, openMenu }: RemoveVoteBtnProps) {
+  const handleClick = () => {
+    openMenu(id);
+  };
+
   return (
-    <Button
-      variant="error"
-      size="xs"
-      className="w-11/12 rounded-3xl col-span-2 justify-self-end"
-    >
-      Remove Vote
-    </Button>
+    <div className="flex justify-end col-span-2">
+      <EllipsisVerticalIcon
+        className="w-1/6 h-1/6 hover:cursor-pointer"
+        onClick={handleClick}
+      />
+      <Button
+        variant="error"
+        size="xs"
+        className="rounded-3xl col-span-2 justify-self-end"
+      >
+        Remove Vote
+      </Button>
+    </div>
   );
 }
