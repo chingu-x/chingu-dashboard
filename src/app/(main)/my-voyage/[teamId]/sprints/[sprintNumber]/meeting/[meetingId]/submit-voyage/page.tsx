348cb6f726499aab3c0f7abf76462f5b5b57b3d9
Jane Moroz <Moroz.jane@yahoo.com>
2024-05-14 16:27:11 +0300

    refactor: remove some checks for now

--- src/app/(main)/my-voyage/[teamId]/sprints/[sprintNumber]/meeting/[meetingId]/submit-voyage/page.tsx
+++ src/app/(main)/my-voyage/[teamId]/sprints/[sprintNumber]/meeting/[meetingId]/submit-voyage/page.tsx
@@ -1,12 +1,7 @@
 import { Suspense } from "react";
-import { redirect } from "next/navigation";
 
 import SubmitProjectWrapper from "@/myVoyage/sprints/components/SubmitProjectWrapper";
 import Spinner from "@/components/Spinner";
-
-import { getUser } from "@/utils/getUser";
-import { getCurrentVoyageTeam } from "@/utils/getCurrentVoyageTeam";
-import routePaths from "@/utils/routePaths";
 
 interface VoyageSubmissionPageProps {
   params: {
@@ -16,25 +11,12 @@ interface VoyageSubmissionPageProps {
   };
 }
 
-export default async function VoyageSubmissionPage({
+export default function VoyageSubmissionPage({
   params,
 }: VoyageSubmissionPageProps) {
-  const teamId = Number(params.teamId);
-
-  const [user, error] = await getUser();
-
-  const { err, currentTeam } = getCurrentVoyageTeam({ user, error, teamId });
-
-  if (err) {
-    return err;
-  }
-
-  if (currentTeam) {
-    return (
-      <Suspense fallback={<Spinner />}>
-        <SubmitProjectWrapper params={params} />
-      </Suspense>
-    );
-  }
-  redirect(routePaths.dashboardPage());
+  return (
+    <Suspense fallback={<Spinner />}>
+      <SubmitProjectWrapper params={params} />
+    </Suspense>
+  );
 }
