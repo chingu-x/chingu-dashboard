2c785bf5e85853d0bbef5b7a61a2d100a2b28b56
Matt <84277551+MattRueter@users.noreply.github.com>
2024-05-14 14:26:55 -0500

    update button rendering logic based on number of votes and user id.

--- src/app/(main)/my-voyage/[teamId]/tech-stack/components/RemoveVoteBtn.tsx
+++ src/app/(main)/my-voyage/[teamId]/tech-stack/components/RemoveVoteBtn.tsx
@@ -4,19 +4,27 @@ import Button from "@/components/Button";
 interface RemoveVoteBtnProps {
   id: number;
   openMenu: (value: number) => void;
+  numberOfVotes: number;
 }
 
-export default function RemoveVoteBtn({ id, openMenu }: RemoveVoteBtnProps) {
+export default function RemoveVoteBtn({
+  id,
+  openMenu,
+  numberOfVotes,
+}: RemoveVoteBtnProps) {
   const handleClick = () => {
     openMenu(id);
   };
 
   return (
     <div className="flex justify-end col-span-2">
-      <EllipsisVerticalIcon
-        className="w-1/6 h-1/6 hover:cursor-pointer"
-        onClick={handleClick}
-      />
+      {numberOfVotes < 2 && (
+        <EllipsisVerticalIcon
+          className="w-1/6 h-1/6 hover:cursor-pointer"
+          onClick={handleClick}
+        />
+      )}
+
       <Button
         variant="error"
         size="xs"
