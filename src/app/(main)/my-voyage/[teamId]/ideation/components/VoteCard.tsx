6f4ca0188886ea1f36f0670fa27809bdbba60e4e
Dan Ko <danko56666@gmail.com>
2024-05-14 17:45:38 -0400

    test

--- src/app/(main)/my-voyage/[teamId]/ideation/components/VoteCard.tsx
+++ src/app/(main)/my-voyage/[teamId]/ideation/components/VoteCard.tsx
@@ -27,7 +27,7 @@ interface VoteCardProps {
 
 function VoteCard({ teamId, projectIdeaId, users, className }: VoteCardProps) {
   const [currentUserVoted, setCurrentUserVoted] = useState<null | boolean>(
-    null,
+    null
   );
   const { id } = useUser();
   const { loading } = useIdeation();
@@ -58,7 +58,7 @@ function VoteCard({ teamId, projectIdeaId, users, className }: VoteCardProps) {
 
       if (error) {
         dispatch(
-          onOpenModal({ type: "error", content: { message: error.message } }),
+          onOpenModal({ type: "error", content: { message: error.message } })
         );
       }
 
@@ -73,7 +73,7 @@ function VoteCard({ teamId, projectIdeaId, users, className }: VoteCardProps) {
 
       if (error) {
         dispatch(
-          onOpenModal({ type: "error", content: { message: error.message } }),
+          onOpenModal({ type: "error", content: { message: error.message } })
         );
       }
 
@@ -84,15 +84,11 @@ function VoteCard({ teamId, projectIdeaId, users, className }: VoteCardProps) {
 
   const getVoteUsers = useCallback(
     () => users.map((user) => user.votedBy.member.id),
-    [users],
+    [users]
   );
 
   function buttonContent() {
-    if (
-      addIdeationVoteLoading ||
-      removeIdeationVoteLoading ||
-      (loading && voteChanged)
-    ) {
+    if (addIdeationVoteLoading || removeIdeationVoteLoading) {
       return <Spinner />;
     }
 
