8bfed6b66c5def1e9724829b18bfab09d4a0da10
Matt <84277551+MattRueter@users.noreply.github.com>
2024-05-15 11:03:13 -0500

    Move JSX of EditPopver into reusable EditMenu component.

---
+++ src/components/EditMenu.tsx
@@ -0,0 +1,32 @@
+import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
+import Button from "@/components/Button";
+
+interface EditMenuProps {
+  handleClick: any;
+  handleDelete: any;
+}
+
+export default function EditMenu({ handleClick, handleDelete }: EditMenuProps) {
+  return (
+    <div className="flex flex-col justify-evenly items-center w-[150px] h-[116px] bg-base-200 border border-base-100 rounded-lg shadow-lg absolute top-0 right-0 z-10">
+      <Button
+        variant="outline"
+        size="sm"
+        className="w-[118px] h-[34px] justify-start"
+        onClick={handleClick}
+      >
+        <PencilSquareIcon className="w-4 h-4" />
+        Edit
+      </Button>
+      <Button
+        variant="error"
+        size="sm"
+        className="w-[118px] h-[34px] justify-start"
+        onClick={handleDelete}
+      >
+        <TrashIcon className="w-4 h-4" />
+        Delete
+      </Button>
+    </div>
+  );
+}
