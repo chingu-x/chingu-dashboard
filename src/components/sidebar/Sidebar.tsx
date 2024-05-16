5b05a1c2efa88f1e4f6f83c2227c0f2f80049728
Winnie <winnieng3210@gmail.com>
2024-05-16 09:45:18 -0400

    side bar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -144,23 +144,26 @@ export default function Sidebar() {
         isOpenSidebar ? "w-[18.438rem]" : "w-[5.813rem]"
       } text-center bg-base-200 flex flex-col justify-between border-r border-neutral-content shadow-[4px_4px_4px_0] shadow-neutral-focus/5 h-full`}
     >
-      <ul
+      <div
         className={`flex flex-col ${
           isOpenSidebar ? "items-start pl-10" : "items-center"
         } pt-6`}
       >
-        {pagesProperties.map((element) => (
-          <PageButton
-            key={element.name}
-            element={element}
-            onClick={handlePageClick}
-            selectedButton={selectedButton}
-            isOpen={isOpenSidebar}
-            link={element.link}
-            setHoveredButton={setHoveredButton}
-            voyagePages={voyagePages}
-          />
-        ))}
+        <ul>
+          {pagesProperties.map((element) => (
+            <PageButton
+              key={element.name}
+              element={element}
+              onClick={handlePageClick}
+              selectedButton={selectedButton}
+              isOpen={isOpenSidebar}
+              link={element.link}
+              setHoveredButton={setHoveredButton}
+              voyagePages={voyagePages}
+            />
+          ))}
+        </ul>
+
         {isOpenSidebar && (
           <ul className="flex flex-col items-center">
             {voyagePages.map((element) => (
@@ -176,7 +179,7 @@ export default function Sidebar() {
             ))}
           </ul>
         )}
-      </ul>
+      </div>
       <div className="flex flex-col items-end justify-start border-t border-neutral-content min-h-[80px] pt-4 pr-6">
         <ExpandButton isOpen={isOpenSidebar} onClick={setIsOpenSidebar} />
       </div>
