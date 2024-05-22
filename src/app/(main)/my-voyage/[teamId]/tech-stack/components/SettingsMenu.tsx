ce2d59c267d010606da17af6f471d2c3f54120fa
Matt <84277551+MattRueter@users.noreply.github.com>
2024-05-22 12:14:30 -0500

    fix SettingsMenu layout relative to <li>.

--- src/app/(main)/my-voyage/[teamId]/tech-stack/components/SettingsMenu.tsx
+++ src/app/(main)/my-voyage/[teamId]/tech-stack/components/SettingsMenu.tsx
@@ -32,7 +32,7 @@ export default function SettingsMenu({
   }, [onClose]);
 
   return (
-    <div ref={menuRef}>
+    <div className="ml-52 -mt-10 absolute" ref={menuRef}>
       <EditMenu handleClick={openEdit} />
     </div>
   );
