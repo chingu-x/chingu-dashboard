c16237a34792627bbe4231edef19e98fccddd585
Matt <84277551+MattRueter@users.noreply.github.com>
2024-06-04 14:43:31 -0500

    create service file.

---
+++ src/app/(main)/my-voyage/[teamId]/tech-stack/techStackService.ts
@@ -0,0 +1,104 @@
+"use server";
+
+import { CacheTag } from "@/utils/cacheTag";
+import { getAccessToken } from "@/utils/getCookie";
+import { type AsyncActionResponse, handleAsync } from "@/utils/handleAsync";
+import { DELETE, PATCH, POST } from "@/utils/requests";
+import { revalidateTag } from "next/cache";
+import { TechStackItem } from "@/store/features/techStack/techStackSlice";
+
+interface AddTechItemProps {
+  teamId: number;
+  techName: string;
+  techCategory: number;
+  voyageTeamMemberId: number;
+}
+
+type AddTechItemBody = Omit<AddTechItemProps, "teamId">;
+
+interface AddTechItemResponse {
+  teamTechStackItemVotedId: number;
+  teamTechId: number;
+  teamMemberId: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface EditTechItemProps {
+  techItemId: number;
+  techName: string;
+}
+
+type EditTechItemBody = Omit<EditTechItemProps, "techItemId">;
+
+interface EditTechItemResponse extends TechStackItem {
+  voyageTeamMemberId: number;
+  voyageTeamId: number;
+}
+
+interface DeleteTechItemProps {
+  techItemId: number;
+}
+
+export async function addTechItem({
+  teamId,
+  techName,
+  techCategory,
+  voyageTeamMemberId,
+}: AddTechItemProps): Promise<AsyncActionResponse<AddTechItemResponse>> {
+  const token = getAccessToken();
+  const addTechItemAsync = () =>
+    POST<AddTechItemBody, AddTechItemResponse>(
+      `/api/v1/voyages/teams/${teamId}/techs`,
+      token,
+      "default",
+      { techName, techCategory, voyageTeamMemberId },
+    );
+
+  const [res, error] = await handleAsync(addTechItemAsync);
+
+  if (res) {
+    revalidateTag(CacheTag.techStack);
+  }
+
+  return [res, error];
+}
+
+export async function editTechItem({
+  techItemId,
+  techName,
+}: EditTechItemProps): Promise<AsyncActionResponse<EditTechItemResponse>> {
+  const token = getAccessToken();
+
+  const editTechItemAsync = () =>
+    PATCH<EditTechItemBody, EditTechItemResponse>(
+      `/api/v1/voyages/techs/${techItemId}`,
+      token,
+      "default",
+      { techName },
+    );
+
+  const [res, error] = await handleAsync(editTechItemAsync);
+
+  if (res) {
+    revalidateTag(CacheTag.techStack);
+  }
+
+  return [res, error];
+}
+
+export async function deleteTechItem({
+  techItemId,
+}: DeleteTechItemProps): Promise<AsyncActionResponse<void>> {
+  const token = getAccessToken();
+
+  const deleteTechItemAsync = () =>
+    DELETE<void>(`/api/v1/voyages/techs/${techItemId}/vote`, token, "default");
+  const [res, error] = await handleAsync(deleteTechItemAsync);
+
+  if (res) {
+    revalidateTag(CacheTag.techStack);
+  }
+
+  return [res, error];
+}
