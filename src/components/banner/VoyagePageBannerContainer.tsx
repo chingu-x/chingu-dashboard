18e6c2504e9b0231cd11a83b0354ea0ced5f1581
Winnie <winnieng3210@gmail.com>
2024-06-06 08:41:59 -0400

    page level one heading

--- src/components/banner/VoyagePageBannerContainer.tsx
+++ src/components/banner/VoyagePageBannerContainer.tsx
@@ -13,7 +13,7 @@ export default function VoyagePageBannerContainer({
     <div className="box-border flex max-h-[320px] w-full flex-row justify-between gap-x-10 px-20 lg:gap-x-20">
       {children}
       <div className="flex max-w-[700px] flex-col justify-center gap-y-3.5 2xl:max-w-none">
-        <h3 className="text-3xl font-bold text-base-300">{title}</h3>
+        <h1 className="text-3xl font-bold text-base-300">{title}</h1>
         <p className="text-lg font-medium text-base-300">{description}</p>
       </div>
     </div>
