ff839db66137bcd8ebb329ef19dcf9e486802614
Stan Stanislaus <stan.stanislaus@steffesgroup.com>
2024-06-19 19:44:20 -0500

    chore: reorder imports to satisfy lint

--- src/app/(main)/my-voyage/[teamId]/ideation/page.tsx
+++ src/app/(main)/my-voyage/[teamId]/ideation/page.tsx
@@ -1,6 +1,6 @@
+import { Suspense } from "react";
 import IdeationComponentWrapper from "./components/IdeationComponentWrapper";
 import Spinner from "@/components/Spinner";
-import { Suspense } from "react";
 
 interface IdeationPageProps {
   params: {
