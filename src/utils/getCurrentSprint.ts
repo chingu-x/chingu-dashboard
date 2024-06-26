b5b0059b45a26de195dde980cb0a16f55cdeb361
Winnie <winnieng3210@gmail.com>
2024-06-26 11:19:28 -0400

    change date

--- src/utils/getCurrentSprint.ts
+++ src/utils/getCurrentSprint.ts
@@ -2,12 +2,14 @@ import { isWithinInterval } from "date-fns";
 
 import { type Sprint } from "@/store/features/sprint/sprintSlice";
 
+const currentDate = new Date(2024, 5, 15);
+
 export function getCurrentSprint(sprints: Sprint[]) {
   const currentDate = new Date();
   const currentSprint = sprints.find((sprint) =>
     isWithinInterval(currentDate, {
       start: sprint.startDate,
-      end: sprint.endDate,
+      end: currentDate,
     }),
   );
   return currentSprint;
