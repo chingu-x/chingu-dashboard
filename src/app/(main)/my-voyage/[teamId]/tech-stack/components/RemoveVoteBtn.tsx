6b3c3c8b2a6e9b87a46d8666f870a0540ed18141
Matt <84277551+MattRueter@users.noreply.github.com>
2024-07-03 10:52:24 -0500

    wire up remove vote function.

--- src/app/(main)/my-voyage/[teamId]/tech-stack/components/RemoveVoteBtn.tsx
+++ src/app/(main)/my-voyage/[teamId]/tech-stack/components/RemoveVoteBtn.tsx
@@ -1,12 +1,28 @@
 import Button from "@/components/Button";
+import useServerAction from "@/hooks/useServerAction";
+import { removeTechItemVote } from "../techStackService";
 
 interface RemoveVoteBtnProps {
   techItemId: number;
 }
 
 export default function RemoveVoteBtn({ techItemId }: RemoveVoteBtnProps) {
-  const handleClick = () => {
+  const {
+    runAction: removeVoteAction,
+    isLoading: removeVoteLoading,
+    setIsLoading: setRemoveVoteLoading,
+  } = useServerAction(removeTechItemVote);
+
+  const handleClick = async () => {
     console.log(techItemId);
+    const [res, error] = await removeVoteAction({
+      techItemId,
+    });
+    if (error) {
+      //TODO: add error handling dispatch new component.
+      console.log(error);
+    }
+    setRemoveVoteLoading(false);
   };
 
   return (
