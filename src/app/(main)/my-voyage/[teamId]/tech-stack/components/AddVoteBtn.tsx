b54dc55f32b7a90117d15764b1a12e9ceb41fe33
Matt <84277551+MattRueter@users.noreply.github.com>
2024-07-03 10:47:08 -0500

    wire up add vote function.

--- src/app/(main)/my-voyage/[teamId]/tech-stack/components/AddVoteBtn.tsx
+++ src/app/(main)/my-voyage/[teamId]/tech-stack/components/AddVoteBtn.tsx
@@ -1,12 +1,29 @@
 import Button from "@/components/Button";
+import useServerAction from "@/hooks/useServerAction";
+import { addTechItemVote } from "../techStackService";
 
 interface AddVoteBtnProps {
   techItemId: number;
 }
 
 export default function AddVoteBtn({ techItemId }: AddVoteBtnProps) {
-  const handleClick = () => {
-    console.log(techItemId);
+  const {
+    runAction: addVoteAction,
+    isLoading: addVoteLoading,
+    setIsLoading: setAddVoteLoading,
+  } = useServerAction(addTechItemVote);
+
+  const handleClick = async () => {
+    const [res, error] = await addVoteAction({
+      techItemId,
+    });
+
+    if (error) {
+      //TODO add error component dispatch.
+      console.log(error);
+    }
+
+    setAddVoteLoading(false);
   };
 
   return (
