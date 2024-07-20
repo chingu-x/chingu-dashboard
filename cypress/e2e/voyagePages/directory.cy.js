210eed5f68d006e9abd82a32aa87a086d34a8036
Winnie <winnieng3210@gmail.com>
2024-07-20 15:34:52 -0400

    add data cypress ids

--- cypress/e2e/voyagePages/directory.cy.js
+++ cypress/e2e/voyagePages/directory.cy.js
@@ -3,7 +3,17 @@ describe("directory page", () => {
     cy.login("jessica.williamson@gmail.com", "password");
   });
 
-  //   it("can add average hour per Sprint", () => {
-  //     cy.contains("button", "Directory").click();
-  //   });
+  // it("can add average hour per Sprint", () => {
+  //   cy.contains("button", "Directory").click();
+
+  //   cy.get('[data-cy="avg-hr-per-sprint"]').click();
+  //   cy.get("#avgHours").clear().type("35");
+  //   cy.contains("button", "Save").click();
+  //   cy.get('[data-cy="avg-hr-per-sprint"]')
+  //     .invoke("text")
+  //     .then((text) => {
+  //       cy.log(text);
+  //       expect(text).to.equal("35");
+  //     });
+  // });
 });
