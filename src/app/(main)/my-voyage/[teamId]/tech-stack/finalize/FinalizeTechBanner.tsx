cf4ebb75c7261fcf9058eae2ee804d106841aff4
Matt <84277551+MattRueter@users.noreply.github.com>
2024-07-22 14:31:34 -0500

    create finalize tech stack route & add alert banner.

---
+++ src/app/(main)/my-voyage/[teamId]/tech-stack/finalize/FinalizeTechBanner.tsx
@@ -0,0 +1,22 @@
+import Alert from "@/components/Alert";
+
+export default function FinalizeTechBanner() {
+  return (
+    <div className="flex w-full max-w-[871px] flex-1 flex-col justify-center gap-y-4 rounded-2xl bg-base-200 p-10 shadow-md">
+      <h2 className="text-2xl font-semibold text-base-300">
+        Finalize your choices
+      </h2>
+      <p className="text-base font-medium text-base-300">
+        Finalize the tech stack that you and your team plan on creating for
+        your Voyage!
+      </p>
+      <div className="w-full">
+        <Alert
+          context="info"
+          message="Important: It's advisable to collaborate as a team and 
+          ensure everyone agrees before finalizing your tech stack. You will be able to make changes to your tech stack later."          
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/app/(main)/my-voyage/[teamId]/tech-stack/finalize/page.tsx
@@ -0,0 +1,15 @@
+import FinalizeTechBanner from "./FinalizeTechBanner"
+
+export default async function FinalizeTechStackPage(){
+  return(
+    <>
+      <FinalizeTechBanner />
+      {/**
+       * message component
+       * list of SelectTechCards
+       * finalize button
+       * cancel button
+       */}
+    </>
+  )
+}
