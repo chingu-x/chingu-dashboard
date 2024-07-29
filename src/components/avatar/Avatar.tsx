7a7eba6ffd003434e7b904d6b0dc90d9ba6cf46b
Jane Moroz <Moroz.jane@yahoo.com>
2024-07-29 13:21:31 +0300

    chore: set <Avatar/> to its original state

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -4,8 +4,6 @@ import { cn } from "@/lib/utils";
 
 interface AvatarProps {
   image?: string | StaticImageData;
-  firstName?: string;
-  lastName?: string;
   customClassName?: string;
   width: number;
   height: number;
@@ -13,59 +11,26 @@ interface AvatarProps {
 
 export default function Avatar({
   image,
-  firstName,
-  lastName,
   customClassName,
   width = 24,
   height = 24,
 }: AvatarProps) {
-  console.log(width);
-  console.log(height);
-
   return (
     <div
       className={cn(
-        `w-[${width}px] h-[${height}px] flex items-center justify-center overflow-hidden rounded-full bg-[#FE6AA6]`,
+        "shrink-1 cursor-pointer overflow-hidden rounded-full border border-neutral px-0",
         customClassName,
       )}
     >
-      <span className="font-semibold uppercase">jw</span>
+      <Image
+        alt="avatar"
+        src={
+          image ??
+          "https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
+        }
+        width={width}
+        height={height}
+      />
     </div>
   );
-
-  if (image) {
-    return (
-      <div
-        className={cn(
-          "shrink-1 cursor-pointer overflow-hidden rounded-full border border-neutral px-0",
-          customClassName,
-        )}
-      >
-        <Image
-          alt="avatar"
-          src={
-            image ??
-            "https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
-          }
-          width={width}
-          height={height}
-        />
-      </div>
-    );
-  }
-
-  if (firstName && lastName) {
-    return (
-      <div
-        className={cn(
-          "shrink-1 cursor-pointer overflow-hidden rounded-full border border-red-400 px-0",
-          customClassName,
-        )}
-      >
-        <div className={`w-[${width}px] h-[${height}px]`}>
-          <span>jw</span>
-        </div>
-      </div>
-    );
-  }
 }
