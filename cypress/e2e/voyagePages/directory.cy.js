5909e71d358f187296977d1fda3d10847f5973de
Winnie <winnieng3210@gmail.com>
2024-08-01 10:51:32 -0400

    test cleanup

--- cypress/e2e/voyagePages/directory.cy.js
+++ cypress/e2e/voyagePages/directory.cy.js
@@ -3,17 +3,20 @@ describe("directory page", () => {
     cy.login("jessica.williamson@gmail.com", "password");
   });
 
-  // it("can add average hour per Sprint", () => {
-  //   cy.contains("button", "Directory").click();
+  it("can input average hour per Sprint", () => {
+    cy.contains("button", "Directory").click();
 
-  //   cy.get('[data-cy="avg-hr-per-sprint"]').click();
-  //   cy.get("#avgHours").clear().type("35");
-  //   cy.contains("button", "Save").click();
-  //   cy.get('[data-cy="avg-hr-per-sprint"]')
-  //     .invoke("text")
-  //     .then((text) => {
-  //       cy.log(text);
-  //       expect(text).to.equal("35");
-  //     });
-  // });
+    cy.get('[data-cy="avg-hr-per-sprint"]').click();
+    cy.get("#avgHours").clear().type("12");
+    cy.contains("button", "Save").click();
+    cy.wait(2000);
+    cy.reload();
+    // TODO for later: create some sort of fixture instead
+    cy.get('[data-cy="avg-hr-per-sprint"]')
+      .invoke("text")
+      .then((text) => {
+        cy.log(text);
+        expect(text).to.equal("12");
+      });
+  });
 });
