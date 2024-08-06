3240c4b80f5305b08ba34d879a2e4c60a0cca7e6
Dan Ko <danko56666@gmail.com>
2024-08-06 18:48:38 -0400

    remove comment

--- src/utils/getCurrentSprint.ts
+++ src/utils/getCurrentSprint.ts
@@ -3,7 +3,6 @@ import { isWithinInterval } from "date-fns";
 import { type Sprint } from "@/store/features/sprint/sprintSlice";
 
 export const currentDate = new Date(2024, 5, 10, 12);
-// export const currentDate = new Date();
 
 export function getCurrentSprint(sprints: Sprint[]) {
   const currentSprint = sprints.find((sprint) =>
