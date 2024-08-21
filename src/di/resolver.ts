673017dfcbea68293a6a784cf4dd979a112720dc
Timothy Russo <russotimothy@live.it>
2024-08-21 09:06:16 +0200

    feat: tsyring configuration

--- src/di/resolver.ts
+++ src/di/resolver.ts
@@ -3,4 +3,8 @@ import type { InjectionToken } from "tsyringe";
 import container from "./config";
 
 export const resolve =
-  <T>(token: InjectionToken<T>) => () => container.resolve(token);
+  <T>(token: InjectionToken<T>) =>
+  // eslint-disable-next-line indent
+  () =>
+    // eslint-disable-next-line indent
+    container.resolve(token);
