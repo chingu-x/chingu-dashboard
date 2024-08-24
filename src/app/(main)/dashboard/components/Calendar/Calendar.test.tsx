cda461fde23a69a6bbd9afbcbb8ef43e8b9fe2bd
Dan Ko <danko56666@gmail.com>
2024-08-24 00:14:47 -0400

    add test

---
+++ src/app/(main)/dashboard/components/Calendar/Calendar.test.tsx
@@ -0,0 +1,26 @@
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { format } from "date-fns";
+import Calendar from "./Calendar";
+import { rootReducer } from "@/store/store";
+
+describe("Calendar Component", () => {
+  it("displays the month and year on a single line", () => {
+    const store = configureStore({
+      reducer: rootReducer,
+    });
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <Calendar />
+      </Provider>,
+    );
+
+    const currentDate = new Date();
+    const formattedDate = format(currentDate, "MMMM y");
+    const monthYearElement = getByText(formattedDate);
+
+    expect(monthYearElement.closest("div")).toHaveClass("whitespace-nowrap");
+  });
+});
