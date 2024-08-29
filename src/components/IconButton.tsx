1586733f83420f5e13b2ce3a6dab34b020062b88
JaneMoroz <moroz.jane@yahoo.com>
2024-08-29 21:58:47 +0300

    fix: fix ResourceItem and ResourceCard title issue

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -16,7 +16,7 @@ export default function IconButton({
     <button
       type="button"
       className={cn(
-        "flex h-10 w-10 items-center justify-center rounded-full px-2 hover:bg-base-300/25 [&>*]:h-5 [&>*]:w-5 [&>*]:stroke-[1.5px] [&>*]:text-base-300",
+        "flex h-10 w-10 shrink-0 items-center justify-center rounded-full p-2 hover:bg-base-300/25 [&>*]:h-5 [&>*]:w-5 [&>*]:stroke-[1.5px] [&>*]:text-base-300",
         className,
       )}
       aria-label={ariaLabel}
