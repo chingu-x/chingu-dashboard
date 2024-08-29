82787d55e168db24746cbd7c390c3639500a4abd
JaneMoroz <moroz.jane@yahoo.com>
2024-08-29 20:36:31 +0300

    fix: fix top nav and side bar bg, border colors, shadows and etc.

--- src/components/navbar/ChinguMenu.tsx
+++ src/components/navbar/ChinguMenu.tsx
@@ -5,19 +5,20 @@ import routePaths from "@/utils/routePaths";
 
 export default function ChinguMenu() {
   return (
-    <Link href={routePaths.dashboardPage()}>
-      <div className="flex cursor-pointer flex-col items-center gap-2 sm:flex-row">
-        <Image
-          src={chinguLogo}
-          width={50}
-          height={50}
-          alt="Chingu Logo"
-          priority={true}
-        />
-        <h2 className="text-xs font-semibold text-base-300 sm:text-lg">
-          Chingu
-        </h2>
-      </div>
+    <Link
+      href={routePaths.dashboardPage()}
+      className="flex flex-col flex-wrap items-center gap-2 cursor-pointer sm:flex-row"
+    >
+      <Image
+        src={chinguLogo}
+        width={50}
+        height={50}
+        alt="Chingu Logo"
+        priority={true}
+      />
+      <h2 className="hidden text-xs font-semibold text-base-300 sm:flex sm:text-lg">
+        Chingu
+      </h2>
     </Link>
   );
 }
