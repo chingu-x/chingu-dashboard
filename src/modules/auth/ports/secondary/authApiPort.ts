835e9115f6192568fe366a050cad26fd3d50eb44
Dan Ko <danko56666@gmail.com>
2024-10-16 20:48:55 -0400

    finish implementation of auth api port

--- src/modules/auth/ports/secondary/authApiPort.ts
+++ src/modules/auth/ports/secondary/authApiPort.ts
@@ -1,3 +1,6 @@
+import { type LoginRequestDto } from "@/modules/auth/application/dtos/request.dto";
+import { type LoginResponseDto } from "@/modules/auth/application/dtos/response.dto";
+
 export interface AuthApiPort {
-  login: (props) => Promise<>;
+  login: (props: LoginRequestDto) => Promise<LoginResponseDto>;
 }
