51f59841a06312b2f6b347e05094e4067ccbe806
Dan Ko <danko56666@gmail.com>
2024-10-21 20:23:17 -0400

    implement logout method in auth api port

--- src/modules/auth/ports/secondary/authApiPort.ts
+++ src/modules/auth/ports/secondary/authApiPort.ts
@@ -1,6 +1,10 @@
 import { type LoginRequestDto } from "@/modules/auth/application/dtos/request.dto";
-import { type LoginResponseDto } from "@/modules/auth/application/dtos/response.dto";
+import type {
+  LogoutResponseDto,
+  LoginResponseDto,
+} from "@/modules/auth/application/dtos/response.dto";
 
 export interface AuthApiPort {
   login: ({ email, password }: LoginRequestDto) => Promise<LoginResponseDto>;
+  logout: () => Promise<LogoutResponseDto>;
 }
