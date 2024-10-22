ecedee56566e3014b2b7b4815f0612aa29626b11
Dan Ko <danko56666@gmail.com>
2024-10-21 20:34:46 -0400

    implement log out in auth client adapter

--- src/modules/auth/adapters/primary/authClientAdapter.ts
+++ src/modules/auth/adapters/primary/authClientAdapter.ts
@@ -1,7 +1,11 @@
 import { inject, injectable } from "tsyringe";
 import { type LoginRequestDto } from "@/modules/auth/application/dtos/request.dto";
-import { type LoginResponseDto } from "@/modules/auth/application/dtos/response.dto";
+import type {
+  LogoutResponseDto,
+  LoginResponseDto,
+} from "@/modules/auth/application/dtos/response.dto";
 import { type LoginUsecase } from "@/modules/auth/application/usecases/loginUsecase";
+import { type LogoutUsecase } from "@/modules/auth/application/usecases/logoutUsecase";
 import { type AuthClientPort } from "@/modules/auth/ports/primary/authClientPort";
 import { TYPES } from "@/di/types";
 
@@ -10,9 +14,14 @@ export class AuthClientAdapter implements AuthClientPort {
   constructor(
     @inject(TYPES.LoginUsecase)
     private readonly loginUsecase: LoginUsecase,
+    private readonly logoutUsecase: LogoutUsecase,
   ) {}
 
   async login({ email, password }: LoginRequestDto): Promise<LoginResponseDto> {
     return await this.loginUsecase.execute({ email, password });
+  }
+
+  async logout(): Promise<LogoutResponseDto> {
+    return await this.logoutUsecase.execute();
   }
 }
