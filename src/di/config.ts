87789a748242ad00206c866e5f0064a8dc076fbd
Dan Ko <danko56666@gmail.com>
2024-11-14 13:28:32 -0500

    update di config

--- src/di/config.ts
+++ src/di/config.ts
@@ -5,11 +5,15 @@ import { AxiosAdapter } from "@/modules/restApi/adapters/secondary/AxiosAdapter"
 import { AuthClientAdapter } from "@/modules/auth/adapters/primary/authClientAdapter";
 import { LoginUsecase } from "@/modules/auth/application/usecases/loginUsecase";
 import { LogoutUsecase } from "@/modules/auth/application/usecases/logoutUsecase";
+import { UserClientAdapter } from "@/modules/user/adapters/primary/userClientAdapter";
+import { GetUserUsecase } from "@/modules/user/application/usecases/getUserUsecase";
 
 container.register(TYPES.RestApiPort, { useClass: AxiosAdapter });
 container.register(TYPES.AuthApiPort, { useClass: AuthApiAdapter });
 container.register(TYPES.LoginUsecase, { useClass: LoginUsecase });
 container.register(TYPES.LogoutUsecase, { useClass: LogoutUsecase });
 container.register(TYPES.AuthClientAdapter, { useClass: AuthClientAdapter });
+container.register(TYPES.GetUserUsecase, { useClass: GetUserUsecase });
+container.register(TYPES.UserClientAdapter, { useClass: UserClientAdapter });
 
 export default container;
--- src/di/types.ts
+++ src/di/types.ts
@@ -1,14 +1,12 @@
 export const TYPES = {
   /* Ports */
   RestApiPort: Symbol.for("RestApiPort"),
-  AuthClientPort: Symbol.for("AuthClientPort"),
   AuthApiPort: Symbol.for("AuthApiPort"),
   UserApiPort: Symbol.for("UserApiPort"),
 
   /* Adapters */
-  AxiosAdapter: Symbol.for("AxiosAdapter"),
   AuthClientAdapter: Symbol.for("AuthClientAdapter"),
-  AuthApiAdapter: Symbol.for("AuthApiAdapter"),
+  UserClientAdapter: Symbol.for("UserClientAdapter"),
 
   /* UseCases */
   LoginUsecase: Symbol.for("LoginUsecase"),
