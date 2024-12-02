2a08901a46bb6d9c8653db41244888f1e48a6a17
Dan Ko <danko56666@gmail.com>
2024-12-02 08:09:20 -0500

    add secondary port for reset password

--- src/modules/auth/ports/secondary/authApiPort.ts
+++ src/modules/auth/ports/secondary/authApiPort.ts
@@ -1,6 +1,7 @@
 import type {
   RequestResetPasswordDto,
   LoginRequestDto,
+  ResetPasswordDto,
 } from "@/modules/auth/application/dtos/request.dto";
 import type {
   LogoutResponseDto,
@@ -11,4 +12,5 @@ export interface AuthApiPort {
   login: ({ email, password }: LoginRequestDto) => Promise<LoginResponseDto>;
   logout: () => Promise<LogoutResponseDto>;
   requestResetPassword: ({ email }: RequestResetPasswordDto) => Promise<void>;
+  resetPassword: ({ password, token }: ResetPasswordDto) => Promise<void>;
 }
