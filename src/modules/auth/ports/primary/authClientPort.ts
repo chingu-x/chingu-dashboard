add068599118c0f5de6a7876c1c7d7a669769d35
Dan Ko <danko56666@gmail.com>
2024-12-02 08:11:11 -0500

    add primary port for reset password

--- src/modules/auth/ports/primary/authClientPort.ts
+++ src/modules/auth/ports/primary/authClientPort.ts
@@ -1,6 +1,7 @@
 import type {
   RequestResetPasswordDto,
   LoginRequestDto,
+  ResetPasswordDto,
 } from "@/modules/auth/application/dtos/request.dto";
 import type {
   LogoutResponseDto,
@@ -11,4 +12,5 @@ export interface AuthClientPort {
   login: (props: LoginRequestDto) => Promise<LoginResponseDto>;
   logout: () => Promise<LogoutResponseDto>;
   requestResetPassword: (props: RequestResetPasswordDto) => Promise<void>;
+  resetPassword: (props: ResetPasswordDto) => Promise<void>;
 }
