b1a413176a02f0e7dd5fb471e4e36cdc5890ca22
Olga Yudkin <cvtqx@protonmail.com>
2025-01-22 12:37:32 -0500

    chore:replaced banner in directorycomponentwrapper

--- src/app/(main)/my-voyage/[teamId]/directory/components/DirectoryComponentWrapper.tsx
+++ src/app/(main)/my-voyage/[teamId]/directory/components/DirectoryComponentWrapper.tsx
@@ -1,9 +1,10 @@
 import { redirect } from "next/navigation";
 
+import { Banner } from "@chingu-x/components/banner";
+import Image from "next/image";
 import DirectoryProvider from "./DirectoryProvider";
 import TeamMember from "./TeamMember";
 
-import Banner from "@/components/banner/Banner";
 import ErrorComponent from "@/components/Error";
 
 import { type TeamDirectory } from "@/store/features/directory/directorySlice";
@@ -131,9 +132,26 @@ export default async function DirectoryComponentWrapper({
         description="Behold, your mighty band of teammates! If you want them to plan with precision and prowess, make sure your deets are up to date, or else prepare for some serious spreadsheet confusion!"
       >
         <Banner
-          imageLight="/img/directory_banner_light.png"
-          imageDark="/img/directory_banner_dark.png"
-          alt="directory_banner"
+          imageLight={
+            <Image
+              src="/img/directory_banner_light.png"
+              alt="Light directory banner"
+              fill={true}
+              sizes="276px"
+              priority
+              style={{ objectFit: "contain" }}
+            />
+          }
+          imageDark={
+            <Image
+              src="/img/directory_banner_dark.png"
+              alt="Dark directory banner"
+              fill={true}
+              sizes="276px"
+              priority
+              style={{ objectFit: "contain" }}
+            />
+          }          
           height="h-[200px]"
           width="w-[276px]"
         />
