210c234b4a97a996c150dcce1ce59b9fbf233a7c
Dan Ko <danko56666@gmail.com>
2025-01-28 19:49:54 -0500

    chore: update naming of get user function

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -14,6 +14,7 @@ import { getUserState } from "@/store/features/user/userSlice";
 import Spinner from "@/components/Spinner";
 import { CacheTag } from "@/utils/cacheTag";
 import { userAdapter } from "@/utils/adapters";
+import { currentDate } from "@/utils/getCurrentSprint";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -30,7 +31,7 @@ export default function Layout({ children }: LayoutProps) {
   });
 
   async function getUserQuery() {
-    return await userAdapter.getUser();
+    return await userAdapter.fetchUser({ currentDate });
   }
 
   if (isPending) {
