20fe7a0ca1fd948b7147d1c73cdf16d63ff4e2d2
Olga Yudkin <cvtqx@protonmail.com>
2025-02-05 12:26:23 -0500

    chore: added modetogglebutton to modeToggle component

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { type ChangeEvent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
-import { AnimatePresence, type Variants, motion } from "framer-motion";
-
-import { SunIcon, MoonIcon } from "@heroicons/react/20/solid";
+import { ModeToggleButton } from "@chingu-x/components/mode-toggle-button";
 
 export default function ModeToggle() {
   const [mounted, setMounted] = useState(false);
@@ -20,56 +18,13 @@ export default function ModeToggle() {
 
   // If enableSystem is true and the active theme is "system" (when a user firsts visits a website), resolvedTheme returns whether the system preference resolved to "dark" or "light".
   // So we need to set theme to resolvedTheme first time the user visits, but next time the theme is gonna come from localStorage
-  if (theme === "system" && resolvedTheme) {
-    setTheme(resolvedTheme);
-  }
+  const activeTheme = theme === "system" && resolvedTheme ? resolvedTheme : theme;
 
-  const toggleTheme = (e: ChangeEvent<HTMLInputElement>) => {
-    setTheme(e.target.checked === true ? "light" : "dark");
-  };
-
-  const modeToggleVariants: Variants = {
-    hidden: {
-      rotate: 25,
-    },
-    show: {
-      rotate: 0,
-    },
+  const toggleTheme = () => {
+    setTheme(activeTheme === "light" ? "dark" : "light");
   };
 
   return (
-    <label className="flex cursor-pointer items-center">
-      <input
-        className="invisible h-0 w-0"
-        data-testid="mode-toggle"
-        type="checkbox"
-        onChange={toggleTheme}
-        checked={theme === "light"}
-      />
-      <AnimatePresence mode="wait" initial={false}>
-        <div className="h-6 w-6">
-          {theme === "light" && (
-            <motion.div
-              key="moneIcon"
-              variants={modeToggleVariants}
-              initial="hidden"
-              animate="show"
-            >
-              <MoonIcon className="h-full w-full fill-current text-base-300" />
-            </motion.div>
-          )}
-          {theme === "dark" && (
-            <motion.div
-              key="sunIcon"
-              variants={modeToggleVariants}
-              initial="hidden"
-              animate="show"
-            >
-              <SunIcon className="h-full w-full fill-current text-base-300" />
-            </motion.div>
-          )}
-        </div>
-      </AnimatePresence>
-    </label>
+    <ModeToggleButton theme={activeTheme as "light" | "dark"} onChange={toggleTheme} />
   );
 }
