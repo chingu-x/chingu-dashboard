56afcc855def424dd844eaa09ecb639aee66dc70
Olga Yudkin <cvtqx@protonmail.com>
2025-02-05 13:24:07 -0500

    chore: replaced socialButtons

--- src/components/socialLoginButtonsContainer/SocialLoginButtonsContainer.tsx
+++ src/components/socialLoginButtonsContainer/SocialLoginButtonsContainer.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import { Button } from "@chingu-x/components/button";
+import { SocialButton } from "@chingu-x/components/social-button";
 
 function SocialLoginButtonsContainer() {
   return (
@@ -8,45 +7,15 @@ function SocialLoginButtonsContainer() {
         Continue using your social accounts
       </p>
       <div className="flex gap-x-6">
-        <Button
-          type="button"
-          title="Github"
-          className="flex h-[100px] w-[100px] flex-col gap-x-2 gap-y-[5px] border-2 border-base-100 bg-base-200 text-sm font-medium capitalize text-base-300 hover:border-base-100 hover:bg-base-content focus:bg-base-content focus:text-black active:bg-base-content"
-        >
-          <Image
-            src={"/img/github.png"}
-            width={48}
-            height={48}
-            alt="Github icon"
-          />
+        <SocialButton variant="github">
           Github
-        </Button>
-        <Button
-          type="button"
-          title="Discord"
-          className="flex h-[100px] w-[100px] flex-col gap-x-2 gap-y-[5px] border-2 border-base-100 bg-base-200 text-sm font-medium capitalize text-base-300 hover:border-base-100 hover:bg-base-content focus:bg-base-content focus:text-black active:bg-base-content"
-        >
-          <Image
-            src={"/img/discord.png"}
-            width={48}
-            height={48}
-            alt="Discord icon"
-          />
+        </SocialButton>
+        <SocialButton variant="discord">
           Discord
-        </Button>
-        <Button
-          type="button"
-          title="LinkedIn"
-          className="flex h-[100px] w-[100px] flex-col gap-x-2 gap-y-[5px] border-2 border-base-100 bg-base-200 text-sm font-medium capitalize text-base-300 hover:border-base-100 hover:bg-base-content focus:bg-base-content focus:text-black active:bg-base-content"
-        >
-          <Image
-            src={"/img/linkedin.png"}
-            width={48}
-            height={48}
-            alt="LinkedIn icon"
-          />
+        </SocialButton>
+        <SocialButton variant="linkedin">
           LinkedIn
-        </Button>
+        </SocialButton>
       </div>
     </>
   );
