d21a89ae77d47e76ba7fbbfeba47f559b14335a8
Kristi Hwang <kristi.y.l.h@gmail.com>
2025-02-06 12:49:31 -0800

    feat: fix errors

--- src/components/assessment/ui/ButtonsController.tsx
+++ src/components/assessment/ui/ButtonsController.tsx
@@ -7,12 +7,12 @@ export default function ButtonsController() {
   return (
     <div className="w-full max-w-[814px] space-y-4">
       <QuizButton />
-      <div className="flex w-full items-center justify-center gap-[42px] rounded-tl-[16px] bg-opacity-70 pt-[40px]">
-        <Button className="w-full !bg-[#F5F5F5] py-[10px] text-black">
+      <div className="flex w-full items-center justify-center gap-[42px] rounded-tl-[16px] pt-[40px]">
+        <Button className="w-full !bg-[#F5F5F5]/70 py-[10px] text-black">
           <ArrowLeftIcon className="mr-2 h-4 w-4" />
           Back to Self Assessment Page
         </Button>
-        <Button className="w-full !bg-[#217A56] py-[10px]">
+        <Button className="w-full !bg-[#217A56]/70 py-[10px]">
           Join an Upcoming Voyage
           <ArrowRightIcon className="ml-2 h-4 w-4 text-white" />
         </Button>
--- src/components/assessment/ui/ConfettiScreen.tsx
+++ src/components/assessment/ui/ConfettiScreen.tsx
@@ -3,10 +3,15 @@ import dynamic from "next/dynamic";
 import confettiAnimation from "@/public/lotties/assessment_confetti.json";
 
 const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+interface ConfettiScreenProps {
+  className: string;
+}
 
-export default function ConfettiScreen() {
+export default function ConfettiScreen({ className }: ConfettiScreenProps) {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-white">
+    <div
+      className={`flex h-screen w-screen items-center justify-center bg-white ${className}`}
+    >
       <Lottie animationData={confettiAnimation} loop={false} />
     </div>
   );
