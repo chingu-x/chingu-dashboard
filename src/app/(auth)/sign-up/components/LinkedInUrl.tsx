41c0d8c59a8bd8e8788968f1ad511e73fdb7d4e3
Homvi <honvedoadamkj@gmail.com>
2025-02-06 11:39:50 +0100

    feat(auth): create LinkedInUrl component for onboarding step
    - Added a new "LinkedInUrl" component to prompt users for their LinkedIn profile URL
    - Included optional text and styling for the component

---
+++ src/app/(auth)/sign-up/components/LinkedInUrl.tsx
@@ -0,0 +1,12 @@
+export default function LinkedInUrl() {
+  return (
+    <div className="py-10">
+      <h2 className="text-2xl font-semibold text-base-300">
+        If you are on LinkedIn what is your profile URL?
+      </h2>
+      <p className="w-full text-right text-[10px] font-medium text-neutral">
+        (optional)
+      </p>
+    </div>
+  );
+}
