96d75f993f5802499ba4c3dcefba7c3691f496be
Kristi Hwang <kristi.y.l.h@gmail.com>
2025-02-07 00:15:30 -0800

    feat: add all images for assessment-home-icon, apply styles to each image element

--- src/components/assessment/home/AssessmentPage.tsx
+++ src/components/assessment/home/AssessmentPage.tsx
@@ -9,14 +9,68 @@ export default function AssessmentPage() {
     >
       <div
         alt="header"
-        className="flex min-h-[200px] w-[1300px] min-w-[895px] flex-row gap-[40px] rounded-[16px] text-2xl font-light"
+        className="flex min-h-[200px] w-[1300px] min-w-[895px] flex-row items-center justify-center gap-[40px] rounded-[16px] text-2xl font-light"
       >
-        <div alt="icon-container">
+        <div
+          alt="icon-container"
+          className="relative flex h-[200px] w-[231px] items-center justify-center"
+        >
           <Image
             src="/img/assessment-home-icon/bg_circle.png"
             alt="assessment-home"
             width={231}
             height={200}
+            className="absolute inset-0 mx-auto my-auto"
+          />
+          <Image
+            src="/img/assessment-home-icon/operation2.png"
+            alt="operation2"
+            width={23}
+            height={21}
+            className="absolute left-[164.28px] top-[160.99px] text-[#217A56]"
+          />
+          <Image
+            src="/img/assessment-home-icon/operation1.png"
+            alt="operation1"
+            width={15}
+            height={19}
+            className="absolute left-[253px] top-[179px] text-[#14B8A6]"
+          />
+          <Image
+            src="/img/assessment-home-icon/operation3.png"
+            alt="operation3"
+            width={17}
+            height={23}
+            className="absolute left-[223px] text-[#14B8A6]"
+          />
+
+          <Image
+            src="/img/assessment-home-icon/code1.png"
+            alt="code1"
+            width={21}
+            height={10}
+            className="absolute left-[375px] top-[104px] text-[#E6624B]"
+          />
+          <Image
+            src="/img/assessment-home-icon/node2.png"
+            alt="node2"
+            width={21}
+            height={14}
+            className="absolute left-[295px] top-[23px] text-[#FFE5b4]"
+          />
+          <Image
+            src="/img/assessment-home-icon/curlies.png"
+            alt="curlies"
+            width={87}
+            height={114}
+            className="absolute left-[180px] top-[48px] rotate-[3.76deg] text-[#EEEFF0] opacity-50"
+          />
+          <Image
+            src="/img/assessment-home-icon/angle_bracket.png"
+            alt="angle_bracket"
+            width={87}
+            height={113}
+            className="absolute left-[273px] top-[57px] rotate-[-6.33deg] text-[#EEEFF0]"
           />
         </div>
         <div
@@ -26,8 +80,7 @@ export default function AssessmentPage() {
           <p className="font-inter h-[40px] w-[700px] text-left text-[39px] leading-[40px]">
             Hey there tech Wizard!
           </p>
-          <p className="font-inter decoration-skip-ink h-[72px] w-[700px] bg-[#16171A] text-left text-[20px] font-medium leading-[24px]">
-            {" "}
+          <p className="font-inter decoration-skip-ink h-[72px] w-[700px] text-left text-[20px] font-medium leading-[24px] text-[#16171A]">
             What's your tech superpower level? Are you a beginner Batman or an
             expert Iron Man? Let us know so we can team you up with the right
             sidekick.
