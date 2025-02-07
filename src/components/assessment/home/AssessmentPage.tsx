3c8c686a219b1bb75fea28192c923bfce2b8f6f3
Kristi Hwang <kristi.y.l.h@gmail.com>
2025-02-06 22:01:41 -0800

    feat: apply tile design, update fonts

--- src/components/assessment/home/AssessmentPage.tsx
+++ src/components/assessment/home/AssessmentPage.tsx
@@ -2,26 +2,73 @@ import Button from "@/components/Button";
 
 export default function AssessmentPage() {
   return (
-    <div className="left-[250px] mt-[76px] flex h-[547px] w-[1478px] flex-col items-center justify-center gap-[75px] px-[40px] opacity-0">
-      <div>
-        <p className="h-[40px] w-[700px]">Hey there tech Wizard!</p>
+    <div
+      alt="component-container"
+      className="left-[250px] mt-[76px] flex h-[547px] w-[1478px] flex-col items-center justify-center gap-[75px] px-[40px]"
+    >
+      <div alt="intro-container">
+        <p className="font-inter h-[40px] w-[700px] text-[39px] leading-[40px]">
+          Hey there tech Wizard!
+        </p>
+        <p className="font-inter decoration-skip-ink text-left text-[20px] font-medium leading-[24px]">
+          {" "}
+          What's your tech superpower level? Are you a beginner Batman or an
+          expert Iron Man? Let us know so we can team you up with the right
+          sidekick.
+        </p>
       </div>
-      <div className="flex flex-row">
-        <div className="h-[192px] w-[439.33px] gap-[24px] px-[24px]">
-          Front End Development
-          <Button className="w-full bg-[#217A56] py-[10px] text-black">
+      <div
+        alt="tile-row"
+        className="flex h-[192px] w-[1398px] flex-row gap-[40px]"
+      >
+        <div
+          alt="tile"
+          className="flex h-[192px] w-[439px] flex-col items-center justify-center gap-[24px] bg-[#F5F5F5] p-[24px]"
+        >
+          <p
+            alt="tile-name"
+            className="font-inter decoration-skip-ink h-[30px] w-[283px] text-center text-[25px] font-semibold leading-[30px]"
+          >
+            Front End Development
+          </p>
+          <p className="font-inter h-[19px] w-[8px] text-center text-[16px] font-medium leading-[19px] text-[#000000]">
+            -
+          </p>
+          <Button className="font-inter h-[47px] w-[207px] w-full gap-[8px] bg-[#217A56] px-[22px] py-[10px] py-[14px] text-[16px] font-semibold leading-[19px] text-[#F5F5F5]">
             Take Assessment
           </Button>
         </div>
-        <div className="h-[192px] w-[439.33px] gap-[24px] px-[24px]">
-          Back End Development
-          <Button className="w-full bg-[#217A56] py-[10px] text-black">
+        <div
+          alt="tile"
+          className="flex h-[192px] w-[439.33px] flex-col items-center justify-center gap-[24px] bg-[#F5F5F5] p-[24px]"
+        >
+          <p
+            alt="tile-name"
+            className="font-inter decoration-skip-ink h-[30px] w-[283px] text-center text-[25px] font-semibold leading-[30px]"
+          >
+            Back End Development
+          </p>
+          <p className="font-inter h-[19px] w-[8px] text-center text-[16px] font-medium leading-[19px] text-[#000000]">
+            -
+          </p>
+          <Button className="font-inter h-[47px] w-[207px] w-full gap-[8px] bg-[#217A56] px-[22px] py-[10px] py-[14px] text-[16px] font-semibold leading-[19px] text-[#F5F5F5]">
             Take Assessment
           </Button>
         </div>
-        <div className="h-[192px] w-[439.33px] gap-[24px] px-[24px]">
-          UX Design
-          <Button className="w-full bg-[#217A56] py-[10px] text-black">
+        <div
+          alt="tile"
+          className="flex h-[192px] w-[439.33px] flex-col items-center justify-center gap-[24px] bg-[#F5F5F5] p-[24px]"
+        >
+          <p
+            alt="tile-name"
+            className="font-inter decoration-skip-ink h-[30px] w-[283px] text-center text-[25px] font-semibold leading-[30px]"
+          >
+            UX Design
+          </p>
+          <p className="font-inter h-[19px] w-[8px] text-center text-[16px] font-medium leading-[19px] text-[#000000]">
+            -
+          </p>
+          <Button className="font-inter h-[47px] w-[207px] w-full gap-[8px] bg-[#217A56] px-[22px] py-[10px] py-[14px] text-[16px] font-semibold leading-[19px] text-[#F5F5F5]">
             Coming Soon
           </Button>
         </div>
@@ -29,39 +76,3 @@ export default function AssessmentPage() {
     </div>
   );
 }
-
-// import Button from "@/components/Button";
-
-// export default function AssessmentPage() {
-//   return (
-//     <div className="flex h-screen items-center justify-center">
-//       <div className="flex h-[547px] w-[1478px] flex-col items-center justify-center gap-[75px] px-[40px] opacity-0">
-//         <div>
-//           <p className="h-[40px] w-[700px] text-center">
-//             Hey there tech Wizard!
-//           </p>
-//         </div>
-//         <div className="flex flex-col items-center gap-[24px]">
-//           <div className="flex flex-col items-center">
-//             <p>Front End Development</p>
-//             <Button className="w-full bg-[#217A56] py-[10px] text-black">
-//               Take Assessment
-//             </Button>
-//           </div>
-//           <div className="flex flex-col items-center">
-//             <p>Back End Development</p>
-//             <Button className="w-full bg-[#217A56] py-[10px] text-black">
-//               Take Assessment
-//             </Button>
-//           </div>
-//           <div className="flex flex-col items-center">
-//             <p>UX Design</p>
-//             <Button className="w-full bg-[#217A56] py-[10px] text-black">
-//               Coming Soon
-//             </Button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
