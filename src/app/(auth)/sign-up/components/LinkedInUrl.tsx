8522aa3444946e48bc8cffc9bb90cc0d0c72e238
Homvi <honvedoadamkj@gmail.com>
2025-02-07 13:45:04 +0100

    feat: add LinkedIn URL input field with icon and placeholder

--- src/app/(auth)/sign-up/components/LinkedInUrl.tsx
+++ src/app/(auth)/sign-up/components/LinkedInUrl.tsx
@@ -1,12 +1,23 @@
+import { LinkIcon } from "@heroicons/react/24/outline";
+import TextInput from "@/components/inputs/TextInput";
+
 export default function LinkedInUrl() {
   return (
-    <div className="py-10">
+    <div className="flex flex-col gap-8 py-10">
       <h2 className="text-2xl font-semibold text-base-300">
         If you are on LinkedIn what is your profile URL?
       </h2>
-      <p className="w-full text-right text-[10px] font-medium text-neutral">
-        (optional)
-      </p>
+      <div>
+        <p className="w-full text-right text-[10px] font-medium text-neutral">
+          (optional)
+        </p>
+        <TextInput
+          id="url"
+          placeholder="e.g. https://www.linkedin.com/yourprofile"
+          ariaLabel="link"
+          inputGroupContent={<LinkIcon />}
+        />
+      </div>
     </div>
   );
 }
