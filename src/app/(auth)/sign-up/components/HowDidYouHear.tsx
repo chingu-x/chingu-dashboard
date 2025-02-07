5e1e32eb51303be8443228e847c1a1ad1fa12795
Homvi <honvedoadamkj@gmail.com>
2025-02-07 13:45:25 +0100

    feat: refactor "HowDidYouHear" component to use RadioGroupItem

--- src/app/(auth)/sign-up/components/HowDidYouHear.tsx
+++ src/app/(auth)/sign-up/components/HowDidYouHear.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import RadioGroupVertical from "@/components/inputs/RadioGroup/RadioGroupVertical";
+import RadioGroupItem from "@/components/inputs/RadioGroup/RadioGroupItem";
 
 interface Option {
   id: string;
@@ -7,16 +7,13 @@ interface Option {
   label: string;
 }
 
-const optionsFirstCol: Option[] = [
+const options: Option[] = [
   { id: "devto", value: "devto", label: "dev.to" },
   { id: "freecodecamp", value: "freecodecamp", label: "FreeCodeCamp Forum" },
   { id: "medium", value: "medium", label: "Medium" },
   { id: "linkedin", value: "linkedin", label: "LinkedIn" },
   { id: "twitter", value: "twitter", label: "X (Formerly Twitter)" },
   { id: "other", value: "other", label: "Other" },
-];
-
-const optionsSecondCol: Option[] = [
   { id: "job_hackers", value: "job_hackers", label: "The Job Hackers" },
   { id: "google_search", value: "google_search", label: "Google Search" },
   {
@@ -31,37 +28,36 @@ const optionsSecondCol: Option[] = [
 export default function HowDidYouHear() {
   const [selectedOption, setSelectedOption] = useState<string>("");
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedOption(e.target.value);
+  const handleChange = (value: string) => {
+    setSelectedOption(value);
   };
 
   return (
-    <div className="py-10">
+    <div className="">
       <h2 className="text-2xl font-semibold text-base-300">
         How did you hear about us?
       </h2>
-      <p className="w-full text-right text-[10px] font-medium text-neutral">
+      <div className="h-[19px]"></div>
+      <p className="h-8 w-full text-right text-[10px] font-medium text-neutral">
         (optional)
       </p>
-      <div className="grid grid-cols-2 gap-x-8 gap-y-6 pt-10">
-        <RadioGroupVertical
-          options={optionsFirstCol.map((option) => ({
-            ...option,
-            checked: selectedOption === option.value,
-            onChange: handleChange,
-          }))}
-          name="how_did_you_hear"
-          className="font-semibold text-base-300"
-        />
-        <RadioGroupVertical
-          options={optionsSecondCol.map((option) => ({
-            ...option,
-            checked: selectedOption === option.value,
-            onChange: handleChange,
-          }))}
-          name="how_did_you_hear"
-          className="font-semibold text-base-300"
-        />
+      <div className="grid grid-cols-2 gap-4">
+        {options.map((option) => (
+          <div
+            className="w-[317px] cursor-pointer rounded-lg border border-neutral-content p-4"
+            key={option.id}
+            onClick={() => handleChange(option.value)}
+          >
+            <RadioGroupItem
+              id={option.id}
+              label={option.label}
+              checked={selectedOption === option.value}
+              onChange={() => handleChange(option.value)}
+              name="how_did_you_hear"
+              className="text-sm font-semibold text-base-300"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
