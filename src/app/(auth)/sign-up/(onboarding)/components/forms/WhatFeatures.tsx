a085562407594e1d1de26cceb6299ee109885b77
Homvi <honvedoadamkj@gmail.com>
2025-02-11 18:34:20 +0100

    refactor(what-features): simplify option structure and improve readability
    - Merged `optionsDataFirstCol` and `optionsDataSecondCol` into a single `options` array.
    - Removed redundant `formatOption` function and integrated option formatting directly into JSX.
    - Improved layout structure by wrapping the header and description inside a container.
    - Adjusted checkbox styling for better UI consistency.

--- src/app/(auth)/sign-up/(onboarding)/components/forms/WhatFeatures.tsx
+++ src/app/(auth)/sign-up/(onboarding)/components/forms/WhatFeatures.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import CheckboxGroup from "@/components/inputs/CheckBoxGroup/CheckboxGroupVertical";
 import FormWrapper from "@/app/(auth)/sign-up/(onboarding)/components/forms/FormWrapper";
 import QuestionHeader from "@/app/(auth)/sign-up/(onboarding)/components/forms/QuestionHeader";
 import FormSpacer from "@/app/(auth)/sign-up/(onboarding)/components/forms/FormsSpacer";
+import { CheckboxGroupItem } from "@/components/inputs/CheckBoxGroup/CheckboxGroupItem";
 
 interface Option {
   id: string;
@@ -10,7 +10,7 @@ interface Option {
   description: string;
 }
 
-const optionsDataFirstCol: Option[] = [
+const options: Option[] = [
   {
     id: "1",
     title: "Solidarity",
@@ -31,9 +31,6 @@ const optionsDataFirstCol: Option[] = [
     title: "Specific Study Groups",
     description: "Either the #YDKJS or P1xt Guide study-groups",
   },
-];
-
-const optionsDataSecondCol: Option[] = [
   {
     id: "5",
     title: "Teamwork",
@@ -42,8 +39,7 @@ const optionsDataSecondCol: Option[] = [
   {
     id: "6",
     title: "Accountability",
-    // eslint-disable-next-line quotes
-    description: 'Having an "Accountability Buddy" to help me stay motivated',
+    description: "Having an 'Accountability Buddy' to help me stay motivated",
   },
   {
     id: "7",
@@ -64,61 +60,59 @@ export default function WhatFeatures() {
     const { value } = e.target;
     const isAlreadySelected = selectedFeatures.includes(value);
 
-    // If the feature is not already selected and maximum (3) is reached,
-    // revert the change on the DOM and do nothing.
     if (!isAlreadySelected && selectedFeatures.length >= 3) {
       e.target.checked = false;
       return;
     }
 
-    setSelectedFeatures((prev) => {
-      if (prev.includes(value)) {
-        return prev.filter((item) => item !== value);
-      }
-      return [...prev, value];
-    });
+    setSelectedFeatures((prev) =>
+      isAlreadySelected
+        ? prev.filter((item) => item !== value)
+        : [...prev, value],
+    );
   };
 
-  // When three items are selected, disable the unchecked ones.
   const isMaxReached = selectedFeatures.length >= 3;
-
-  const formatOption = (option: Option) => ({
-    id: option.id,
-    value: option.id,
-    label: (
-      <div className="flex flex-col gap-2 text-base-300">
-        <h4 className="text-sm font-semibold leading-4">{option.title}</h4>
-        <p className="max-w-[245px] text-sm font-medium leading-4">
-          {option.description}
-        </p>
-      </div>
-    ),
-    checked: selectedFeatures.includes(option.id),
-    // Disable this option if it’s not already selected and max count is reached.
-    disabled: isMaxReached && !selectedFeatures.includes(option.id),
-  });
-
-  const optionsFirstCol = optionsDataFirstCol.map(formatOption);
-  const optionsSecondCol = optionsDataSecondCol.map(formatOption);
 
   return (
     <FormWrapper>
-      <QuestionHeader>What features are you most excited about?</QuestionHeader>
-      <p className="mb-6 text-sm font-medium leading-4 text-base-300">
-        (Maximum 3)
-      </p>
-      <FormSpacer />
-      <div className="grid grid-cols-2 gap-x-8 gap-y-6">
-        <CheckboxGroup
-          options={optionsFirstCol}
-          onChange={handleChange}
-          name="features"
-        />
-        <CheckboxGroup
-          options={optionsSecondCol}
-          onChange={handleChange}
-          name="features"
-        />
+      <div>
+        <div className="gap-1">
+          <QuestionHeader>
+            What features are you most excited about?
+          </QuestionHeader>
+          <p className="mb-6 text-sm font-medium leading-4 text-base-300">
+            (Maximum 3)
+          </p>
+        </div>
+
+        <FormSpacer />
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        {options.map((option) => (
+          <div
+            className="min-w-[220px] rounded-lg border border-neutral-content p-4"
+            key={option.id}
+          >
+            <CheckboxGroupItem
+              id={option.id}
+              label={
+                <div className="flex flex-col gap-2 text-base-300">
+                  <h4 className="text-sm font-semibold leading-4">
+                    {option.title}
+                  </h4>
+                  <p className="max-w-[245px] text-sm font-medium leading-4">
+                    {option.description}
+                  </p>
+                </div>
+              }
+              value={option.id}
+              checked={selectedFeatures.includes(option.id)}
+              disabled={isMaxReached && !selectedFeatures.includes(option.id)}
+              onChange={handleChange}
+            />
+          </div>
+        ))}
       </div>
     </FormWrapper>
   );
