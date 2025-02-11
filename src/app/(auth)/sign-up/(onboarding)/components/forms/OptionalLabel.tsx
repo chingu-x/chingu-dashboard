ad7f2922fe78a0f8964d27ddda08df10f76432fa
Homvi <honvedoadamkj@gmail.com>
2025-02-11 18:32:56 +0100

    style(optional-label): adjust height from 8 to 6
    Updated the height of `OptionalLabel` from `h-8` to `h-6` for better spacing and alignment with other form elements.

--- src/app/(auth)/sign-up/(onboarding)/components/forms/OptionalLabel.tsx
+++ src/app/(auth)/sign-up/(onboarding)/components/forms/OptionalLabel.tsx
@@ -1,6 +1,6 @@
 export default function OptionalLabel() {
   return (
-    <p className="h-8 w-full text-right text-[10px] font-medium text-neutral">
+    <p className="h-6 w-full text-right text-[10px] font-medium text-neutral">
       (optional)
     </p>
   );
